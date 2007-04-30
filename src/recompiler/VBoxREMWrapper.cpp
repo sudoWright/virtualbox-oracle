b2eb8e3a8f01faf94c0730d5a37269b0088607ae
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-04-30 12:03:47 +0000

    Removed the old recompiler code.
    svn:sync-xref-src-repo-rev: r20831

--- src/recompiler/VBoxREMWrapper.cpp
+++ src/recompiler/VBoxREMWrapper.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxREMWrapper.cpp 20593 2007-04-19 18:22:33Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxREMWrapper.cpp 20831 2007-04-30 12:03:47Z knut.osmundsen@oracle.com $ */
 /** @file
  *
  * VBoxREM Win64 DLL Wrapper.
