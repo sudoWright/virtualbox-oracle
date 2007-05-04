b4b2185d91fef165a46b09cec352e4b06f4c53cb
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-05-04 18:23:45 +0000

    Drop the VINF_IOM_HC_IOPORT_READWRITE status code as it isn't really used. Made VINF_EM_LAST inclusive as per last/end convention used elsewhere in the VM.
    svn:sync-xref-src-repo-rev: r20938

--- src/VBox/VMM/VMMR0/VMMR0.cpp
+++ src/VBox/VMM/VMMR0/VMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMR0.cpp 20578 2007-04-19 13:52:53Z noreply@oracle.com $ */
+/* $Id: VMMR0.cpp 20938 2007-05-04 18:23:45Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMM - Host Context Ring 0.
  */
@@ -278,9 +278,6 @@ static void vmmR0RecordRC(PVM pVM, int rc)
             break;
         case VINF_IOM_HC_IOPORT_WRITE:
             STAM_COUNTER_INC(&pVM->vmm.s.StatGCRetIOWrite);
-            break;
-        case VINF_IOM_HC_IOPORT_READWRITE:
-            STAM_COUNTER_INC(&pVM->vmm.s.StatGCRetIOReadWrite);
             break;
         case VINF_IOM_HC_MMIO_READ:
             STAM_COUNTER_INC(&pVM->vmm.s.StatGCRetMMIORead);
