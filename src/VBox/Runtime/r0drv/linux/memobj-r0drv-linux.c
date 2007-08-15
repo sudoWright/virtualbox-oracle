90e927e3c1a17cdbf43f288d4be09dd6bdf7503d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-08-15 19:55:16 +0000

    Fixed build error and changed from $ to $.
    svn:sync-xref-src-repo-rev: r23648

--- src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: memobj-r0drv-linux.c 23645 2007-08-15 19:41:26Z knut.osmundsen@oracle.com $ */
+/* $Revision: 23648 $ */
 /** @file
  * innotek Portable Runtime - Ring-0 Memory Objects, Linux.
  */
@@ -209,7 +209,7 @@ static int rtR0MemObjLinuxAllocPages(PRTR0MEMOBJLNX *ppMemLnx, RTR0MEMOBJTYPE en
             BUG();
     }
 
-    fPageByPage = false;
+    fContiguous = true;
 #endif /* < 2.4.22 */
     pMemLnx->fContiguous = fContiguous;
 
