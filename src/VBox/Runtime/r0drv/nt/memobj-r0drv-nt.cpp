3bb6fc4e14dfb33ba23ad3e7553ac3c2da784a62
Frank Mehnert <noreply@oracle.com>
2007-09-13 07:10:32 +0000

    eol
    svn:sync-xref-src-repo-rev: r24433

--- src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
@@ -1,4 +1,4 @@
-/* $Id: memobj-r0drv-nt.cpp 24400 2007-09-12 10:12:31Z noreply@oracle.com $ */
+/* $Id: memobj-r0drv-nt.cpp 24433 2007-09-13 07:10:32Z noreply@oracle.com $ */
 /** @file
  * innotek Portable Runtime - Ring-0 Memory Objects, NT.
  */
--- src/VBox/Runtime/table/table.cpp
+++ src/VBox/Runtime/table/table.cpp
@@ -1,4 +1,4 @@
-/* $Id: table.cpp 23517 2007-08-07 17:07:59Z noreply@oracle.com $ */
+/* $Id: table.cpp 24433 2007-09-13 07:10:32Z noreply@oracle.com $ */
 /** @file
  * innotek Portable Runtime - AVL tree, RTHCPHYS, unique keys.
  */
