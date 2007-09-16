8b793e55983968aba4068bec0d8c71186615a738
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-09-16 20:45:14 +0000

    use SUPR0Printf as uprintf doesn't seem to grok half of the string we give it.
    svn:sync-xref-src-repo-rev: r24545

--- src/VBox/HostDrivers/Support/SUPDRV.h
+++ src/VBox/HostDrivers/Support/SUPDRV.h
@@ -1,4 +1,4 @@
-/* $Revision: 24541 $ */
+/* $Revision: 24545 $ */
 /** @file
  * VirtualBox Support Driver - Internal header.
  */
@@ -187,7 +187,7 @@ __END_DECLS
  * Solaris
  */
 #elif defined(RT_OS_SOLARIS)
-# define OSDBGPRINT(a) uprintf a
+# define OSDBGPRINT(a) SUPR0Printf a
 
 
 #else
