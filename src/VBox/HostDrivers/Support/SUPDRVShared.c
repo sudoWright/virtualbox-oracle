ff5e9f1912b7764c2c1a35baca2cef29033077a4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-10-05 13:32:57 +0000

    RTSemEventWaitNoResume
    svn:sync-xref-src-repo-rev: r25065

--- src/VBox/HostDrivers/Support/SUPDRVShared.c
+++ src/VBox/HostDrivers/Support/SUPDRVShared.c
@@ -1,4 +1,4 @@
-/* $Revision: 24829 $ */
+/* $Revision: 25065 $ */
 /** @file
  * VirtualBox Support Driver - Shared code.
  */
@@ -126,6 +126,7 @@ static SUPFUNC g_aFunctions[] =
     { "RTSemEventCreate",                       (void *)RTSemEventCreate },
     { "RTSemEventSignal",                       (void *)RTSemEventSignal },
     { "RTSemEventWait",                         (void *)RTSemEventWait },
+    { "RTSemEventWaitNoResume",                 (void *)RTSemEventWaitNoResume },
     { "RTSemEventDestroy",                      (void *)RTSemEventDestroy },
     { "RTSpinlockCreate",                       (void *)RTSpinlockCreate },
     { "RTSpinlockDestroy",                      (void *)RTSpinlockDestroy },
