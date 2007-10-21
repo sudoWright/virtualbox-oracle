12b095d2da1e1b8eadf858572890e1d1342cc48f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-10-21 20:37:26 +0000

    alloc* -> common/alloc
    svn:sync-xref-src-repo-rev: r25524

--- src/VBox/Runtime/common/alloc/heapsimple.cpp
+++ src/VBox/Runtime/common/alloc/heapsimple.cpp
@@ -1,4 +1,4 @@
-/* $Id: heapsimple.cpp 23517 2007-08-07 17:07:59Z noreply@oracle.com $ */
+/* $Id: heapsimple.cpp 25524 2007-10-21 20:37:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * innotek Portable Runtime - A Simple Heap.
  */
