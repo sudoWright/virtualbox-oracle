a22d48c05edaf509c8ead9b034436fd6f0de89de
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-10-21 20:26:23 +0000

    Moved the time stuff out of the root dir.
    svn:sync-xref-src-repo-rev: r25518

--- src/VBox/Runtime/time/timesup.cpp
+++ src/VBox/Runtime/time/timesup.cpp
@@ -1,4 +1,4 @@
-/* $Id: timesup.cpp 23517 2007-08-07 17:07:59Z noreply@oracle.com $ */
+/* $Id: timesup.cpp 25518 2007-10-21 20:26:23Z knut.osmundsen@oracle.com $ */
 /** @file
  * innotek Portable Runtime - Time using SUPLib.
  */
