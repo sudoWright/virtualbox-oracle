71aff0812537b193a109782decab97ca12009c39
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-10-21 20:54:16 +0000

    various stray files -> common/misc/
    svn:sync-xref-src-repo-rev: r25531

--- src/VBox/Runtime/common/misc/zip.cpp
+++ src/VBox/Runtime/common/misc/zip.cpp
@@ -1,4 +1,4 @@
-/* $Id: zip.cpp 23517 2007-08-07 17:07:59Z noreply@oracle.com $ */
+/* $Id: zip.cpp 25531 2007-10-21 20:54:16Z knut.osmundsen@oracle.com $ */
 /** @file
  * innotek Portable Runtime - Compression.
  */
