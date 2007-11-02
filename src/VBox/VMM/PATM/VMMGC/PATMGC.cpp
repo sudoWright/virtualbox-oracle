15987a6f3c2b75ec688b7e400b0cc93e871f61d8
Sander van Leeuwen <noreply@oracle.com>
2007-11-02 09:46:07 +0000

    More logging
    svn:sync-xref-src-repo-rev: r25901

--- src/VBox/VMM/PATM/VMMGC/PATMGC.cpp
+++ src/VBox/VMM/PATM/VMMGC/PATMGC.cpp
@@ -1,4 +1,4 @@
-/* $Id: PATMGC.cpp 25250 2007-10-12 14:14:27Z noreply@oracle.com $ */
+/* $Id: PATMGC.cpp 25901 2007-11-02 09:46:07Z noreply@oracle.com $ */
 /** @file
  * PATM - Dynamic Guest OS Patching Manager - Guest Context
  */
@@ -102,7 +102,7 @@ PATMGCDECL(int) PATMGCHandleWriteToPatchPage(PVM pVM, PCPUMCTXCORE pRegFrame, RT
 
 #ifdef LOG_ENABLED
     if (pPatchPage)
-        Log(("PATMIsWriteToPatchPage: Found page %VGv for write to %VGv %d bytes\n", pPatchPage->Core.Key, GCPtr, cbWrite));
+        Log(("PATMIsWriteToPatchPage: Found page %VGv for write to %VGv %d bytes (page low:high %VGv:%VGv\n", pPatchPage->Core.Key, GCPtr, cbWrite, pPatchPage->pLowestAddrGC, pPatchPage->pHighestAddrGC));
 #endif
 
     if (pPatchPage)
