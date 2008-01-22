b07f72e5c289b8073bcf2b034336caa5db686d32
Dmitry Kuminov <noreply@oracle.com>
2008-01-22 16:13:28 +0000

    Main: Fixed build after r27415.
    svn:sync-xref-src-repo-rev: r27425

--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.h 27415 2008-01-22 13:35:10Z noreply@oracle.com $ */
+/* $Id: MachineImpl.h 27425 2008-01-22 16:13:28Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -102,8 +102,6 @@ public:
         {
             /** Control of the direct session opened by openSession() */
             ComPtr <IInternalSessionControl> mDirectControl;
-            /** Cached IUnknown of mDirectControl (IPC) for fast comparison */
-            ComPtr <IUnknown> mDirectControlUnk;
 
             typedef std::list <ComPtr <IInternalSessionControl> > RemoteControlList;
 
