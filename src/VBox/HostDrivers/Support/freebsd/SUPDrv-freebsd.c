5c97c00a3c1b673f0de5ae30f66ff5f6f24f765b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-06-06 13:23:08 +0000

    space
    svn:sync-xref-src-repo-rev: r31737

--- src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c
+++ src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-freebsd.c 31727 2008-06-06 12:19:12Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrv-freebsd.c 31737 2008-06-06 13:23:08Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxDrv - FreeBSD specifics.
  */
@@ -529,6 +529,7 @@ bool VBOXCALL   supdrvOSObjCanAccess(PSUPDRVOBJ pObj, PSUPDRVSESSION pSession, c
     NOREF(prc);
     return false;
 }
+
 
 bool VBOXCALL  supdrvOSGetForcedAsyncTscMode(PSUPDRVDEVEXT pDevExt)
 {
