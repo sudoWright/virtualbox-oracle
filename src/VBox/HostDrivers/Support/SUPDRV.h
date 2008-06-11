5f27e9e3665d8202fcc0d7ca2c3c664254295a0e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-06-11 16:42:07 +0000

    The TSC checks are now fully done in the generic code.
    svn:sync-xref-src-repo-rev: r31947

--- src/VBox/HostDrivers/Support/SUPDRV.h
+++ src/VBox/HostDrivers/Support/SUPDRV.h
@@ -1,4 +1,4 @@
-/* $Revision: 31727 $ */
+/* $Revision: 31947 $ */
 /** @file
  * VirtualBox Support Driver - Internal header.
  */
@@ -711,8 +711,6 @@ typedef struct SUPDRVDEVEXT
     PMDL                    pGipMdl;
     /** GIP timer interval (ms). */
     ULONG                   ulGipTimerInterval;
-    /** Flag to force async GIP timer mode. */
-    BOOLEAN                 fForceAsyncTsc;
     /** Current CPU affinity mask. */
     KAFFINITY               uAffinityMask;
 # endif
