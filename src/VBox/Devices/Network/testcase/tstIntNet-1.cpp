14cfb218d919028941fc331d1e77213f5b59b188
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-07-15 20:00:27 +0000

    intnet: More TrunkIf code.
    svn:sync-xref-src-repo-rev: r33396

--- src/VBox/Devices/Network/testcase/tstIntNet-1.cpp
+++ src/VBox/Devices/Network/testcase/tstIntNet-1.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstIntNet-1.cpp 33375 2008-07-15 14:38:39Z knut.osmundsen@oracle.com $ */
+/* $Id: tstIntNet-1.cpp 33396 2008-07-15 20:00:27Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox - Testcase for internal networking, simple NetFlt trunk creation.
  */
@@ -122,6 +122,7 @@ int main(int argc, char **argv)
         g_cErrors++;
     }
 
+    RTThreadSleep(1000);
     SUPTerm(false /* not forced */);
 
     /*
