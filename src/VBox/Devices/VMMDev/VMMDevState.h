2fec3390a5958a9eff53d2fb9d3b8c83db59ba1b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-08-08 22:16:39 +0000

    VMMDev: Cosmetics.
    svn:sync-xref-src-repo-rev: r34375

--- src/VBox/Devices/VMMDev/VMMDevState.h
+++ src/VBox/Devices/VMMDev/VMMDevState.h
@@ -1,6 +1,6 @@
-/* $Id: VMMDevState.h 34374 2008-08-08 22:12:14Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMDevState.h 34375 2008-08-08 22:16:39Z knut.osmundsen@oracle.com $ */
 /** @file
- * VMMDev - Guest <-> VMM/Host communication device, Internal header.
+ * VMMDev - Guest <-> VMM/Host communication device, internal header.
  */
 
 /*
