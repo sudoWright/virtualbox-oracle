642568462c91792dd70f8f171ff894b0756bab88
Ulrich Moeller <noreply@oracle.com>
2008-09-02 13:09:44 +0000

    All: license header changes for 2.0 (OSE headers, add Sun GPL/LGPL disclaimer)
    svn:sync-xref-src-repo-rev: r35885

--- src/VBox/Runtime/VBox/logbackdoor-redirect.cpp
+++ src/VBox/Runtime/VBox/logbackdoor-redirect.cpp
@@ -1,6 +1,6 @@
-/* $Id: logbackdoor-redirect.cpp 35390 2008-08-26 17:30:49Z knut.osmundsen@oracle.com $ */
+/* $Id: logbackdoor-redirect.cpp 35885 2008-09-02 13:09:44Z noreply@oracle.com $ */
 /** @file
- * Virtual Box Runtime - RTLog stubs for the stripped down IPRT used by 
+ * Virtual Box Runtime - RTLog stubs for the stripped down IPRT used by
  *                       RuntimeGuestR3Shared (X11), output is redirected
  *                       to the RTLogBackdoor API where possible.
  */
