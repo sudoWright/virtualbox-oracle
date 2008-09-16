aacdbb9aa01b56472c4d86e58bcf479e2d67a885
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-09-16 03:12:57 +0000

    Debugger: kind of getting somewhere...
    svn:sync-xref-src-repo-rev: r36578

--- src/VBox/Debugger/VBoxDbgBase.h
+++ src/VBox/Debugger/VBoxDbgBase.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxDbgBase.h 36561 2008-09-15 15:22:01Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxDbgBase.h 36578 2008-09-16 03:12:57Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Debugger GUI - Base class.
  */
@@ -58,7 +58,6 @@ public:
     virtual ~VBoxDbgBase();
 
 
-protected:
     /**
      * Checks if the VM is OK for normal operations.
      * @returns true if ok, false if not.
