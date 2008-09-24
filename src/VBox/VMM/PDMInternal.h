f80745e79f5e67519046d7f1addf216e78dcd94a
Sander van Leeuwen <noreply@oracle.com>
2008-09-24 14:07:47 +0000

    Started with VMM device heap for use with VT-x real-mode emulation. (v86 tss)
    svn:sync-xref-src-repo-rev: r36992

--- src/VBox/VMM/PDMInternal.h
+++ src/VBox/VMM/PDMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PDMInternal.h 36942 2008-09-22 16:03:25Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMInternal.h 36992 2008-09-24 14:07:47Z noreply@oracle.com $ */
 /** @file
  * PDM - Internal header file.
  */
@@ -842,6 +842,14 @@ typedef struct PDM
     R3PTRTYPE(PPDMASYNCCOMPLETIONMANAGER) pAsyncCompletionManagerHead;
     /** Head of the templates. (singly linked) */
     R3PTRTYPE(PPDMASYNCCOMPLETIONTEMPLATE) pAsyncCompletionTemplates;
+
+    /** PDM VMM device heap
+     * @{ */
+    RTR3PTR                         pvVMMDevHeap;
+    RTUINT                          cbVMMDevHeap;
+    RTUINT                          cbVMMDevHeapLeft;
+    RTGCPHYS                        GCPhysVMMDevHeap;
+    /** @} */
 
     /** TEMPORARY HACKS FOR NETWORK POLLING.
      * @todo fix NAT and kill this!
