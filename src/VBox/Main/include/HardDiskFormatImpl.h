fd068e1e423d54d4b862160b8aea84efa1cfe4cc
Dmitry Kuminov <noreply@oracle.com>
2008-11-07 11:23:23 +0000

    Main: Removed redundant suffixes from DataType, DataFlags and HardDiskFormatCapabilities enums + cleanup.
    svn:sync-xref-src-repo-rev: r38981

--- src/VBox/Main/include/HardDiskFormatImpl.h
+++ src/VBox/Main/include/HardDiskFormatImpl.h
@@ -1,4 +1,4 @@
-/* $Id: HardDiskFormatImpl.h 38837 2008-11-05 10:10:24Z noreply@oracle.com $ */
+/* $Id: HardDiskFormatImpl.h 38981 2008-11-07 11:23:23Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -93,7 +93,7 @@ private:
         Bstr description;
         DataType_T type;
         ULONG flags;
-        Bstr defaults;
+        Bstr defaultValue;
     };
     typedef std::list <Property> PropertyList;
 
