81240142e16030ca9f96e5c8863bb5e78c2b8c9b
Sander van Leeuwen <noreply@oracle.com>
2008-12-16 09:25:08 +0000

    Save and restore the host's EFER msr in the 32->64 switcher.
    svn:sync-xref-src-repo-rev: r41069

--- src/VBox/VMM/CPUMInternal.h
+++ src/VBox/VMM/CPUMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: CPUMInternal.h 40866 2008-12-13 06:11:26Z knut.osmundsen@oracle.com $ */
+/* $Id: CPUMInternal.h 41069 2008-12-16 09:25:08Z noreply@oracle.com $ */
 /** @file
  * CPUM - Internal header file.
  */
@@ -188,8 +188,13 @@ typedef struct CPUMHOSTCTX
      * This member is not used by the hypervisor context. */
     CPUMSYSENTER    SysEnter;
 
+    /** MSRs
+     * @{ */
+    uint64_t        efer;
+    /** @} */
+
     /* padding to get 64byte aligned size */
-    uint8_t         auPadding[24+32];
+    uint8_t         auPadding[16+32];
 
 #elif HC_ARCH_BITS == 64 || defined(VBOX_WITH_HYBRID_32BIT_KERNEL)
 
