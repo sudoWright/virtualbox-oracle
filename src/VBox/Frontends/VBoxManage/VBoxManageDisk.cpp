3c30e413b5d1b9f7ad0e26af86b0fb914e643ede
Ulrich Moeller <noreply@oracle.com>
2009-02-05 16:08:49 +0000

    Main: rework error macros everywhere; make error messages much more readable (esp. with VBoxManage); use shared function to actually print message; reduces size of VBoxManage debug build from 3.4 to 2.3 MB
    svn:sync-xref-src-repo-rev: r42461

--- src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageDisk.cpp 41882 2009-01-21 16:53:45Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxManageDisk.cpp 42461 2009-02-05 16:08:49Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - The disk delated commands.
  */
@@ -27,6 +27,7 @@
 #include <VBox/com/com.h>
 #include <VBox/com/array.h>
 #include <VBox/com/ErrorInfo.h>
+#include <VBox/com/errorprint2.h>
 #include <VBox/com/VirtualBox.h>
 
 #include <iprt/asm.h>
