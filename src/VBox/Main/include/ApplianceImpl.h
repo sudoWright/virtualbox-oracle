e00b93727d12f96d2f5f59310906b172e3aa4e12
Ulrich Moeller <noreply@oracle.com>
2009-02-09 16:33:28 +0000

    OVF: change IVirtualSystemDescription::setFinalValues method; remove IVirtualSystemDescription::disableItem; API docs; implement lots of VBoxManage import command line handling
    svn:sync-xref-src-repo-rev: r42567

--- src/VBox/Main/include/ApplianceImpl.h
+++ src/VBox/Main/include/ApplianceImpl.h
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImpl.h 42566 2009-02-09 16:29:25Z noreply@oracle.com $ */
+/* $Id: ApplianceImpl.h 42567 2009-02-09 16:33:28Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -152,10 +152,9 @@ public:
                               ComSafeArrayOut(BSTR, aOrigValues),
                               ComSafeArrayOut(BSTR, aConfigValues),
                               ComSafeArrayOut(BSTR, aExtraConfigValues));
-    STDMETHOD(DisableItem)(ULONG index);
-    STDMETHOD(SetFinalValues)(ComSafeArrayIn(IN_BSTR, aFinalValues));
 
-
+    STDMETHOD(SetFinalValues)(ComSafeArrayIn(BOOL, aEnabled),
+                              ComSafeArrayIn(IN_BSTR, aFinalValues));
 
     STDMETHOD(GetWarnings)(ComSafeArrayOut(BSTR, aWarnings));
 
