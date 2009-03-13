0bcdcc6b41b7a72c2510c0a6c6107de4abca8566
Pritesh Kothari <noreply@oracle.com>
2009-03-13 12:38:53 +0000

    Cbinding: burn fix :(
    svn:sync-xref-src-repo-rev: r44353

--- src/VBox/Main/cbinding/VBoxXPCOMCGlue.c
+++ src/VBox/Main/cbinding/VBoxXPCOMCGlue.c
@@ -1,4 +1,4 @@
-/* $Revision: 44350 $ */
+/* $Revision: 44353 $ */
 /** @file
  * Glue code for dynamically linking to VBoxXPCOMC.
  */
@@ -164,7 +164,7 @@ int VBoxCGlueInit(void)
     if (tryLoadOne("/opt/VirtualBox/i386") == 0)
         return 0;
 #elif defined(__APPLE__)
-    if (tryLoadOne("/Application/VirtualBox.app/Contents/MacOS", pszMsgPrefix) == 0)
+    if (tryLoadOne("/Application/VirtualBox.app/Contents/MacOS") == 0)
         return 0;
 #else
 # error "port me"
