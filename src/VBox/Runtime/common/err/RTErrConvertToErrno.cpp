3570848a8e524c7fa6a2223f115fb30d50e79357
Michael Thayer <noreply@oracle.com>
2009-03-16 16:10:06 +0000

    Runtime: weaken an assertion in RTErrConvertToErrno
    svn:sync-xref-src-repo-rev: r44528

--- src/VBox/Runtime/common/err/RTErrConvertToErrno.cpp
+++ src/VBox/Runtime/common/err/RTErrConvertToErrno.cpp
@@ -1,4 +1,4 @@
-/* $Rev: 42910 $ */
+/* $Rev: 44528 $ */
 /** @file
  * IPRT - Convert iprt status codes to errno.
  */
@@ -440,7 +440,8 @@ RTDECL(int) RTErrConvertToErrno(int iErr)
 #endif
 
         default:
-            AssertMsgFailed(("Unhandled error code %Rrc\n", iErr));
+            if (iErr > -1000)  /* VBox private errors */
+                AssertMsgFailed(("Unhandled error code %Rrc\n", iErr));
 #ifdef EPROTO
             return EPROTO;
 #else
