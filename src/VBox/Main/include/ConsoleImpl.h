c83ef5b03833e08a685f78b92fe847516181c820
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-04-02 15:38:31 +0000

    VMSetRuntimeError[V] refactoring: fFatal -> fFlags, may return informational status codes.
    svn:sync-xref-src-repo-rev: r45568

--- src/VBox/Main/include/ConsoleImpl.h
+++ src/VBox/Main/include/ConsoleImpl.h
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.h 45132 2009-03-26 19:35:20Z alexander.eichner@oracle.com $ */
+/* $Id: ConsoleImpl.h 45568 2009-04-02 15:38:31Z knut.osmundsen@oracle.com $ */
 
 /** @file
  *
@@ -457,9 +457,9 @@ private:
                         const char *pszFormat, va_list args);
 
     static DECLCALLBACK(void)
-    setVMRuntimeErrorCallback (PVM pVM, void *pvUser, bool fFatal,
-                               const char *pszErrorID,
-                               const char *pszFormat, va_list args);
+    setVMRuntimeErrorCallback (PVM pVM, void *pvUser, uint32_t fFatal,
+                               const char *pszErrorId,
+                               const char *pszFormat, va_list va);
 
     HRESULT                     captureUSBDevices (PVM pVM);
     void                        detachAllUSBDevices (bool aDone);
