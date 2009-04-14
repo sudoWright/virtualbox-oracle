8ab0694e5b0f2663633a96c6369185917d106e50
Pritesh Kothari <noreply@oracle.com>
2009-04-14 15:46:23 +0000

    Cbinding: Update version to 2.5
    svn:sync-xref-src-repo-rev: r45939

--- src/VBox/Main/cbinding/VBoxXPCOMCGlue.h
+++ src/VBox/Main/cbinding/VBoxXPCOMCGlue.h
@@ -1,4 +1,4 @@
-/* $Revision: 45938 $ */
+/* $Revision: 45939 $ */
 /** @file VBoxXPCOMCGlue.h
  * Glue for dynamically linking with VBoxXPCOMC.
  */
@@ -31,7 +31,7 @@
 #ifndef ___VBoxXPCOMC_cglue_h
 #define ___VBoxXPCOMC_cglue_h
 
-#include "VBoxCAPI_v2_2.h"
+#include "VBoxCAPI_v2_5.h"
 
 #ifdef __cplusplus
 extern "C" {
