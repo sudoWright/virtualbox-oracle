8aeea88ca2316fa27baa19554d882a36cf64cb1b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-07-02 11:50:23 +0000

    PDMQueue: tyop.
    svn:sync-xref-src-repo-rev: r49436

--- src/VBox/VMM/PDMQueue.cpp
+++ src/VBox/VMM/PDMQueue.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMQueue.cpp 49435 2009-07-02 11:49:05Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMQueue.cpp 49436 2009-07-02 11:50:23Z knut.osmundsen@oracle.com $ */
 /** @file
  * PDM Queue - Transport data and tasks to EMT and R3.
  */
@@ -800,7 +800,7 @@ static bool pdmR3QueueFlush(PPDMQUEUE pQueue)
                 while (pCur->pNextR3)
                     pCur = pCur->pNextR3;
                 pCur->pNextR3 = pItems;
-                pItem = pPending;
+                pItems = pPending;
             }
         }
         return false;
