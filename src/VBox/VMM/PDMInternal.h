2ba5bb9fa8a17f45bfb7157869d026579369c970
Sander van Leeuwen <noreply@oracle.com>
2009-07-02 13:52:46 +0000

    Backed out 49450. Not necessary
    svn:sync-xref-src-repo-rev: r49452

--- src/VBox/VMM/PDMInternal.h
+++ src/VBox/VMM/PDMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PDMInternal.h 49450 2009-07-02 13:45:03Z noreply@oracle.com $ */
+/* $Id: PDMInternal.h 49452 2009-07-02 13:52:46Z noreply@oracle.com $ */
 /** @file
  * PDM - Internal header file.
  */
@@ -229,10 +229,8 @@ typedef struct PDMCRITSECTINT
     STAMCOUNTER                     StatContentionRZUnlock;
     /** R3 lock contention. */
     STAMCOUNTER                     StatContentionR3;
-#ifdef VBOX_WITH_STATISTICS
     /** Profiling the time the section is locked. */
     STAMPROFILEADV                  StatLocked;
-#endif
 } PDMCRITSECTINT;
 typedef PDMCRITSECTINT *PPDMCRITSECTINT;
 
