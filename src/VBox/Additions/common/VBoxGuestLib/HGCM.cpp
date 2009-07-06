5ab01aa0c60a5adeec671fe16ebb378b70b25a76
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-07-06 15:35:22 +0000

    oops.
    svn:sync-xref-src-repo-rev: r49591

--- src/VBox/Additions/common/VBoxGuestLib/HGCM.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/HGCM.cpp
@@ -1,4 +1,4 @@
-/* $Revision: 49590 $ */
+/* $Revision: 49591 $ */
 /** @file
  * VBoxGuestLib - Host-Guest Communication Manager.
  *
@@ -115,7 +115,7 @@ void vbglHGCMHandleFree (struct VBGLHGCMHANDLEDATA *pHandle)
     return;
 }
 
-DECLVBGL(int) VbglR0HGCMInternalConnect (VBGLHGCMHANDLE *pHandle, VBoxGuestHGCMConnectInfo *pData)
+DECLVBGL(int) VbglHGCMConnect (VBGLHGCMHANDLE *pHandle, VBoxGuestHGCMConnectInfo *pData)
 {
     int rc;
     struct VBGLHGCMHANDLEDATA *pHandleData;
