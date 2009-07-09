9c6ee1b94240fcf93bf0b61d969691e39ca242ec
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-07-09 16:33:54 +0000

    VBoxGuest.cpp: Fixed bug in the wake-for-event wakeup code: missing event matching, caused waiters to wake up when they shouldn't.
    svn:sync-xref-src-repo-rev: r49864

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuest.cpp 49732 2009-07-07 22:51:24Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuest.cpp 49864 2009-07-09 16:33:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuest - Guest Additions Driver, Common Code.
  */
@@ -1477,7 +1477,8 @@ bool VBoxGuestCommonISR(PVBOXGUESTDEVEXT pDevExt)
              */
             fEvents |= pDevExt->f32PendingEvents;
             for (pWait = pDevExt->WaitList.pHead; pWait; pWait = pWait->pNext)
-                if (!pWait->fResEvents)
+                if (    (pWait->fReqEvents & fEvents)
+                    &&  !pWait->fResEvents)
                 {
                     pWait->fResEvents = pWait->fReqEvents & fEvents;
                     fEvents &= ~pWait->fResEvents;
