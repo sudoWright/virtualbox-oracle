b2707002b703b41beaabe759af9acf56cf4f5210
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-07-17 12:18:30 +0000

    IPRT: Moved rtPathVolumeSpecLen.
    svn:sync-xref-src-repo-rev: r50224

--- src/VBox/Runtime/r3/dir.cpp
+++ src/VBox/Runtime/r3/dir.cpp
@@ -1,4 +1,4 @@
-/* $Id: dir.cpp 49930 2009-07-10 16:35:40Z knut.osmundsen@oracle.com $ */
+/* $Id: dir.cpp 50224 2009-07-17 12:18:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Directory Manipulation.
  */
@@ -51,8 +51,8 @@
 #include <iprt/err.h>
 #include <iprt/assert.h>
 #include <iprt/uni.h>
-#include "internal/fs.h"
 #include "internal/dir.h"
+#include "internal/path.h"
 
 
 static DECLCALLBACK(bool) rtDirFilterWinNtMatch(PRTDIR pDir, const char *pszName);
