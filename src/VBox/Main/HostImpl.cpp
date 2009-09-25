2efc864f9cde73dda87777f57ff5083b5c54efb9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-09-25 15:26:52 +0000

    Host::uninit: llChildren is modified during child uninit.
    svn:sync-xref-src-repo-rev: r52855

--- src/VBox/Main/HostImpl.cpp
+++ src/VBox/Main/HostImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: HostImpl.cpp 52805 2009-09-24 19:39:07Z noreply@oracle.com $ */
+/* $Id: HostImpl.cpp 52855 2009-09-25 15:26:52Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation: Host
  */
@@ -341,15 +341,12 @@ void Host::uninit()
 
 #ifdef VBOX_WITH_USB
     /* uninit all USB device filters still referenced by clients */
-    for (USBDeviceFilterList::iterator it = m->llChildren.begin();
-         it != m->llChildren.end();
-         ++it)
+    while (!m->llChildren.empty())
     {
-        ComObjPtr<HostUSBDeviceFilter> &pChild = *it;
+        ComObjPtr<HostUSBDeviceFilter> pChild = m->llChildren.front();
+        m->llChildren.pop_front();
         pChild->uninit();
     }
-
-    m->llChildren.clear();
 
     m->llUSBDeviceFilters.clear();
 #endif
