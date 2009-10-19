76dcac88d3b74ddea77ffe4b088001ce64563d76
Ulrich Moeller <noreply@oracle.com>
2009-10-19 18:55:38 +0000

    windows build fix
    svn:sync-xref-src-repo-rev: r53667

--- src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageInfo.cpp 53663 2009-10-19 18:36:03Z noreply@oracle.com $ */
+/* $Id: VBoxManageInfo.cpp 53667 2009-10-19 18:55:38Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - The 'showvminfo' command and helper routines.
  */
@@ -1844,7 +1844,7 @@ HRESULT showVMInfo (ComPtr<IVirtualBox> virtualBox,
     if (SUCCEEDED(rc) && snapshot)
     {
         ComPtr<ISnapshot> currentSnapshot;
-        rc = machine->GetCurrentSnapshot(currentSnapshot.asOutParam());
+        rc = machine->COMGETTER(CurrentSnapshot)(currentSnapshot.asOutParam());
         if (SUCCEEDED(rc))
         {
             if (details != VMINFO_MACHINEREADABLE)
