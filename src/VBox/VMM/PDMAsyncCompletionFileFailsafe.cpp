83bc81ecc78f91b9f5e4ede8e706c2150b4732ad
Alexander Eichner <alexander.eichner@oracle.com>
2009-11-04 21:54:42 +0000

    AsyncCompletion: Don't enqueue requests if the endpoint is moving
    svn:sync-xref-src-repo-rev: r54381

--- src/VBox/VMM/PDMAsyncCompletionFileFailsafe.cpp
+++ src/VBox/VMM/PDMAsyncCompletionFileFailsafe.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMAsyncCompletionFileFailsafe.cpp 54171 2009-10-30 22:22:17Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMAsyncCompletionFileFailsafe.cpp 54381 2009-11-04 21:54:42Z alexander.eichner@oracle.com $ */
 /** @file
  * PDM Async I/O - Transport data asynchronous in R3 using EMT.
  * Failsafe File I/O manager.
@@ -155,6 +155,8 @@ int pdmacFileAioMgrFailsafe(RTTHREAD ThreadSelf, void *pvUser)
                         pAioMgr->pEndpointsHead->AioMgr.pEndpointPrev = pEndpointNew;
                     pAioMgr->pEndpointsHead = pEndpointNew;
 
+                    pAioMgr->cEndpoints++;
+
                     /*
                      * Process the task list the first time. There might be pending requests
                      * if the endpoint was migrated from another endpoint.
@@ -180,6 +182,8 @@ int pdmacFileAioMgrFailsafe(RTTHREAD ThreadSelf, void *pvUser)
 
                     if (pNext)
                         pNext->AioMgr.pEndpointPrev = pPrev;
+
+                    pAioMgr->cEndpoints--;
                     break;
                 }
                 case PDMACEPFILEAIOMGRBLOCKINGEVENT_CLOSE_ENDPOINT:
