c2266c966a72bfaaa75bbad3badcb709397a18a1
Frank Mehnert <noreply@oracle.com>
2009-12-29 19:11:46 +0000

    fix for compiling against Linux 2.6.33+
    svn:sync-xref-src-repo-rev: r56282

--- src/VBox/Additions/linux/drm/vboxvideo_drm.c
+++ src/VBox/Additions/linux/drm/vboxvideo_drm.c
@@ -1,4 +1,4 @@
-/** @file $Id: vboxvideo_drm.c 51623 2009-08-31 09:30:13Z noreply@oracle.com $
+/** @file $Id: vboxvideo_drm.c 56282 2009-12-29 19:11:46Z noreply@oracle.com $
  *
  * VirtualBox Additions Linux kernel driver, DRM support
  */
@@ -53,7 +53,9 @@
  *    Gareth Hughes <gareth@valinux.com>
  */
 
-#include <linux/autoconf.h>
+#ifndef AUTOCONF_INCLUDED
+# include <linux/autoconf.h>
+#endif
 #include <linux/version.h>
 #include <linux/module.h>
 
