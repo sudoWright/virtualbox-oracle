5ea8ab9c4abf1ef5db3408e2406bcb7f1c966285
Frank Mehnert <noreply@oracle.com>
2010-02-07 21:37:44 +0000

    build fix
    svn:sync-xref-src-repo-rev: r57389

--- src/VBox/Runtime/r3/posix/process-posix.cpp
+++ src/VBox/Runtime/r3/posix/process-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: process-posix.cpp 57388 2010-02-07 21:35:45Z noreply@oracle.com $ */
+/* $Id: process-posix.cpp 57389 2010-02-07 21:37:44Z noreply@oracle.com $ */
 /** @file
  * IPRT - Process, POSIX.
  */
@@ -381,6 +381,6 @@ RTR3DECL(int)   RTProcDaemonize(bool fNoChDir, bool fNoClose, const char *pszPid
         exit(0);
     }
 
-    return rc;
+    return VINF_SUCCESS;
 }
 
