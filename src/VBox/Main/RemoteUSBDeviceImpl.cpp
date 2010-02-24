c8eda5548d7585acb3b54dffe219805c2a29f2bf
Ulrich Moeller <noreply@oracle.com>
2010-02-24 16:24:33 +0000

    Main: Bstr makeover (third attempt) -- make Bstr(NULL) and Bstr() behave the same; resulting cleanup; make some more internal methods use Utf8Str instead of Bstr; fix a lot of CheckComArgNotNull??() usage
    svn:sync-xref-src-repo-rev: r58004

--- src/VBox/Main/RemoteUSBDeviceImpl.cpp
+++ src/VBox/Main/RemoteUSBDeviceImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: RemoteUSBDeviceImpl.cpp 57796 2010-02-17 12:24:34Z noreply@oracle.com $ */
+/* $Id: RemoteUSBDeviceImpl.cpp 58004 2010-02-24 16:24:33Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -141,7 +141,7 @@ STDMETHODIMP RemoteUSBDevice::COMGETTER(Id) (BSTR *aId)
     if (FAILED(autoCaller.rc())) return autoCaller.rc();
 
     /* this is const, no need to lock */
-    Bstr(mData.id).cloneTo(aId);
+    mData.id.toUtf16().detachTo(aId);
 
     return S_OK;
 }
