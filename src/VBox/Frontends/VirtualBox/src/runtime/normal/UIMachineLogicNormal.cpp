d1a4c144acf4a240095e878ff479722d6521510d
Christian Poetzsch <noreply@oracle.com>
2010-03-06 13:33:12 +0000

    FE/Qt4: new core: dock menu
    svn:sync-xref-src-repo-rev: r58469

--- src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineLogicNormal.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineLogicNormal.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineLogicNormal.cpp 58240 2010-03-02 15:08:43Z noreply@oracle.com $ */
+/* $Id: UIMachineLogicNormal.cpp 58469 2010-03-06 13:33:12Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -67,6 +67,11 @@ void UIMachineLogicNormal::initialize()
 
         /* Prepare normal machine window: */
         prepareMachineWindows();
+
+#ifdef Q_WS_MAC
+        /* Prepare dock: */
+        prepareDock();
+#endif /* Q_WS_MAC */
 
         /* Initialization: */
         sltMachineStateChanged();
