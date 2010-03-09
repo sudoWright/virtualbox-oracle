fdb7021056438ba2ec9f2a546166c83c715fb3bc
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2010-03-09 14:46:14 +0000

    Solaris/Installer: vboxconfig.sh wording.
    svn:sync-xref-src-repo-rev: r58596

--- src/VBox/Installer/solaris/vboxconfig.sh
+++ src/VBox/Installer/solaris/vboxconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: vboxconfig.sh 58130 2010-02-26 13:52:34Z ramshankar.venkataraman@oracle.com $
+# $Id: vboxconfig.sh 58596 2010-03-09 14:46:14Z ramshankar.venkataraman@oracle.com $
 
 # Sun VirtualBox
 # VirtualBox Configuration Script, Solaris host.
@@ -672,7 +672,7 @@ postinstall()
 
         return 0
     else
-        errorprint "Failed to update boot-archive"
+        errorprint "Failed to install drivers"
         exit 666
     fi
     return 1
