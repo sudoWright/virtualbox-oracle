187ac9c2bbcb02dd65ae7a78e767f8737561fcf5
Sander van Leeuwen <noreply@oracle.com>
2010-03-15 13:36:29 +0000

    Don't check page tables without any entries.
    svn:sync-xref-src-repo-rev: r58802

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 58684 2010-03-11 14:09:57Z noreply@oracle.com $ */
+/* $Id: PGMAllPool.cpp 58802 2010-03-15 13:36:29Z noreply@oracle.com $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -3447,7 +3447,8 @@ int pgmPoolTrackFlushGCPhysPTsSlow(PVM pVM, PPGMPAGE pPhysPage)
     while (--iPage >= PGMPOOL_IDX_FIRST)
     {
         PPGMPOOLPAGE pPage = &pPool->aPages[iPage];
-        if (pPage->GCPhys != NIL_RTGCPHYS)
+        if (    pPage->GCPhys != NIL_RTGCPHYS
+            &&  pPage->cPresent)
         {
             switch (pPage->enmKind)
             {
