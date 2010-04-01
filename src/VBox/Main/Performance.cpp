ab3cc09ac80c6564a778f6a57c6c580a3d34cf16
Sander van Leeuwen <noreply@oracle.com>
2010-04-01 11:56:23 +0000

    Wrong unit
    svn:sync-xref-src-repo-rev: r59631

--- src/VBox/Main/Performance.cpp
+++ src/VBox/Main/Performance.cpp
@@ -1,4 +1,4 @@
-/* $Id: Performance.cpp 59629 2010-04-01 11:07:21Z noreply@oracle.com $ */
+/* $Id: Performance.cpp 59631 2010-04-01 11:56:23Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -147,7 +147,7 @@ int CollectorGuestHAL::enable()
 
         ret = mConsole->COMGETTER(Guest)(mGuest.asOutParam());
         if (ret == S_OK)
-            mGuest->COMSETTER(StatisticsUpdateInterval)(1000 /* 1000 ms */);
+            mGuest->COMSETTER(StatisticsUpdateInterval)(1 /* 1 sec */);
     }
     return ret;
 }
