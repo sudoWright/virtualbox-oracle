8e33c39480f6da8d68999300d6adf43c90bc7526
Vasily Levchenko <noreply@oracle.com>
2010-04-07 11:00:07 +0000

    VBoxManage: typo.
    svn:sync-xref-src-repo-rev: r59748

--- src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageModifyVM.cpp 59682 2010-04-04 14:16:32Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxManageModifyVM.cpp 59748 2010-04-07 11:00:07Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of modifyvm command.
  */
@@ -219,7 +219,7 @@ static const RTGETOPTDEF g_aModifyVMOptions[] =
     { "--intnet",                   MODIFYVM_INTNET,                    RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
     { "--natnet",                   MODIFYVM_NATNET,                    RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
     { "--natbindip",                MODIFYVM_NATBINDIP,                 RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
-    { "--natsettings",              MODIFYVM_NATSETTINGS,               RTGETOPT_REQ_UINT32 | RTGETOPT_FLAG_INDEX },
+    { "--natsettings",              MODIFYVM_NATSETTINGS,               RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
     { "--natpf",                    MODIFYVM_NATPF,                     RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
     { "--nattftpprefix",            MODIFYVM_NATTFTPPREFIX,             RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
     { "--nattftpfile",              MODIFYVM_NATTFTPFILE,               RTGETOPT_REQ_STRING | RTGETOPT_FLAG_INDEX },
