0bb8a01f235c3e156e8d74c88f288400357aa072
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-05-17 15:15:02 +0000

    docs: clearification./
    svn:sync-xref-src-repo-rev: r61705

--- src/VBox/Devices/Builtins2.h
+++ src/VBox/Devices/Builtins2.h
@@ -1,6 +1,8 @@
-/* $Id: Builtins2.h 61663 2010-05-17 10:15:58Z knut.osmundsen@oracle.com $ */
+/* $Id: Builtins2.h 61705 2010-05-17 15:15:02Z knut.osmundsen@oracle.com $ */
 /** @file
  * Built-in drivers & devices part 2 header.
+ *
+ * These drivers and devices are in separate modules because of LGPL.
  */
 
 /*
