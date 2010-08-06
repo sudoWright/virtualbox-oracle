61f3bcbd885587873f854ee360c08ecb0ad85f79
Vasily Levchenko <noreply@oracle.com>
2010-08-06 13:13:43 +0000

    Audio/HDA: typo (wrong condition).
    svn:sync-xref-src-repo-rev: r64510

--- src/VBox/Devices/Audio/DevIchIntelHDA.cpp
+++ src/VBox/Devices/Audio/DevIchIntelHDA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevIchIntelHDA.cpp 64494 2010-08-06 04:55:19Z noreply@oracle.com $ */
+/* $Id: DevIchIntelHDA.cpp 64510 2010-08-06 13:13:43Z noreply@oracle.com $ */
 /** @file
  * DevIchIntelHD - VBox ICH Intel HD Audio Controller.
  */
@@ -583,8 +583,8 @@ static int hdaProcessInterrupt(INTELHDLinkState* pState)
    /* @todo add state change */
     if(   INTCTL_CIE(pState)
        && (   RIRBSTS_RINTFL(pState)
-           || RIRBSTS_RIRBOIS(pState))
-           || STATESTS(pState))
+           || RIRBSTS_RIRBOIS(pState)
+           || STATESTS(pState)))
     {
         fIrq = true;
     }
