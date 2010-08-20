61d3c5510cbbe990f7c7533ba1ab91f51d50de2f
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2010-08-20 13:55:37 +0000

    Runtime/r3/coredumper: rename.
    svn:sync-xref-src-repo-rev: r65022

--- src/VBox/Runtime/r3/solaris/coredumper-solaris.cpp
+++ src/VBox/Runtime/r3/solaris/coredumper-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTCoreDump.cpp 65021 2010-08-20 13:53:41Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: coredumper-solaris.cpp 65022 2010-08-20 13:55:37Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT Testcase - Core Dumper.
  */
--- src/VBox/Runtime/r3/solaris/coredumper-solaris.h
+++ src/VBox/Runtime/r3/solaris/coredumper-solaris.h
@@ -1,4 +1,4 @@
-/* $Id: tstRTCoreDump.h 65021 2010-08-20 13:53:41Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: coredumper-solaris.h 65022 2010-08-20 13:55:37Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT Testcase - Core dump, header.
  */
