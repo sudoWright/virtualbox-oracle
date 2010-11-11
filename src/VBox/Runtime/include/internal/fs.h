50c7ee361b8a7e413f44bd798f784c6354d1b667
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-11-11 17:16:37 +0000

    iprt: Working on tar vfs.
    svn:sync-xref-src-repo-rev: r67667

--- src/VBox/Runtime/include/internal/fs.h
+++ src/VBox/Runtime/include/internal/fs.h
@@ -1,4 +1,4 @@
-/* $Id: fs.h 60692 2010-04-27 08:22:32Z noreply@oracle.com $ */
+/* $Id: fs.h 67667 2010-11-11 17:16:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Internal RTFs header.
  */
@@ -42,6 +42,8 @@ bool    rtFsModeIsValidPermissions(RTFMODE fMode);
 
 #ifndef RT_OS_WINDOWS
 void    rtFsConvertStatToObjInfo(PRTFSOBJINFO pObjInfo, const struct stat *pStat, const char *pszName, unsigned cbName);
+void    rtFsObjInfoAttrSetUnixOwner(PRTFSOBJINFO pObjInfo, RTUID uid);
+void    rtFsObjInfoAttrSetUnixGroup(PRTFSOBJINFO pObjInfo, RTUID gid);
 #endif
 
 #ifdef RT_OS_LINUX
