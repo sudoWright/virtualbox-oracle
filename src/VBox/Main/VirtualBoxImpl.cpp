7f966fdecf53d4f43317a5b16818753c89a80cc9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-12-07 15:28:38 +0000

    ExtPackManager: Drop the drop zone idea.
    svn:sync-xref-src-repo-rev: r68605

--- src/VBox/Main/VirtualBoxImpl.cpp
+++ src/VBox/Main/VirtualBoxImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.cpp 68604 2010-12-07 15:28:17Z knut.osmundsen@oracle.com $ */
+/* $Id: VirtualBoxImpl.cpp 68605 2010-12-07 15:28:38Z knut.osmundsen@oracle.com $ */
 
 /** @file
  * Implementation of IVirtualBox in VBoxSVC.
@@ -503,7 +503,6 @@ HRESULT VirtualBox::init()
         /* extension manager */
         rc = unconst(m->ptrExtPackManager).createObject();
         if (SUCCEEDED(rc))
-            /** @todo Define drop zone location. */
             rc = m->ptrExtPackManager->initExtPackManager(this, VBOXEXTPACKCTX_PER_USER_DAEMON);
         if (FAILED(rc))
             throw rc;
