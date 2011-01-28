8f03b89988eeef5636ee956cfb76749b74bd1eeb
Alexander Eichner <alexander.eichner@oracle.com>
2011-01-28 14:52:18 +0000

    BusLogic: assertion to catch cases where the guest tries to send more mailboxes than it allocated space for during init
    svn:sync-xref-src-repo-rev: r69730

--- src/VBox/Devices/Storage/DevBusLogic.cpp
+++ src/VBox/Devices/Storage/DevBusLogic.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevBusLogic.cpp 69717 2011-01-28 12:29:38Z alexander.eichner@oracle.com $ */
+/* $Id: DevBusLogic.cpp 69730 2011-01-28 14:52:18Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox storage devices: BusLogic SCSI host adapter BT-958.
  */
@@ -1632,6 +1632,8 @@ static int buslogicRegisterWrite(PBUSLOGIC pBusLogic, unsigned iRegister, uint8_
             /* Fast path for mailbox execution command. */
             if ((uVal == BUSLOGICCOMMAND_EXECUTE_MAILBOX_COMMAND) && (pBusLogic->uOperationCode == 0xff))
             {
+                AssertMsg(pBusLogic->cMailboxesReady < pBusLogic->cMailbox,
+                          ("Mailbox count exceeded, max is %u\n", pBusLogic->cMailbox));
                 ASMAtomicIncU32(&pBusLogic->cMailboxesReady);
                 if (!ASMAtomicXchgBool(&pBusLogic->fNotificationSend, true))
                 {
