c083c7383672b7d1947245a6f1ac1546e844a557
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-03-03 14:12:20 +0000

    rem: stdio.h build fix, 2nd try.
    svn:sync-xref-src-repo-rev: r70329

--- src/recompiler/VBoxRecompiler.c
+++ src/recompiler/VBoxRecompiler.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxRecompiler.c 70326 2011-03-03 14:02:19Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxRecompiler.c 70329 2011-03-03 14:12:20Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Recompiler - QEMU.
  */
@@ -21,6 +21,7 @@
 *******************************************************************************/
 #define LOG_GROUP LOG_GROUP_REM
 #include "osdep.h"
+#include <stdio.h>      /* FILE */
 #include "exec-all.h"
 #include "config.h"
 #include "cpu-all.h"
