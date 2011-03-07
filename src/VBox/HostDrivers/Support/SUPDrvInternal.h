cd7f45397783b02e45a3d66b5331303388a52b86
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-03-07 16:30:28 +0000

    oops
    svn:sync-xref-src-repo-rev: r70395

--- src/VBox/HostDrivers/Support/SUPDrvInternal.h
+++ src/VBox/HostDrivers/Support/SUPDrvInternal.h
@@ -1,4 +1,4 @@
-/* $Revision: 70394 $ */
+/* $Revision: 70395 $ */
 /** @file
  * VirtualBox Support Driver - Internal header.
  */
@@ -168,7 +168,6 @@
  * @todo fix the mutex implementation on linux and make this the default. */
 # define SUPDRV_USE_MUTEX_FOR_GIP
 #endif
-#define SUPDRV_USE_MUTEX_FOR_LDR /* !!! DONT COMMIT THIS !!! */
 
 
 /**
