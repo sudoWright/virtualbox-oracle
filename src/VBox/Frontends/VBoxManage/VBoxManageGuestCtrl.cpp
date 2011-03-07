c5596d802d6e0cc89c0a3484bf264995b92b0181
Frank Mehnert <noreply@oracle.com>
2011-03-07 15:22:23 +0000

    VBoxManage: typo
    svn:sync-xref-src-repo-rev: r70393

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageGuestCtrl.cpp 70255 2011-02-28 14:34:48Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxManageGuestCtrl.cpp 70393 2011-03-07 15:22:23Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of guestcontrol command.
  */
@@ -125,7 +125,7 @@ void usageGuestControl(PRTSTREAM pStrm)
                  "                            [--dos2unix]\n"
                  "                            [--environment \"<NAME>=<VALUE> [<NAME>=<VALUE>]\"]\n"
                  "                            [--timeout <msec>] [--unix2dos] [--verbose]\n"
-                 "                            [--wait-exit] [--wait-stdout] [--wait-stdout]"
+                 "                            [--wait-exit] [--wait-stdout] [--wait-stdout]\n"
                  //"                          [--output-format=<dos>|<unix>]\n"
                  "                            [--output-type=<binary>|<text>]\n"
                  "                            [-- [<argument1>] ... [<argumentN>]\n"
