f0607357b417abe8656a2acf6c108ce313185a03
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-03-09 17:22:38 +0000

    GVMMR0.cpp: bound check when copying out statistics.
    svn:sync-xref-src-repo-rev: r70457

--- src/VBox/VMM/VMMR0/GVMMR0.cpp
+++ src/VBox/VMM/VMMR0/GVMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: GVMMR0.cpp 69206 2010-12-27 16:13:13Z knut.osmundsen@oracle.com $ */
+/* $Id: GVMMR0.cpp 70457 2011-03-09 17:22:38Z knut.osmundsen@oracle.com $ */
 /** @file
  * GVMM - Global VM Manager.
  */
@@ -2413,6 +2413,8 @@ GVMMR0DECL(int) GVMMR0QueryStatistics(PGVMMSTATS pStats, PSUPDRVSESSION pSession
             pStats->aHostCpus[iDstCpu].cStarts    = 0;
 #endif
             iDstCpu++;
+            if (iDstCpu >= RT_ELEMENTS(pStats->aHostCpus))
+                break;
         }
     }
     pStats->cHostCpus = iDstCpu;
