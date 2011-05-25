cfb97f83cb96ffb296fb01a5451ae4e2b9bfa302
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-05-25 11:37:52 +0000

    Some ASMAtomic*Size elimiation.
    svn:sync-xref-src-repo-rev: r71907

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -1,4 +1,4 @@
-/* $Revision: 71705 $ */
+/* $Revision: 71907 $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code.
  */
@@ -5139,8 +5139,7 @@ static DECLCALLBACK(void) supdrvGipMpEvent(RTMPEVENT enmEvent, RTCPUID idCpu, vo
      */
     if (enmEvent == RTMPEVENT_OFFLINE)
     {
-        RTCPUID idGipMaster;
-        ASMAtomicReadSize(&pDevExt->idGipMaster, &idGipMaster);
+        RTCPUID idGipMaster = ASMAtomicReadU32(&pDevExt->idGipMaster);
         if (idGipMaster == idCpu)
         {
             /*
