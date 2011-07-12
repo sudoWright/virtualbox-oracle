7f002567b540fa56dbad3c636b6f98c69e8a8a2a
Andreas Loeffler <andreas.loeffler@oracle.com>
2011-07-12 10:20:09 +0000

    Build fix.
    svn:sync-xref-src-repo-rev: r72785

--- src/VBox/Main/testcase/tstGuestCtrlParseBuffer.cpp
+++ src/VBox/Main/testcase/tstGuestCtrlParseBuffer.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstGuestCtrlParseBuffer.cpp 72783 2011-07-12 10:15:23Z andreas.loeffler@oracle.com $ */
+/* $Id: tstGuestCtrlParseBuffer.cpp 72785 2011-07-12 10:20:09Z andreas.loeffler@oracle.com $ */
 
 /** @file
  *
@@ -37,7 +37,7 @@ typedef std::map< RTCString, VBOXGUESTCTRL_BUFFER_VALUE >::const_iterator GuestB
 
 char pszUnterm1[] = { 'a', 's', 'd', 'f' };
 
-#ifndef RT_OS_WINDOWS
+#ifndef BYTE
 # define BYTE uint8_t
 #endif
 
