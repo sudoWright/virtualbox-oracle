90d2b52a9cb04eebdc1be91576a9f6101471a7a0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-07-13 23:12:42 +0000

    Alignment fixes for 32-bit build employing the electric fence.
    svn:sync-xref-src-repo-rev: r72846

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -1,4 +1,4 @@
-/* $Id: slirp.c 72615 2011-07-04 06:07:37Z noreply@oracle.com $ */
+/* $Id: slirp.c 72846 2011-07-13 23:12:42Z knut.osmundsen@oracle.com $ */
 /** @file
  * NAT - slirp glue.
  */
@@ -579,7 +579,7 @@ int slirp_init(PNATState *ppData, uint32_t u32NetAddr, uint32_t u32Netmask,
 {
     int fNATfailed = 0;
     int rc;
-    PNATState pData = RTMemAllocZ(sizeof(NATState));
+    PNATState pData = RTMemAllocZ(RT_ALIGN_Z(sizeof(NATState), sizeof(uint64_t)));
     *ppData = pData;
     if (!pData)
         return VERR_NO_MEMORY;
