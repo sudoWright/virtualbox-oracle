5d1367a5cb3314879376a58260f6b079a59a4b08
Vasily Levchenko <noreply@oracle.com>
2011-10-10 08:28:30 +0000

    NAT: configuration of limit of ICMP cache and moving icmp finilization and other ICMP's resources to ''icmp_finit'' routine.
    svn:sync-xref-src-repo-rev: r74342

--- src/VBox/Devices/Network/slirp/libslirp.h
+++ src/VBox/Devices/Network/slirp/libslirp.h
@@ -1,4 +1,4 @@
-/* $Id: libslirp.h 73063 2011-07-22 06:05:36Z noreply@oracle.com $ */
+/* $Id: libslirp.h 74342 2011-10-10 08:28:30Z noreply@oracle.com $ */
 /** @file
  * NAT - slirp interface.
  */
@@ -46,7 +46,7 @@ struct mbuf;
 extern "C" {
 #endif
 
-int slirp_init(PNATState *, uint32_t, uint32_t, bool, bool, int, void *);
+int slirp_init(PNATState *, uint32_t, uint32_t, bool, bool, int, int, void *);
 void slirp_register_statistics(PNATState pData, PPDMDRVINS pDrvIns);
 void slirp_deregister_statistics(PNATState pData, PPDMDRVINS pDrvIns);
 void slirp_term(PNATState);
