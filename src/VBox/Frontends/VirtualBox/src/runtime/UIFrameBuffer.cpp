2f5e9fbfd98b77efec33c8880511766141608e77
Michael Thayer <noreply@oracle.com>
2011-10-18 15:10:13 +0000

    FE/Qt: semantic refactoring and documentation: desktop geometry -> maximum guest size
    svn:sync-xref-src-repo-rev: r74427

--- src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBuffer.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBuffer.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIFrameBuffer.cpp 73323 2011-08-04 13:08:39Z noreply@oracle.com $ */
+/* $Id: UIFrameBuffer.cpp 74427 2011-10-18 15:10:13Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -183,7 +183,7 @@ STDMETHODIMP UIFrameBuffer::VideoModeSupported(ULONG uWidth, ULONG uHeight, ULON
     if (!pbSupported)
         return E_POINTER;
     *pbSupported = TRUE;
-    QSize screen = m_pMachineView->desktopGeometry();
+    QSize screen = m_pMachineView->maxGuestSize();
     if ((screen.width() != 0) && (uWidth > (ULONG)screen.width()))
         *pbSupported = FALSE;
     if ((screen.height() != 0) && (uHeight > (ULONG)screen.height()))
