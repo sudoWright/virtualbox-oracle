5976e99ff1c3ed75f157b2e9862d3bc69fc309f4
Sergey Dubov <sergey.dubov@oracle.com>
2011-11-08 10:31:52 +0000

    FE/Qt: 5804: Allow one operation on several VMs: VM details support for choosing several VMs, step 1.
    svn:sync-xref-src-repo-rev: r74751

--- src/VBox/Frontends/VirtualBox/src/extensions/QILabel.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QILabel.cpp
@@ -1,4 +1,4 @@
-/* $Id: QILabel.cpp 67140 2010-10-28 09:27:05Z noreply@oracle.com $ */
+/* $Id: QILabel.cpp 74751 2011-11-08 10:31:52Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -236,12 +236,6 @@ void QILabel::paintEvent (QPaintEvent *aEvent)
         option.initFrom (this);
         style()->drawPrimitive (QStyle::PE_FrameFocusRect, &option, &painter, this);
     }
-}
-
-void QILabel::showEvent(QShowEvent *pEvent)
-{
-    QLabel::showEvent(pEvent);
-    emit shown();
 }
 
 void QILabel::init()
