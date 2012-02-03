e7484604121396d5d36dba017176a41e1c386f16
Mikhail Sennikovsky <noreply@oracle.com>
2012-02-03 12:13:33 +0000

    wddm/3d: visible regions fixes for WPF apps
    svn:sync-xref-src-repo-rev: r76080

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.h
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPWddm.h 74010 2011-09-13 14:36:55Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxMPWddm.h 76080 2012-02-03 12:13:33Z noreply@oracle.com $ */
 /** @file
  * VBox WDDM Miniport driver
  */
@@ -26,6 +26,7 @@
 #endif
 #include "common/VBoxMPUtils.h"
 #include "common/VBoxMPDevExt.h"
+#include "../../common/VBoxVideoTools.h"
 
 //#define VBOXWDDM_DEBUG_VIDPN
 
