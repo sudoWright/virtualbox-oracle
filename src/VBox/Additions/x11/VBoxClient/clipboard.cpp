6c5f325d41703d3b6c3cbc4853bca69a3cd9c666
Michael Thayer <noreply@oracle.com>
2012-05-08 13:52:54 +0000

    Additions/x11: removed a dead file from VBoxClient/clipboard.
    svn:sync-xref-src-repo-rev: r77840

--- src/VBox/Additions/x11/VBoxClient/clipboard.cpp
+++ src/VBox/Additions/x11/VBoxClient/clipboard.cpp
@@ -1,4 +1,4 @@
-/** $Id: clipboard.cpp 72238 2011-06-14 13:14:57Z noreply@oracle.com $ */
+/** $Id: clipboard.cpp 77840 2012-05-08 13:52:54Z noreply@oracle.com $ */
 /** @file
  * Guest Additions - X11 Shared Clipboard.
  */
@@ -33,7 +33,6 @@
 #include <VBox/GuestHost/SharedClipboard.h>
 
 #include "VBoxClient.h"
-#include "clipboard.h"
 
 /****************************************************************************
 *   Global Variables                                                        *
