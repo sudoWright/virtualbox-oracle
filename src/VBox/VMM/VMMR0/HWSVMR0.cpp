35841e6e8add5f591db7f641df4d29d60b8808dc
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2012-07-05 12:10:53 +0000

    VMM: RDTSCP support on Intel. Segregated some common CPU features from the AMD superset into Extended features as they're now available on Intel too.
    svn:sync-xref-src-repo-rev: r78914

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWSVMR0.cpp 78835 2012-06-29 02:52:49Z knut.osmundsen@oracle.com $ */
+/* $Id: HWSVMR0.cpp 78914 2012-07-05 12:10:53Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM SVM (AMD-V) - Host Context Ring-0.
  */
@@ -2218,7 +2218,7 @@ ResumeExecution:
     case SVM_EXIT_RDTSCP:                /* Guest software attempted to execute RDTSCP. */
     {
         Log2(("SVM: Rdtscp\n"));
-        STAM_COUNTER_INC(&pVCpu->hwaccm.s.StatExitRdtsc);
+        STAM_COUNTER_INC(&pVCpu->hwaccm.s.StatExitRdtscp);
         rc = EMInterpretRdtscp(pVM, pVCpu, pCtx);
         if (rc == VINF_SUCCESS)
         {
