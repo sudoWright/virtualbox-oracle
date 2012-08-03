2774a8d9612e8f3e943f997c86bbd89cd6cc2df0
Sergey Dubov <sergey.dubov@oracle.com>
2012-08-03 01:42:21 +0000

    FE/Qt: 6234: Support for VM groups: Items sorting functionality.
    svn:sync-xref-src-repo-rev: r79740

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemMachine.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemMachine.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGChooserItemMachine.cpp 79736 2012-08-02 21:18:05Z sergey.dubov@oracle.com $ */
+/* $Id: UIGChooserItemMachine.cpp 79740 2012-08-03 01:42:21Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -232,6 +232,11 @@ void UIGChooserItemMachine::addItem(UIGChooserItem*, int)
 }
 
 void UIGChooserItemMachine::removeItem(UIGChooserItem*)
+{
+    AssertMsgFailed(("Machine graphics item do NOT support children!"));
+}
+
+void UIGChooserItemMachine::setItems(const QList<UIGChooserItem*>&, UIGChooserItemType)
 {
     AssertMsgFailed(("Machine graphics item do NOT support children!"));
 }
