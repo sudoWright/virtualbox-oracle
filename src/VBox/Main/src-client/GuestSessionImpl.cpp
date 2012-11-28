fb1722f0927634b13e3aeaa24aa0a9475fd46486
Andreas Loeffler <andreas.loeffler@oracle.com>
2012-11-28 11:44:04 +0000

    Guest Control: Respect detached processes.
    svn:sync-xref-src-repo-rev: r82354

--- src/VBox/Main/src-client/GuestSessionImpl.cpp
+++ src/VBox/Main/src-client/GuestSessionImpl.cpp
@@ -1,5 +1,5 @@
 
-/* $Id: GuestSessionImpl.cpp 81055 2012-10-01 13:33:30Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: GuestSessionImpl.cpp 82354 2012-11-28 11:44:04Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox Main - XXX.
  */
@@ -847,6 +847,15 @@ int GuestSession::processCreateExInteral(GuestProcessStartupInfo &procInfo, ComO
         {
             return VERR_INVALID_PARAMETER;
         }
+    }
+
+    if (   (procInfo.mFlags & ProcessCreateFlag_WaitForProcessStartOnly)
+        && (   (procInfo.mFlags & ProcessCreateFlag_WaitForStdOut)
+            || (procInfo.mFlags & ProcessCreateFlag_WaitForStdErr)
+           )
+       )
+    {
+        return VERR_INVALID_PARAMETER;
     }
 
     /* Adjust timeout. If set to 0, we define
