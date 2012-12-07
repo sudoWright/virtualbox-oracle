1e705fb305e7418f9dc72ecb5b8b4ad939a06ce9
Sergey Dubov <sergey.dubov@oracle.com>
2012-12-07 15:10:02 +0000

    FE/Qt: VM group UI: Fixing issue with anti-aliased text trimming.
    svn:sync-xref-src-repo-rev: r82550

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGDetailsElement.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGDetailsElement.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGDetailsElement.cpp 82546 2012-12-07 13:43:35Z sergey.dubov@oracle.com $ */
+/* $Id: UIGDetailsElement.cpp 82550 2012-12-07 15:10:02Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -565,9 +565,11 @@ void UIGDetailsElement::paintElementInfo(QPainter *pPainter, const QStyleOptionG
     paintText(/* Painter: */
               pPainter,
               /* Rectangle to paint in: */
-              QRect(QPoint(iMachineNameX, iMachineNameY), m_nameSize),
+              QPoint(iMachineNameX, iMachineNameY),
               /* Font to paint text: */
               m_nameFont,
+              /* Paint device: */
+              model()->paintDevice(),
               /* Text to paint: */
               m_strName,
               /* Name hovered? */
