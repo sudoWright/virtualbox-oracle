f083624d152c05057d3345edd898a17f775e6e09
Sergey Dubov <sergey.dubov@oracle.com>
2013-01-28 16:48:34 +0000

    FE/Qt: Reworking shortcut providers into singleton shortcut-pool. Integrate shortcut-pool with corresponding action-pools.
    svn:sync-xref-src-repo-rev: r83417

--- src/VBox/Frontends/VirtualBox/src/selector/VBoxSnapshotsWgt.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/VBoxSnapshotsWgt.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSnapshotsWgt.cpp 82224 2012-11-21 00:21:24Z sergey.dubov@oracle.com $ */
+/* $Id: VBoxSnapshotsWgt.cpp 83417 2013-01-28 16:48:34Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -38,7 +38,6 @@
 #include "UIWizardCloneVM.h"
 #include "UIToolBar.h"
 #include "UIVirtualBoxEventHandler.h"
-#include "UISelectorShortcuts.h"
 #include "UIConverter.h"
 
 /* COM includes: */
