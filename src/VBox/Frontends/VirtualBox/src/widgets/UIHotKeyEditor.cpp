797164c42d97754283be2d0b3ae80a64b8a28076
Sergey Dubov <sergey.dubov@oracle.com>
2013-02-18 14:05:17 +0000

    FE/Qt: UIHotKeyEditor: Few NLS fixes.
    svn:sync-xref-src-repo-rev: r83839

--- src/VBox/Frontends/VirtualBox/src/widgets/UIHotKeyEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIHotKeyEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIHotKeyEditor.cpp 83834 2013-02-18 13:12:52Z sergey.dubov@oracle.com $ */
+/* $Id: UIHotKeyEditor.cpp 83839 2013-02-18 14:05:17Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -156,8 +156,8 @@ void UIHotKeyEditor::sltClear()
 
 void UIHotKeyEditor::retranslateUi()
 {
-    m_pResetButton->setToolTip(tr("Reset to Default"));
-    m_pClearButton->setToolTip(tr("Unset Shortcut"));
+    m_pResetButton->setToolTip(tr("Reset shortcut to default"));
+    m_pClearButton->setToolTip(tr("Unset shortcut"));
 }
 
 bool UIHotKeyEditor::eventFilter(QObject *pWatched, QEvent *pEvent)
