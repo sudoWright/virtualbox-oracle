a48382d42d5d57fdbe68703b8cc806980e1d4089
Sergey Dubov <sergey.dubov@oracle.com>
2013-03-12 15:16:43 +0000

    FE/Qt: Selector UI: Update vertical scroll-bars on window resize.
    svn:sync-xref-src-repo-rev: r84238

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGDetailsView.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGDetailsView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGDetailsView.cpp 83576 2013-02-04 15:54:15Z noreply@oracle.com $ */
+/* $Id: UIGDetailsView.cpp 84238 2013-03-12 15:16:43Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -81,8 +81,10 @@ void UIGDetailsView::preparePalette()
     setPalette(pal);
 }
 
-void UIGDetailsView::resizeEvent(QResizeEvent*)
+void UIGDetailsView::resizeEvent(QResizeEvent *pEvent)
 {
+    /* Call to base-class: */
+    QGraphicsView::resizeEvent(pEvent);
     /* Notify listeners: */
     emit sigResized();
 }
