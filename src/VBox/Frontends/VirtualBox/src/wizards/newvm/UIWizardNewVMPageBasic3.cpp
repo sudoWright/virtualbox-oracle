aaaec794ea4c76b9c37c50b5583ffc540070fad3
Sergey Dubov <sergey.dubov@oracle.com>
2013-03-26 14:00:34 +0000

    FE/Qt: Some progress-dialog cleanup.
    svn:sync-xref-src-repo-rev: r84539

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic3.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic3.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMPageBasic3.cpp 80922 2012-09-25 09:01:47Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardNewVMPageBasic3.cpp 84539 2013-03-26 14:00:34Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -112,7 +112,7 @@ void UIWizardNewVMPage3::ensureNewVirtualDiskDeleted()
     /* 2nd step: show delete-storage progress: */
     if (fSuccess)
     {
-        msgCenter().showModalProgressDialog(progress, thisImp()->windowTitle(), ":/progress_media_delete_90px.png", thisImp(), true);
+        msgCenter().showModalProgressDialog(progress, thisImp()->windowTitle(), ":/progress_media_delete_90px.png", thisImp());
         fSuccess = progress.isOk() && progress.GetResultCode() == S_OK;
     }
 
