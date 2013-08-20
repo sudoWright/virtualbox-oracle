c5dc6db4dec6760cde3727321d85a7f3100dd66d
Frank Mehnert <noreply@oracle.com>
2013-08-20 11:55:24 +0000

    NetworkServices/NAT: renamed old VBoxNetNAT to VBoxNetSlirpNAT
    svn:sync-xref-src-repo-rev: r88200

--- src/VBox/NetworkServices/NAT/VBoxNetSlirpNAT.cpp
+++ src/VBox/NetworkServices/NAT/VBoxNetSlirpNAT.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetNAT.cpp 87059 2013-07-07 19:20:36Z noreply@oracle.com $ */
+/* $Id: VBoxNetSlirpNAT.cpp 88200 2013-08-20 11:55:24Z noreply@oracle.com $ */
 /** @file
  * VBoxNetNAT - NAT Service for connecting to IntNet.
  */
