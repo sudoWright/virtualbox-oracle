71f69517cc18b0d4cd2e7b527e732b674bac6148
Valery Ushakov <noreply@oracle.com>
2013-09-11 14:14:37 +0000

    ConfigEntity::ConfigEntity: init m_u32ExpirationPeriod
    svn:sync-xref-src-repo-rev: r88880

--- src/VBox/NetworkServices/DHCP/Config.h
+++ src/VBox/NetworkServices/DHCP/Config.h
@@ -1,4 +1,4 @@
-/* $Id: Config.h 88862 2013-09-11 05:04:47Z noreply@oracle.com $ */
+/* $Id: Config.h 88880 2013-09-11 14:14:37Z noreply@oracle.com $ */
 /**
  * This file contains declarations of DHCP config.
  */
@@ -229,7 +229,8 @@ class ConfigEntity: public BaseConfigEntity
                  int matchingLevel = 0):
       BaseConfigEntity(criteria, matchingLevel),
       m_name(name),
-      m_parentCfg(cfg)
+      m_parentCfg(cfg),
+      m_u32ExpirationPeriod(0)
     {
         unconst(m_parentCfg)->add(this);
     }
