3d5f6a0ae78f99e85bf3bb16740be9d8b4adaaf1
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-10-31 11:49:09 +0000

    Disas: pmulhrw adjustment, seems it wasn't fixed in v1.2.0.0 but in git some time afterwards. Try check for build/patchlevel greater than 0 in addition to existing v1.2.0 requirement.
    svn:sync-xref-src-repo-rev: r90354

--- src/VBox/Disassembler/testcase/tstDisasm-1A.asm
+++ src/VBox/Disassembler/testcase/tstDisasm-1A.asm
@@ -1,4 +1,4 @@
-; $Id: tstDisasm-1A.asm 86825 2013-06-28 12:22:45Z knut.osmundsen@oracle.com $
+; $Id: tstDisasm-1A.asm 90354 2013-10-31 11:49:09Z knut.osmundsen@oracle.com $
 ;; @file
 ; VBox disassembler: Assembler test routines
 ;
@@ -26,7 +26,7 @@
 
 BITS 32
 
-%if __YASM_MAJOR__ > 1 || (__YASM_MAJOR__ == 1 && __YASM_MINOR__ >= 2)
+%if __YASM_VERSION_ID__ >= 001020001h ; v1.2.0.1 and greater, make sure to exclude v1.2.0.0.
  %define pmulhrwa pmulhrw
 %endif
 
