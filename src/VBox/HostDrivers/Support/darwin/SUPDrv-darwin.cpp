d4e7d5e2f47f0dc8a8934286f2d9962814763c86
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2013-12-17 14:49:31 +0000

    Mac OS X host: HID LEDs sync: avoid system.log flooding on 10.8 host.
    svn:sync-xref-src-repo-rev: r91368

--- src/VBox/HostDrivers/Support/darwin/SUPDrv-darwin.cpp
+++ src/VBox/HostDrivers/Support/darwin/SUPDrv-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-darwin.cpp 91300 2013-12-15 23:21:17Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrv-darwin.cpp 91368 2013-12-17 14:49:31Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Support Driver - Darwin Specific Code.
  */
@@ -1166,7 +1166,8 @@ int VBOXCALL    supdrvDarwinResumeBuiltinKbd(void)
         if (pIter)
         {
             while ((pDriver = (IOUSBHIDDriver *)pIter->getNextObject()))
-                pDriver->SuspendPort(false, 0);
+                if (pDriver->IsPortSuspended())
+                    pDriver->SuspendPort(false, 0);
 
             pIter->release();
         }
