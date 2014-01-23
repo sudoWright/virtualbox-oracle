abe6118c63f511ee51109109febf816b20709304
Frank Mehnert <noreply@oracle.com>
2014-01-23 09:22:22 +0000

    Main/src-server: unify {p,m}VBox => {p,m}VirtualBox
    svn:sync-xref-src-repo-rev: r91738

--- src/VBox/Main/include/ApplianceImpl.h
+++ src/VBox/Main/include/ApplianceImpl.h
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImpl.h 91067 2013-12-03 17:57:11Z noreply@oracle.com $ */
+/* $Id: ApplianceImpl.h 91738 2014-01-23 09:22:22Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -108,7 +108,7 @@ private:
     HRESULT getWarnings(std::vector<com::Utf8Str> &aWarnings);
 
     /** weak VirtualBox parent */
-    VirtualBox* const   mVirtualBox;
+    VirtualBox* const mVirtualBox;
 
     struct ImportStack;
     struct TaskOVF;
