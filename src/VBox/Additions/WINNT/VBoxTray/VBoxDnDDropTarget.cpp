0e52e9cc7c97062b9176d2ae2e6081d04913c710
Andreas Loeffler <andreas.loeffler@oracle.com>
2014-03-10 13:54:03 +0000

    DnD: Bugfixes.
    svn:sync-xref-src-repo-rev: r92721

--- src/VBox/Additions/WINNT/VBoxTray/VBoxDnDDropTarget.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxDnDDropTarget.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDnDDropTarget.cpp 92719 2014-03-10 09:37:10Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxDnDDropTarget.cpp 92721 2014-03-10 13:54:03Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxDnDTarget.cpp - IDropTarget implementation.
  */
@@ -463,7 +463,6 @@ STDMETHODIMP VBoxDnDDropTarget::Drop(IDataObject *pDataObject,
                             {
                                 RTCString strRoot = lstURI.RootToString();
                                 size_t cbRoot = strRoot.length() + 1; /* Include termination */
-                                Assert(cbRoot);
 
                                 mpvData = RTMemAlloc(cbRoot);
                                 if (mpvData)
