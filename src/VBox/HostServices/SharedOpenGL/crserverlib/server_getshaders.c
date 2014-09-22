266bbe77f66db9da9a64977a81f19e47a616cbaf
Mikhail Sennikovsky <noreply@oracle.com>
2014-09-22 08:40:59 +0000

    crOpenGL: typo fix
    svn:sync-xref-src-repo-rev: r96203

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_getshaders.c
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_getshaders.c
@@ -1,4 +1,4 @@
-/* $Id: server_getshaders.c 93582 2014-05-06 17:21:16Z noreply@oracle.com $ */
+/* $Id: server_getshaders.c 96203 2014-09-22 08:40:59Z noreply@oracle.com $ */
 
 /** @file
  * VBox OpenGL GLSL related get functions
@@ -89,7 +89,7 @@ void SERVER_DISPATCH_APIENTRY crServerDispatchGetAttachedShaders(GLuint program,
         GLsizei i;
         GLuint *ids=(GLuint*)&pLocal[1];
 
-        for (i=0; i<*pLocal; ++i);
+        for (i=0; i<*pLocal; ++i)
           ids[i] = crStateGLSLShaderHWIDtoID(ids[i]);
     }
 
