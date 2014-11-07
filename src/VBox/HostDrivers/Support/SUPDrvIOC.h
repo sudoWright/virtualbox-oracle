3494c4fb4e7f1f018d1ee58e8dc8f7bf3ca71f6c
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2014-11-07 17:41:01 +0000

    HostDrivers/Support: Enabled global TSC rate calculation and implemented TSC calibration over a longer interval for invariant hosts.
    svn:sync-xref-src-repo-rev: r96841

--- src/VBox/HostDrivers/Support/SUPDrvIOC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIOC.h
@@ -1,4 +1,4 @@
-/* $Revision: 96760 $ */
+/* $Revision: 96841 $ */
 /** @file
  * VirtualBox Support Driver - IOCtl definitions.
  */
@@ -214,7 +214,7 @@ typedef SUPREQHDR *PSUPREQHDR;
  * @todo Pending work on next major version change:
  *          - (none).
  */
-#define SUPDRV_IOC_VERSION                              0x001c0001
+#define SUPDRV_IOC_VERSION                              0x001d0000
 
 /** SUP_IOCTL_COOKIE. */
 typedef struct SUPCOOKIE
