5752438c44ddddfc843204bb1c647a50d426d709
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-03-20 15:17:42 +0000

    SUPR0QueryVTCaps: Added SUPVTCAPS_VTX_UNRESTRICTED_GUEST return flag. Increased the minor SUPDrv version number.
    svn:sync-xref-src-repo-rev: r99089

--- src/VBox/HostDrivers/Support/SUPDrvIOC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIOC.h
@@ -1,4 +1,4 @@
-/* $Revision: 98797 $ */
+/* $Revision: 99089 $ */
 /** @file
  * VirtualBox Support Driver - IOCtl definitions.
  */
@@ -214,7 +214,7 @@ typedef SUPREQHDR *PSUPREQHDR;
  * @todo Pending work on next major version change:
  *          - (nothing)
  */
-#define SUPDRV_IOC_VERSION                              0x001f0004
+#define SUPDRV_IOC_VERSION                              0x001f0005
 
 /** SUP_IOCTL_COOKIE. */
 typedef struct SUPCOOKIE
