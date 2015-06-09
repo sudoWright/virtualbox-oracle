1f1d0ff70402e8ccc61b7ffcca2a7aa527887a4d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-06-09 22:36:56 +0000

    *bldprogs: Updated (C) year.
    svn:sync-xref-src-repo-rev: r100896

--- src/bldprogs/scmdiff.cpp
+++ src/bldprogs/scmdiff.cpp
@@ -1,10 +1,10 @@
-/* $Id: scmdiff.cpp 96246 2014-09-24 13:02:36Z knut.osmundsen@oracle.com $ */
+/* $Id: scmdiff.cpp 100896 2015-06-09 22:36:56Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase / Tool - Source Code Massager.
  */
 
 /*
- * Copyright (C) 2010-2012 Oracle Corporation
+ * Copyright (C) 2010-2015 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
