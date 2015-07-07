62e47eb61c20e4ac6ea201748c4d1ad1f43ff65f
Michael Thayer <noreply@oracle.com>
2015-07-07 09:16:36 +0000

    Additions/linux/installer: no run-level 7.
    svn:sync-xref-src-repo-rev: r101482

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# Linux Additions kernel module init script ($Revision: 101396 $)
+# Linux Additions kernel module init script ($Revision: 101482 $)
 #
 
 #
@@ -16,7 +16,7 @@
 #
 
 
-# chkconfig: 3457 30 70
+# chkconfig: 345 30 70
 # description: VirtualBox Linux Additions kernel modules
 #
 ### BEGIN INIT INFO
