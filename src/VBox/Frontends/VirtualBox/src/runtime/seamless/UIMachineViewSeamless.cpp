ad1c65c4a309cedcfb914a1e491552120fc5b1d8
Michael Thayer <noreply@oracle.com>
2015-08-21 12:50:43 +0000

    FE/Qt: better fix for temporary over-sized windows when exiting full-screen and seamless.
    svn:sync-xref-src-repo-rev: r102275

--- src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineViewSeamless.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineViewSeamless.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineViewSeamless.cpp 102257 2015-08-20 17:08:03Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineViewSeamless.cpp 102275 2015-08-21 12:50:43Z noreply@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineViewSeamless class implementation.
  */
@@ -216,6 +216,8 @@ void UIMachineViewSeamless::adjustGuestScreenSize()
     {
         frameBuffer()->setAutoEnabled(false);
         sltPerformGuestResize(workingArea().size());
+        /* And remember the size to know what we are resizing out of when we exit: */
+        uisession()->setLastFullScreenSize(screenId(), workingArea().size());
     }
 }
 
