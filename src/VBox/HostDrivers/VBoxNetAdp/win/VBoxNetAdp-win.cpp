64685e9733a952c2d39ebcdf4ad0cdb6eb5eb0f4
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2015-09-11 08:23:55 +0000

    NetAdp/Win: free adapter memory on halt (#8033)
    svn:sync-xref-src-repo-rev: r102613

--- src/VBox/HostDrivers/VBoxNetAdp/win/VBoxNetAdp-win.cpp
+++ src/VBox/HostDrivers/VBoxNetAdp/win/VBoxNetAdp-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetAdp-win.cpp 101219 2015-06-23 08:37:03Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetAdp-win.cpp 102613 2015-09-11 08:23:55Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBoxNetAdp-win.cpp - NDIS6 Host-only Networking Driver, Windows-specific code.
  */
@@ -325,6 +325,8 @@ DECLHIDDEN(VOID) vboxNetAdpWinHaltEx(IN NDIS_HANDLE MiniportAdapterContext,
     PVBOXNETADP_ADAPTER pAdapter = (PVBOXNETADP_ADAPTER)MiniportAdapterContext;
     LogFlow(("==>vboxNetAdpWinHaltEx\n"));
     // TODO: Stop something?
+    if (pAdapter)
+        vboxNetAdpWinFreeAdapter(pAdapter);
     LogFlow(("<==vboxNetAdpWinHaltEx\n"));
 }
 
