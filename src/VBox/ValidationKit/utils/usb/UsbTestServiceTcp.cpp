d9cb99495b1f89eccbabc3613a982e4c8d591bd1
Frank Mehnert <noreply@oracle.com>
2016-04-01 06:39:20 +0000

    build fix
    svn:sync-xref-src-repo-rev: r106322

--- src/VBox/ValidationKit/utils/usb/UsbTestServiceTcp.cpp
+++ src/VBox/ValidationKit/utils/usb/UsbTestServiceTcp.cpp
@@ -1,4 +1,4 @@
-/* $Id: UsbTestServiceTcp.cpp 106320 2016-03-31 18:57:37Z alexander.eichner@oracle.com $ */
+/* $Id: UsbTestServiceTcp.cpp 106322 2016-04-01 06:39:20Z noreply@oracle.com $ */
 /** @file
  * UsbTestService - Remote USB test configuration and execution server, TCP/IP Transport Layer.
  */
@@ -106,7 +106,7 @@ static void utsTcpDisconnectClient(PUTSTRANSPORTCLIENT pClient)
 /**
  * @interface_method_impl{UTSTRANSPORT,pfnWaitForConnect}
  */
-static int utsTcpWaitForConnect(PPUTSTRANSPORTCLIENT ppClientNew)
+static DECLCALLBACK(int) utsTcpWaitForConnect(PPUTSTRANSPORTCLIENT ppClientNew)
 {
     int rc;
     RTSOCKET hClientNew;
