85d33f47f8a5b1365ca67fc3d0cbae532c01486b
Michael Thayer <noreply@oracle.com>
2016-05-10 18:50:49 +0000

    bugref:8087: Additions/x11: support non-root X server: prevent the kernel module from loading if the X server version is less than 1.17.
    svn:sync-xref-src-repo-rev: r107150

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# Linux Additions X11 setup init script ($Revision: 106425 $)
+# Linux Additions X11 setup init script ($Revision: 107150 $)
 #
 
 #
@@ -372,6 +372,17 @@ setup()
             # For anything else, assume kernel drivers.
             dox11config=""
             ;;
+    esac
+    case "${x_version}" in
+    4.* | 6.* | 7.* | 1.?.* | 1.1[0-6].* )
+        echo "blacklist vboxvideo" > /etc/modprobe.d/blacklist-vboxvideo.conf
+        ;;
+    *)
+        if test -f /etc/modprobe.d/blacklist-vboxvideo.conf; then
+            rm -f /etc/modprobe.d/blacklist-vboxvideo.conf
+            ${MODPROBE} vboxvideo
+        fi
+        ;;
     esac
     test -n "${dox11config}" &&
         begin "Installing $xserver_version modules"
