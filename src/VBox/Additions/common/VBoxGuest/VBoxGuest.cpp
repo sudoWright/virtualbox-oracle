5dfb87b304144d51e789a94616c240ff460ce7ba
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-07-22 19:16:33 +0000

    (C) 2016
    svn:sync-xref-src-repo-rev: r109079

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
@@ -1,10 +1,10 @@
-/* $Id: VBoxGuest.cpp 105273 2016-01-28 08:28:40Z noreply@oracle.com $ */
+/* $Id: VBoxGuest.cpp 109079 2016-07-22 19:16:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuest - Guest Additions Driver, Common Code.
  */
 
 /*
- * Copyright (C) 2007-2015 Oracle Corporation
+ * Copyright (C) 2007-2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
