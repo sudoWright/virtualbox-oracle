e918c72acc927e412b116b989c25cb53a00d70dc
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-08-01 09:44:15 +0000

    GuestCtrl/Service: Removed gctrl.cpp+gctrl.h as we didn't use anything in them anymore.
    svn:sync-xref-src-repo-rev: r109388

--- src/VBox/HostServices/GuestControl/service.cpp
+++ src/VBox/HostServices/GuestControl/service.cpp
@@ -1,4 +1,4 @@
-/* $Id: service.cpp 109387 2016-08-01 09:41:36Z knut.osmundsen@oracle.com $ */
+/* $Id: service.cpp 109388 2016-08-01 09:44:15Z knut.osmundsen@oracle.com $ */
 /** @file
  * Guest Control Service: Controlling the guest.
  */
@@ -82,7 +82,6 @@
 #include <string>
 #include <list>
 
-#include "gctrl.h"
 
 namespace guestControl {
 
