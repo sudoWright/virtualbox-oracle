060c8670a65cfc3d53a75e7ca7eae6a8b2521518
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-08-01 21:24:06 +0000

    put const after so you can follow our normal continuation indent rule.
    svn:sync-xref-src-repo-rev: r109442

--- src/VBox/HostServices/GuestControl/service.cpp
+++ src/VBox/HostServices/GuestControl/service.cpp
@@ -1,4 +1,4 @@
-/* $Id: service.cpp 109388 2016-08-01 09:44:15Z knut.osmundsen@oracle.com $ */
+/* $Id: service.cpp 109442 2016-08-01 21:24:06Z knut.osmundsen@oracle.com $ */
 /** @file
  * Guest Control Service: Controlling the guest.
  */
@@ -790,8 +790,8 @@ typedef struct ClientState
         return rc;
     }
 
-    int SendReply(const ClientConnection *pConnection,
-                        HostCommand      *pHostCmd)
+    int SendReply(ClientConnection const *pConnection,
+                  HostCommand            *pHostCmd)
     {
         AssertPtrReturn(pConnection, VERR_INVALID_POINTER);
         AssertPtrReturn(pHostCmd, VERR_INVALID_POINTER);
