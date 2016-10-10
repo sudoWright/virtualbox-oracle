c72c9be0199159a0d7a12032d2fe97f34d810624
Michael Thayer <noreply@oracle.com>
2016-10-10 10:25:32 +0000

    bugref:8614: Additions/common/VBoxVideo: make the code more self-contained: add missing header file to VBoxDispMpLogger.cpp (WDDM driver) which was revealed by other code adjustments.
    svn:sync-xref-src-repo-rev: r111173

--- src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/VBoxDispMpLogger.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/VBoxDispMpLogger.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDispMpLogger.cpp 109631 2016-08-04 23:41:09Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxDispMpLogger.cpp 111173 2016-10-10 10:25:32Z noreply@oracle.com $ */
 
 /** @file
  * VBox WDDM Display backdoor logger implementation
@@ -55,6 +55,7 @@
 
 #include <iprt/asm.h>
 #include <iprt/assert.h>
+#include <iprt/err.h>
 #include <iprt/mem.h>
 
 #include <stdio.h>
