7329fb7c224ab0812ccc054f9061b400181a76a1
Sergey Dubov <sergey.dubov@oracle.com>
2016-11-15 14:15:13 +0000

    FE/Qt: bugref:8536: VM Settings: USB page: Allow the hexadecimal revision input for USB filters.
    svn:sync-xref-src-repo-rev: r111936

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsUSBFilterDetails.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsUSBFilterDetails.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsUSBFilterDetails.cpp 109051 2016-07-22 18:44:18Z knut.osmundsen@oracle.com $ */
+/* $Id: UIMachineSettingsUSBFilterDetails.cpp 111936 2016-11-15 14:15:13Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsUSBFilterDetails class implementation.
  */
@@ -39,7 +39,7 @@ UIMachineSettingsUSBFilterDetails::UIMachineSettingsUSBFilterDetails(QWidget *pP
     mLeName->setValidator (new QRegExpValidator (QRegExp (".+"), this));
     mLeVendorID->setValidator (new QRegExpValidator (QRegExp ("[0-9a-fA-F]{0,4}"), this));
     mLeProductID->setValidator (new QRegExpValidator (QRegExp ("[0-9a-fA-F]{0,4}"), this));
-    mLeRevision->setValidator (new QRegExpValidator (QRegExp ("[0-9]{0,4}"), this));
+    mLeRevision->setValidator (new QRegExpValidator (QRegExp ("[0-9a-fA-F]{0,4}"), this));
     mLePort->setValidator (new QRegExpValidator (QRegExp ("[0-9]*"), this));
 
     /* Applying language settings */
