cfa475451a137d93f359fb8eccb450cc69dfd502
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2016-12-01 12:28:44 +0000

    VMM/DBGF, HM: Fix int3 based breakpoints set in the VM debugger when using VT-x.
    svn:sync-xref-src-repo-rev: r112120

--- src/VBox/VMM/VMMAll/DBGFAll.cpp
+++ src/VBox/VMM/VMMAll/DBGFAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFAll.cpp 109034 2016-07-22 18:29:06Z knut.osmundsen@oracle.com $ */
+/* $Id: DBGFAll.cpp 112120 2016-12-01 12:28:44Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, All Context Code.
  */
@@ -149,6 +149,19 @@ VMM_INT_DECL(bool) DBGFBpIsHwArmed(PVM pVM)
 VMM_INT_DECL(bool) DBGFBpIsHwIoArmed(PVM pVM)
 {
     return pVM->dbgf.s.cEnabledHwIoBreakpoints > 0;
+}
+
+
+/**
+ * Checks if any INT3 breakpoints are armed.
+ *
+ * @returns true if armed, false if not.
+ * @param   pVM        The cross context VM structure.
+ * @remarks Don't call this from CPUMRecalcHyperDRx!
+ */
+VMM_INT_DECL(bool) DBGFBpIsInt3Armed(PVM pVM)
+{
+    return pVM->dbgf.s.cEnabledInt3Breakpoints > 0;
 }
 
 
