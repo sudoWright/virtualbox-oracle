2b21ff4574279f7ad06df6f43f6a43e451ce623a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-01-04 10:07:11 +0000

    TestManager: a todo
    svn:sync-xref-src-repo-rev: r112598

--- src/VBox/ValidationKit/testmanager/core/testresults.py
+++ src/VBox/ValidationKit/testmanager/core/testresults.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: testresults.py 112596 2017-01-04 02:49:19Z knut.osmundsen@oracle.com $
+# $Id: testresults.py 112598 2017-01-04 10:07:11Z knut.osmundsen@oracle.com $
 # pylint: disable=C0302
 
 ## @todo Rename this file to testresult.py!
@@ -29,7 +29,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 112596 $"
+__version__ = "$Revision: 112598 $"
 # Standard python imports.
 import unittest;
 
@@ -748,6 +748,8 @@ class TestResultFilter(ModelFilterBase):
         oCrit = FilterCriterion('Failure reasons', sVarNm = 'fr', sTable = 'TestResultFailures', sColumn = 'idFailureReason');
         self.aCriteria.append(oCrit);
         assert self.aCriteria[self.kiFailReasons] is oCrit;
+
+        ## @todo Add build type, error count, gang counts (if any > 1)...
 
     kdMiscConditions = {
         kiMisc_NestedPaging:    'TestBoxesWithStrings.fCpuNestedPaging IS TRUE',
