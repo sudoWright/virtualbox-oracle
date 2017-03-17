3d35fa82009f7a50059ed4c8f698b2ca92b6a848
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-03-17 22:42:52 +0000

    IEM: Mark instructions allowing lock prefix use (incomplete).
    svn:sync-xref-src-repo-rev: r114069

--- src/VBox/VMM/VMMAll/IEMAllInstructionsPython.py
+++ src/VBox/VMM/VMMAll/IEMAllInstructionsPython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: IEMAllInstructionsPython.py 114068 2017-03-17 22:16:24Z knut.osmundsen@oracle.com $
+# $Id: IEMAllInstructionsPython.py 114069 2017-03-17 22:42:52Z knut.osmundsen@oracle.com $
 
 """
 IEM instruction extractor.
@@ -31,7 +31,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 114068 $"
+__version__ = "$Revision: 114069 $"
 
 # pylint: disable=anomalous-backslash-in-string
 
@@ -343,6 +343,7 @@ g_kdHints = {
     'mmx':                   'DISOPTYPE_MMX',                   ##< MMX,MMXExt,3DNow,++ instruction. Not implemented yet!
     'fpu':                   'DISOPTYPE_FPU',                   ##< FPU instruction. Not implemented yet!
     'ignores_op_size':       '',                                ##< Ignores both operand size prefixes.
+    'lock_allowed':          '',                                ##< Lock prefix allowed.
 };
 
 
