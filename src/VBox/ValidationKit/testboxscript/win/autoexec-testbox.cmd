3bc8b7d11e96d3eeeb6898273f5b1702f220613b
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2017-05-04 16:13:23 +0000

    testboxscript: very useful improvement I found on the testboxes
    svn:sync-xref-src-repo-rev: r115212

--- src/VBox/ValidationKit/testboxscript/win/autoexec-testbox.cmd
+++ src/VBox/ValidationKit/testboxscript/win/autoexec-testbox.cmd
@@ -1,5 +1,8 @@
-@echo "$Id: autoexec-testbox.cmd 96152 2014-09-17 14:51:43Z noreply@oracle.com $"
+@echo "$Id: autoexec-testbox.cmd 115212 2017-05-04 16:13:23Z klaus.espenlaub@oracle.com $"
 @echo on
+setlocal EnableExtensions
+set exe=python.exe
+for /f %%x in ('tasklist /NH /FI "IMAGENAME eq %exe%"') do if %%x == %exe% goto end
 %SystemDrive%\Python27\python.exe %SystemDrive%\testboxscript\testboxscript\testboxscript.py --testrsrc-server-type=cifs --builds-server-type=cifs
 pause
-
+:end
