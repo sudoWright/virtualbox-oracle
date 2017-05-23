9753bb7bae26c91115a1335bb59207c1c10f6fc6
Michael Thayer <noreply@oracle.com>
2017-05-23 14:07:04 +0000

    bugref:8533: Additions/x11: fully support VMSVGA Add VBoxClient support for dynamic resizing with VMSVGA enabled.  This is not yet started by default.  VBoxClient --vmsvga should be run as root outside of an X session for recent versions of the Linux kernel and interacts with the SVGA kernel graphics driver.  When run inside an X session it interacts with the X11 graphics driver.
    svn:sync-xref-src-repo-rev: r115628

--- src/VBox/Additions/x11/VBoxClient/VBoxClient.h
+++ src/VBox/Additions/x11/VBoxClient/VBoxClient.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxClient.h 109483 2016-08-02 15:51:18Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxClient.h 115628 2017-05-23 14:07:04Z noreply@oracle.com $ */
 /** @file
  *
  * VirtualBox additions user session daemon.
@@ -79,5 +79,6 @@ extern struct VBCLSERVICE **VBClGetHostVersionService();
 extern struct VBCLSERVICE **VBClGetDragAndDropService();
 #endif /* VBOX_WITH_DRAG_AND_DROP */
 extern struct VBCLSERVICE **VBClCheck3DService();
+extern struct VBCLSERVICE **VBClDisplaySVGAService();
 
 #endif /* !___vboxclient_vboxclient_h */
