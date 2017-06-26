43fb7f25d6c4c4c04b9a555153b96c1976669180
Sergey Dubov <sergey.dubov@oracle.com>
2017-06-26 16:03:40 +0000

    FE/Qt: Runtime UI: Rework ad-hoc medium mounting to be a bit more smarter: - Enumerate empty/busy drives and try to mount one of empty first, then one of busy; - Do not forget to save machine settings after all, check for possible errors as well.
    svn:sync-xref-src-repo-rev: r116373

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.h
@@ -1,4 +1,4 @@
-/* $Id: UISession.h 115033 2017-04-24 17:37:48Z sergey.dubov@oracle.com $ */
+/* $Id: UISession.h 116373 2017-06-26 16:03:40Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISession class declaration.
  */
@@ -398,7 +398,7 @@ private:
     /* Common helpers: */
     void setPointerShape(const uchar *pShapeData, bool fHasAlpha, uint uXHot, uint uYHot, uint uWidth, uint uHeight);
     bool preprocessInitialization();
-    bool mountAdHocImage(KDeviceType enmDeviceType, UIMediumType enmMediumType, const QString &strImage);
+    bool mountAdHocImage(KDeviceType enmDeviceType, UIMediumType enmMediumType, const QString &strMediumName);
     bool postprocessInitialization();
     int countOfVisibleWindows();
     /** Loads VM settings. */
