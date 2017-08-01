cb6b741d6f91d8c950b23085fa02677e58839806
Sergey Dubov <sergey.dubov@oracle.com>
2017-08-01 16:04:00 +0000

    FE/Qt: bugref:8900: Selector UI: Machine Tools: Keep the currently opened tool while switching between machine items; A bit of corresponding API for both Machine and Global tools.
    svn:sync-xref-src-repo-rev: r117325

--- src/VBox/Frontends/VirtualBox/src/selector/UIToolsPaneMachine.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/UIToolsPaneMachine.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsPaneMachine.cpp 117267 2017-07-29 14:41:24Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsPaneMachine.cpp 117325 2017-08-01 16:04:00Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsPaneMachine class implementation.
  */
@@ -51,6 +51,11 @@ UIToolsPaneMachine::~UIToolsPaneMachine()
 {
     /* Cleanup: */
     cleanup();
+}
+
+ToolTypeMachine UIToolsPaneMachine::currentTool() const
+{
+    return m_pLayout->currentWidget()->property("ToolType").value<ToolTypeMachine>();
 }
 
 bool UIToolsPaneMachine::isToolOpened(ToolTypeMachine enmType) const
