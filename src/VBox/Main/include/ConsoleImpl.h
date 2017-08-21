7de65e3471d5de8bb9187b6a42fdb95470d4495d
Andreas Loeffler <andreas.loeffler@oracle.com>
2017-08-21 13:48:12 +0000

    Audio: Implemented ability to enable / disable audio input / output on-the-fly via API.
    svn:sync-xref-src-repo-rev: r117633

--- src/VBox/Main/include/ConsoleImpl.h
+++ src/VBox/Main/include/ConsoleImpl.h
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.h 113111 2017-01-27 14:10:44Z alexander.eichner@oracle.com $ */
+/* $Id: ConsoleImpl.h 117633 2017-08-21 13:48:12Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBox Console COM Class definition
  */
@@ -157,6 +157,7 @@ public:
 
     // events from IInternalSessionControl
     HRESULT i_onNetworkAdapterChange(INetworkAdapter *aNetworkAdapter, BOOL changeAdapter);
+    HRESULT i_onAudioAdapterChange(IAudioAdapter *aAudioAdapter);
     HRESULT i_onSerialPortChange(ISerialPort *aSerialPort);
     HRESULT i_onParallelPortChange(IParallelPort *aParallelPort);
     HRESULT i_onStorageControllerChange();
