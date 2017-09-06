a1419d0f34a671c7fadcba48895fc91e8fa2de5f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-09-06 18:11:39 +0000

    iprt/formats/*.h: Use assertcompile.h instead of assert.h.
    svn:sync-xref-src-repo-rev: r117918

--- include/iprt/formats/mz.h
+++ include/iprt/formats/mz.h
@@ -1,4 +1,4 @@
-/* $Id: mz.h 109030 2016-07-22 18:16:43Z knut.osmundsen@oracle.com $ */
+/* $Id: mz.h 117918 2017-09-06 18:11:39Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT, MZ Executable Header.
  */
@@ -28,7 +28,7 @@
 #define ___iprt_formats_mz_h
 
 #include <iprt/types.h>
-#include <iprt/assert.h>
+#include <iprt/assertcompile.h>
 
 typedef struct _IMAGE_DOS_HEADER
 {
