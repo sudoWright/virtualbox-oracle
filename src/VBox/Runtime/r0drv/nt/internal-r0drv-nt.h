1bc4d26d5bab54c37e9047ee2915bd724441379c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-12-15 16:18:28 +0000

    iprt/r0drv/nt: Fixing NT4, broke it just now.
    svn:sync-xref-src-repo-rev: r119719

--- src/VBox/Runtime/r0drv/nt/internal-r0drv-nt.h
+++ src/VBox/Runtime/r0drv/nt/internal-r0drv-nt.h
@@ -1,4 +1,4 @@
-/* $Id: internal-r0drv-nt.h 119714 2017-12-15 15:07:27Z knut.osmundsen@oracle.com $ */
+/* $Id: internal-r0drv-nt.h 119719 2017-12-15 16:18:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Internal Header for the NT Ring-0 Driver Code.
  */
@@ -58,6 +58,7 @@ extern uint32_t                                g_cRtMpNtMaxCpus;
 extern RTCPUID                                 g_aidRtMpNtByCpuSetIdx[RTCPUSET_MAX_CPUS];
 
 extern decltype(ExAllocatePoolWithTag)        *g_pfnrtExAllocatePoolWithTag;
+extern decltype(ExFreePoolWithTag)            *g_pfnrtExFreePoolWithTag;
 extern PFNMYEXSETTIMERRESOLUTION               g_pfnrtNtExSetTimerResolution;
 extern PFNMYKEFLUSHQUEUEDDPCS                  g_pfnrtNtKeFlushQueuedDpcs;
 extern PFNHALREQUESTIPI_W7PLUS                 g_pfnrtHalRequestIpiW7Plus;
