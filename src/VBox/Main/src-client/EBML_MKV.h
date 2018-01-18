aaac5f9b838b2cd0c77914a8d4c48d962943d284
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-01-18 09:40:49 +0000

    VideoRec/EBML_MKV.h: Added FrameRate element.
    svn:sync-xref-src-repo-rev: r120341

--- src/VBox/Main/src-client/EBML_MKV.h
+++ src/VBox/Main/src-client/EBML_MKV.h
@@ -1,4 +1,4 @@
-/* $Id: EBML_MKV.h 118810 2017-10-28 13:12:06Z knut.osmundsen@oracle.com $ */
+/* $Id: EBML_MKV.h 120341 2018-01-18 09:40:49Z andreas.loeffler@oracle.com $ */
 /** @file
  * EbmlMkvIDs.h - Matroska EBML Class IDs.
  */
@@ -69,6 +69,7 @@ enum MkvElem
     MkvElem_Video                   = 0xE0,
     MkvElem_PixelWidth              = 0xB0,
     MkvElem_PixelHeight             = 0xBA,
+    MkvElem_FrameRate               = 0x2383E3,
 
     MkvElem_Audio                   = 0xE1,
     MkvElem_SamplingFrequency       = 0xB5,
