5ea048ad205fa9d9212c69c07d9b31dd8b509b6a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-01-21 16:18:58 +0000

    ValidationKit: Python 3 and pylint 1.8.1 adjustments/fixes.
    svn:sync-xref-src-repo-rev: r120396

--- src/VBox/ValidationKit/testmanager/batch/add_build.py
+++ src/VBox/ValidationKit/testmanager/batch/add_build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: add_build.py 118412 2017-10-17 14:26:02Z knut.osmundsen@oracle.com $
+# $Id: add_build.py 120396 2018-01-21 16:18:58Z knut.osmundsen@oracle.com $
 # pylint: disable=C0301
 
 """
@@ -28,12 +28,12 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 118412 $"
+__version__ = "$Revision: 120396 $"
 
 # Standard python imports
 import sys;
 import os;
-from optparse import OptionParser;
+from optparse import OptionParser;  # pylint: disable=deprecated-module
 
 # Add Test Manager's modules path
 g_ksTestManagerDir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))));
