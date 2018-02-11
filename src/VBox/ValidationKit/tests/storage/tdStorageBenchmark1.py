6006362b320a0239db73e7c2daa10552bcd34161
Alexander Eichner <alexander.eichner@oracle.com>
2018-02-11 12:07:16 +0000

    ValidationKit/tests/tdStorageBenchmark1: Enable QCOW testing again
    svn:sync-xref-src-repo-rev: r120806

--- src/VBox/ValidationKit/tests/storage/tdStorageBenchmark1.py
+++ src/VBox/ValidationKit/tests/storage/tdStorageBenchmark1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdStorageBenchmark1.py 120805 2018-02-11 12:06:42Z alexander.eichner@oracle.com $
+# $Id: tdStorageBenchmark1.py 120806 2018-02-11 12:07:16Z alexander.eichner@oracle.com $
 
 """
 VirtualBox Validation Kit - Storage benchmark.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 120805 $"
+__version__ = "$Revision: 120806 $"
 
 
 # Standard Python imports.
@@ -489,7 +489,6 @@ class tdStorageBenchmark(vbox.TestDriver):                                      
         self.asHostIoCacheDef        = ['default', 'hostiocache', 'no-hostiocache'];
         self.asHostIoCache           = self.asHostIoCacheDef;
         self.asDiskFormatsDef        = ['VDI', 'VMDK', 'VHD', 'QED', 'Parallels', 'QCOW', 'iSCSI'];
-        self.asDiskFormatsDef        = ['VDI', 'VMDK', 'VHD', 'QED', 'Parallels', 'iSCSI'];
         self.asDiskFormats           = self.asDiskFormatsDef;
         self.asDiskVariantsDef       = ['Dynamic', 'Fixed', 'DynamicSplit2G', 'FixedSplit2G', 'Network'];
         self.asDiskVariants          = self.asDiskVariantsDef;
