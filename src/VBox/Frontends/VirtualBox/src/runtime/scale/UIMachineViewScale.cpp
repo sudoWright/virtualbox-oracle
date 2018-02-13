b24684c2e3588649875bc1f499a3590dca696980
Sergey Dubov <sergey.dubov@oracle.com>
2018-02-13 11:59:10 +0000

    Build fix for r120842.
    svn:sync-xref-src-repo-rev: r120843

--- src/VBox/Frontends/VirtualBox/src/runtime/scale/UIMachineViewScale.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/scale/UIMachineViewScale.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineViewScale.cpp 120842 2018-02-13 11:56:54Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineViewScale.cpp 120843 2018-02-13 11:59:10Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineViewScale class implementation.
  */
@@ -128,7 +128,7 @@ void UIMachineViewScale::applyMachineViewScaleFactor()
 {
     /* If scaled-size is valid: */
     const QSize scaledSize = frameBuffer()->scaledSize();
-    const double dDevicePixelRatio = frameBuffer()->devicePixelRatio();
+    const double dDevicePixelRatio = frameBuffer()->devicePixelRatio(); Q_UNUSED(dDevicePixelRatio);
     const bool fUseUnscaledHiDPIOutput = frameBuffer()->useUnscaledHiDPIOutput();
     if (scaledSize.isValid())
     {
