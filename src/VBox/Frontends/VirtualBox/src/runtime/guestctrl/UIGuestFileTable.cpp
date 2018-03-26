9219c427566d88cfc644079c474ef8aae4998e2d
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-03-26 12:16:37 +0000

    FE/Qt: bugref:6699 Replace 'current location line edit' with an combo box
    svn:sync-xref-src-repo-rev: r121482

--- src/VBox/Frontends/VirtualBox/src/runtime/guestctrl/UIGuestFileTable.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/guestctrl/UIGuestFileTable.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGuestFileTable.cpp 121477 2018-03-26 09:03:32Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIGuestFileTable.cpp 121482 2018-03-26 12:16:37Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIGuestControlFileTable class implementation.
  */
@@ -115,7 +115,7 @@ void UIGuestFileTable::readDirectory(const QString& strPath,
         }
         insertItemsToTree(directories, parent, true, isStartDir);
         insertItemsToTree(files, parent, false, isStartDir);
-        updateCurrentLocationEdit(strPath);
+        //updateCurrentLocationEdit(strPath);
     }
     directory.Close();
 }
