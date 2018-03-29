16e253c18898fa29ee68d4ac1d77af741e63224e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-03-29 15:10:10 +0000

    VBoxLicenseViewer.cpp: Fixed sloppy @todo comment to make scm happy.
    svn:sync-xref-src-repo-rev: r121554

--- src/VBox/Frontends/VirtualBox/src/VBoxLicenseViewer.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxLicenseViewer.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxLicenseViewer.cpp 121531 2018-03-28 17:01:46Z sergey.dubov@oracle.com $ */
+/* $Id: VBoxLicenseViewer.cpp 121554 2018-03-29 15:10:10Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Qt GUI - VBoxLicenseViewer class implementation.
  */
@@ -63,7 +63,7 @@ VBoxLicenseViewer::VBoxLicenseViewer(QWidget *pParent /* = 0 */)
         }
 
         /* Create agree button: */
-        /** todo rework buttons to be a part of button-box itself */
+        /** @todo rework buttons to be a part of button-box itself */
         QDialogButtonBox *pDialogButtonBox = new QIDialogButtonBox;
         if (pDialogButtonBox)
         {
