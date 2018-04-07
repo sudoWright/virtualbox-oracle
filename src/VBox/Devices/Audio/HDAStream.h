f9f3378444ac993a8e0320e3fe64bd7f19250c2a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-04-07 14:35:47 +0000

    HDAStreamPeriod.cpp: No point in hdaStreamPeriodCreate returning VINF_SUCCESS even when RTCritSectInit fails (if that is intentional, please change it to a void function).  The HDASTREAMPERIOD::Dbg.tsStartNs member is used in log statements but was incorrectly guarded by DEBUG rather than LOG_ENABLED (this would create build problems if enabling logging in say a release build).
    svn:sync-xref-src-repo-rev: r121835

--- src/VBox/Devices/Audio/HDAStream.h
+++ src/VBox/Devices/Audio/HDAStream.h
@@ -1,4 +1,4 @@
-/* $Id: HDAStream.h 120809 2018-02-11 21:25:29Z andreas.loeffler@oracle.com $ */
+/* $Id: HDAStream.h 121835 2018-04-07 14:35:47Z knut.osmundsen@oracle.com $ */
 /** @file
  * HDAStream.h - Stream functions for HD Audio.
  */
@@ -23,9 +23,6 @@
 #include "HDAStreamMap.h"
 #include "HDAStreamPeriod.h"
 
-/*********************************************************************************************************************************
-*   Prototypes                                                                                                                   *
-*********************************************************************************************************************************/
 
 typedef struct HDAMIXERSINK *PHDAMIXERSINK;
 
