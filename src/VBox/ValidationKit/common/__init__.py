1fe7fb075b7046cf82a8471ac6f3c254ff06ef76
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-05-23 12:18:08 +0000

    valkit/common: Added netutils.py with getPrimaryHostIp() and gethostnameFqdn() functions.
    svn:sync-xref-src-repo-rev: r122745

--- src/VBox/ValidationKit/common/__init__.py
+++ src/VBox/ValidationKit/common/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: __init__.py 118412 2017-10-17 14:26:02Z knut.osmundsen@oracle.com $
+# $Id: __init__.py 122745 2018-05-23 12:18:08Z knut.osmundsen@oracle.com $
 
 """
 Common code between testmanager, testbox and testdriver.
@@ -26,10 +26,11 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 118412 $"
+__version__ = "$Revision: 122745 $"
 
 
 from common import constants;
 from common import utils;
+from common import netutils;
 from common import webutils;
 
