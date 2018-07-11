fd4c32a328b5282b752089b5fdd2d9851f143c6b
Valery Ushakov <noreply@oracle.com>
2018-07-11 16:39:52 +0000

    NetworkServices/Dhcpd: There's no pressing need to inline Db ctor. While here, init m_pConfig to NULL.
    svn:sync-xref-src-repo-rev: r123637

--- src/VBox/NetworkServices/Dhcpd/Db.h
+++ src/VBox/NetworkServices/Dhcpd/Db.h
@@ -1,4 +1,4 @@
-/* $Id: Db.h 121776 2018-04-05 15:20:32Z noreply@oracle.com $ */
+/* $Id: Db.h 123637 2018-07-11 16:39:52Z noreply@oracle.com $ */
 /** @file
  * DHCP server - address database
  */
@@ -126,7 +126,7 @@ private:
     IPv4Pool m_pool;
 
 public:
-    Db() {}
+    Db();
     ~Db();
 
     int init(const Config *pConfig);
