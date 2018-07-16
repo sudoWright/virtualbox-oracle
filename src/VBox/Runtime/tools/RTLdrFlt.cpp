2b5d2f05c4c0dda8e1a122569bfe8a91a76ba30e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-07-16 10:03:41 +0000

    VMM,DBGC,IPRT: In memory
    - VMM: Morphed part of the NT kernel digger into DBGFR3ModInMem.
    - DBGC: Added 'loadinmem' command for accessing the DBGFR3ModInMem functionality.
    - IPRT: Modified RTDbgModCreateFromPeImage to clearly indicate to caller whether the loader module was consumed or not (missing direct ref counting).
    - IPRT: Added RTLdrGetHostArch for resolving RTLDRARCH_HOST.
    - IPRT: Added RTLdrArchName for naming a RTLDRARCH value.
    
    
    
    svn:sync-xref-src-repo-rev: r123740

--- src/VBox/Runtime/tools/RTLdrFlt.cpp
+++ src/VBox/Runtime/tools/RTLdrFlt.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTLdrFlt.cpp 118769 2017-10-27 15:48:25Z knut.osmundsen@oracle.com $ */
+/* $Id: RTLdrFlt.cpp 123740 2018-07-16 10:03:41Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Utility for translating addresses into symbols+offset.
  */
@@ -224,7 +224,7 @@ int main(int argc, char **argv)
                 return RTEXITCODE_SUCCESS;
 
             case 'V':
-                RTPrintf("$Revision: 118769 $\n");
+                RTPrintf("$Revision: 123740 $\n");
                 return RTEXITCODE_SUCCESS;
 
             case VINF_GETOPT_NOT_OPTION:
@@ -256,7 +256,7 @@ int main(int argc, char **argv)
                 if (enmOpenMethod == kOpenMethod_FromImage)
                     rc = RTDbgModCreateFromImage(&hMod, pszModule, NULL, enmArch, hDbgCfg);
                 else
-                    rc = RTDbgModCreateFromPeImage(&hMod, pszModule, NULL, NIL_RTLDRMOD, cbImage, uTimestamp, hDbgCfg);
+                    rc = RTDbgModCreateFromPeImage(&hMod, pszModule, NULL, NULL, cbImage, uTimestamp, hDbgCfg);
                 if (RT_FAILURE(rc))
                     return RTMsgErrorExit(RTEXITCODE_FAILURE, "RTDbgModCreateFromImage(,%s,,) -> %Rrc", pszModule, rc);
 
