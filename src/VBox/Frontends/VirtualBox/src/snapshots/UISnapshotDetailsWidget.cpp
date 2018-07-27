67c052433dd59cac0479e6d87ff44ad2e92789d8
Sergey Dubov <sergey.dubov@oracle.com>
2018-07-27 09:19:33 +0000

    FE/Qt: Moving snapshots related files to separate folder.
    svn:sync-xref-src-repo-rev: r124000

--- src/VBox/Frontends/VirtualBox/src/snapshots/UISnapshotDetailsWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/snapshots/UISnapshotDetailsWidget.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISnapshotDetailsWidget.cpp 123401 2018-07-04 09:28:36Z sergey.dubov@oracle.com $ */
+/* $Id: UISnapshotDetailsWidget.cpp 124000 2018-07-27 09:19:33Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISnapshotDetailsWidget class implementation.
  */
