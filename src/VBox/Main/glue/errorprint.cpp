15eb77d8210036a4926539afbaf22aed7f59f59d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-08-05 13:58:10 +0000

    Main,Config.kmk: GCC 8.2.0 fixes
    svn:sync-xref-src-repo-rev: r124132

--- src/VBox/Main/glue/errorprint.cpp
+++ src/VBox/Main/glue/errorprint.cpp
@@ -1,4 +1,4 @@
-/* $Id: errorprint.cpp 123499 2018-07-07 15:31:29Z knut.osmundsen@oracle.com $ */
+/* $Id: errorprint.cpp 124132 2018-08-05 13:58:10Z knut.osmundsen@oracle.com $ */
 
 /** @file
  * MS COM / XPCOM Abstraction Layer:
@@ -80,7 +80,7 @@ void GluePrintErrorInfo(const com::ErrorInfo &info)
         RTMsgError("%s", str.c_str());
         Log(("ERROR: %s", str.c_str()));
     }
-    catch (std::bad_alloc)
+    catch (std::bad_alloc &)
     {
         RTMsgError("std::bad_alloc in GluePrintErrorInfo!");
         Log(("ERROR: std::bad_alloc in GluePrintErrorInfo!\n"));
