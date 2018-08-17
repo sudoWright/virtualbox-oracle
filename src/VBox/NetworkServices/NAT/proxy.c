2d3f7a4ada237ffe9235f7ef35fabaf3ead3726b
Michal Necasek <michal.necasek@oracle.com>
2018-08-17 18:27:04 +0000

    One more header.
    svn:sync-xref-src-repo-rev: r124428

--- src/VBox/NetworkServices/NAT/proxy.c
+++ src/VBox/NetworkServices/NAT/proxy.c
@@ -1,4 +1,4 @@
-/* $Id: proxy.c 124427 2018-08-17 18:17:59Z michal.necasek@oracle.com $ */
+/* $Id: proxy.c 124428 2018-08-17 18:27:04Z michal.necasek@oracle.com $ */
 /** @file
  * NAT Network - proxy setup and utilities.
  */
@@ -32,6 +32,7 @@
 #include <sys/poll.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdio.h>
