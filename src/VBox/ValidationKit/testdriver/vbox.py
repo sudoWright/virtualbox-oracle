8d89237484898005d7ecd4a6f173afad3f2d841e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-09-24 11:09:00 +0000

    ValidationKit: pylint adjustments/fixes.
    svn:sync-xref-src-repo-rev: r125281

--- src/VBox/ValidationKit/testdriver/vbox.py
+++ src/VBox/ValidationKit/testdriver/vbox.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vbox.py 124749 2018-08-31 14:47:37Z noreply@oracle.com $
+# $Id: vbox.py 125281 2018-09-24 11:09:00Z knut.osmundsen@oracle.com $
 # pylint: disable=C0302
 
 """
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 124749 $"
+__version__ = "$Revision: 125281 $"
 
 
 # Standard Python imports.
@@ -1097,7 +1097,7 @@ class TestDriver(base.TestDriver):                                              
                 if sResponse is None  or  sResponse.strip() != 'READY':
                     reporter.error('VBoxSVC failed starting up... (sResponse=%s)' % (sResponse,));
                     if not self.oVBoxSvcProcess.wait(5000):
-                        self.oVBoxSvcProcess.terminate(2500);
+                        self.oVBoxSvcProcess.terminate();
                     self.oVBoxSvcProcess.wait(5000);
                     self.oVBoxSvcProcess = None;
 
