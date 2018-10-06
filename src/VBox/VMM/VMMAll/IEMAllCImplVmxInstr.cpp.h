3f1c75c99c315ef3baa2c441faa3fcbdcf7293cd
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-10-06 06:36:57 +0000

    IEM: Nested VMX: bugref:9180 I think I'm finally done with the Mov-CR intercepts. Updated the todo list.
    svn:sync-xref-src-repo-rev: r125525

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllCImplVmxInstr.cpp.h 125524 2018-10-06 06:34:04Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAllCImplVmxInstr.cpp.h 125525 2018-10-06 06:36:57Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - VT-x instruction implementation.
  */
@@ -31,7 +31,6 @@
  *  VMX_EXIT_GETSEC
  *  VMX_EXIT_INVD
  *  VMX_EXIT_RSM
- *  VMX_EXIT_MOV_CRX
  *  VMX_EXIT_MOV_DRX
  *  VMX_EXIT_IO_INSTR
  *  VMX_EXIT_MWAIT
