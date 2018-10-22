3fc15cb9a000806b53699f37d73dc3e17e1b2c87
Michael Thayer <noreply@oracle.com>
2018-10-22 09:37:39 +0000

    Additions/linux/vboxguest: drop __devinitdata attribute. bugref:4567: Linux kernel driver maintenance On old kernels we were putting our PCI device ID structure in a different ELF section which could potentially have been evicted before we referenced it.  This caused compiler warnings and never made much sense (new kernels did away with it altogether), so this change drops it.
    svn:sync-xref-src-repo-rev: r125985

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-linux.c
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-linux.c
@@ -1,4 +1,4 @@
-/* $Rev: 120719 $ */
+/* $Rev: 125985 $ */
 /** @file
  * VBoxGuest - Linux specifics.
  *
@@ -215,11 +215,7 @@ static struct miscdevice        g_MiscDeviceUser =
 
 
 /** PCI hotplug structure. */
-static const struct pci_device_id
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 8, 0)
-__devinitdata
-#endif
-g_VBoxGuestPciId[] =
+static const struct pci_device_id g_VBoxGuestPciId[] =
 {
     {
         vendor:     VMMDEV_VENDORID,
