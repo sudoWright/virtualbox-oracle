855b34935c2e491db2b0c743614cc9b5e58e74f4
Michael Thayer <noreply@oracle.com>
2018-11-30 10:55:38 +0000

    Additions/installer/linux: try enabling the VBoxClient SVGA service. bugref:8533: Additions/x11: fully support VMSVGA On systems with VMSVGA and kernels newer than 4.6 we need to send resize information to the kernel. On all other systems VBoxClient will gracefully fail to start the service for doing that.
    svn:sync-xref-src-repo-rev: r127040

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-# $Id: vboxadd-x11.sh 126592 2018-11-14 01:45:41Z knut.osmundsen@oracle.com $
+# $Id: vboxadd-x11.sh 127040 2018-11-30 10:55:38Z noreply@oracle.com $
 ## @file
-# Linux Additions X11 setup init script ($Revision: 126592 $)
+# Linux Additions X11 setup init script ($Revision: 127040 $)
 #
 
 #
@@ -481,6 +481,8 @@ EOF
     case "${x_version}" in 4.* | 6.* | 7.* | 1.?.* | 1.1* )
         setup_opengl
     esac
+    # Try enabling VMSVGA drm device resizing.
+    VBoxClient --vmsvga
 }
 
 cleanup()
