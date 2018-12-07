8418a23298008aac3a653eb1c5443dceb440bf3b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-12-07 20:12:42 +0000

    VirtualBoxClientListImpl.cpp -> src-global/win/
    svn:sync-xref-src-repo-rev: r127304

--- src/VBox/Main/src-global/win/VirtualBoxClientListImpl.cpp
+++ src/VBox/Main/src-global/win/VirtualBoxClientListImpl.cpp
@@ -1,8 +1,6 @@
-/* $Id: VirtualBoxClientListImpl.cpp 123983 2018-07-26 10:03:25Z vitali.pelenjow@oracle.com $ */
+/* $Id: VirtualBoxClientListImpl.cpp 127304 2018-12-07 20:12:42Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Global COM Class implementation.
- *
- * @todo r=bird: Why is this file in src-all? Shouldn't it be in src-global/win/?!?
  */
 
 /*
