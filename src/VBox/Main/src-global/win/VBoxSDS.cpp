1bf1901cfea88add477a789c4db85718b82a87b1
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-12-09 23:19:16 +0000

    Main: Kicked out RpcChannelHook and IVirtualBoxclientList hacks (basically rolling back r12027) as these won't be used.  See bugref:3300 for details.
    svn:sync-xref-src-repo-rev: r127333

--- src/VBox/Main/src-global/win/VBoxSDS.cpp
+++ src/VBox/Main/src-global/win/VBoxSDS.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSDS.cpp 127323 2018-12-09 19:25:00Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxSDS.cpp 127333 2018-12-09 23:19:16Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxSDS - COM global service main entry (System Directory Service)
  */
@@ -751,15 +751,6 @@ void VBoxSDSNotifyClientCount(uint32_t cClients)
     }
 }
 #endif
-
-
-/** Special export that make VBoxProxyStub not register this process as one that
- * VBoxSDS should be watching.
- */
-extern "C" DECLEXPORT(void) VBOXCALL Is_VirtualBox_service_process_like_VBoxSDS_And_VBoxSDS(void)
-{
-    /* never called, just need to be here */
-}
 
 
 /**
