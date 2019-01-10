30decc6fec72156e7603664f557b3fc5037b09f3
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2019-01-10 18:07:47 +0000

    Main/Recording: thin out the excessive includes so that it doesn't hide missing includes in totally unrelated code, and adjust that code to do the right thing
    svn:sync-xref-src-repo-rev: r128078

--- src/VBox/Main/src-client/UsbWebcamInterface.cpp
+++ src/VBox/Main/src-client/UsbWebcamInterface.cpp
@@ -1,4 +1,4 @@
-/* $Id: UsbWebcamInterface.cpp 127855 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: UsbWebcamInterface.cpp 128078 2019-01-10 18:07:47Z klaus.espenlaub@oracle.com $ */
 /** @file
  * UsbWebcamInterface - Driver Interface for USB Webcam emulation.
  */
@@ -25,6 +25,7 @@
 #include "EmulatedUSBImpl.h"
 
 #include <VBox/vmm/pdmwebcaminfs.h>
+#include <VBox/err.h>
 
 
 typedef struct EMWEBCAMREMOTE
