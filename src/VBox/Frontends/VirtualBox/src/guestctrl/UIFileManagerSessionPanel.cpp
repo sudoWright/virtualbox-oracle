37375175e351b9e117b84df631b563506de59084
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2019-02-06 09:50:26 +0000

    FE/Qt: bugref:6699 Start session panel correctly.
    svn:sync-xref-src-repo-rev: r128655

--- src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerSessionPanel.cpp
+++ src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerSessionPanel.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIFileManagerSessionPanel.cpp 128487 2019-01-30 14:18:45Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIFileManagerSessionPanel.cpp 128655 2019-02-06 09:50:26Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewer class implementation.
  */
@@ -136,6 +136,7 @@ void UIGuestSessionCreateWidget::prepareWidgets()
         connect(m_pCloseButton, &QPushButton::clicked, this, &UIGuestSessionCreateWidget::sigCloseSession);
     }
     m_pMainLayout->insertStretch(-1, 1);
+    switchSessionCreateMode();
     retranslateUi();
 }
 
