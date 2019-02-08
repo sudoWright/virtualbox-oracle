37fd675eb8488d1b2ad523fa239aaca68fa6cba4
Sergey Dubov <sergey.dubov@oracle.com>
2019-02-08 18:05:46 +0000

    FE/Qt: bugref:9241: VirtualBox Manager UI: Make sure Chooser pane item is fully visible on focus-item change if possible (that's necessary after r128731).
    svn:sync-xref-src-repo-rev: r128735

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserView.h
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserView.h
@@ -1,4 +1,4 @@
-/* $Id: UIChooserView.h 128731 2019-02-08 15:38:03Z sergey.dubov@oracle.com $ */
+/* $Id: UIChooserView.h 128735 2019-02-08 18:05:46Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIChooserView class declaration.
  */
@@ -51,12 +51,6 @@ public:
     /** @} */
 
 public slots:
-
-    /** @name General stuff.
-      * @{ */
-        /** Handles focus change to @a pFocusItem. */
-        void sltFocusChanged();
-    /** @} */
 
     /** @name Layout stuff.
       * @{ */
