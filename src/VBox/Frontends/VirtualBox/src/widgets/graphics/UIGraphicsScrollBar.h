65248c3a100e44a36ac26e6adc63ef8f4b84baf9
Sergey Dubov <sergey.dubov@oracle.com>
2019-02-11 16:35:35 +0000

    FE/Qt: bugref:9373: More appropriate prepare/cleanup cascade for UIGraphicsScrollBar.
    svn:sync-xref-src-repo-rev: r128776

--- src/VBox/Frontends/VirtualBox/src/widgets/graphics/UIGraphicsScrollBar.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/graphics/UIGraphicsScrollBar.h
@@ -1,4 +1,4 @@
-/* $Id: UIGraphicsScrollBar.h 128726 2019-02-08 13:53:15Z sergey.dubov@oracle.com $ */
+/* $Id: UIGraphicsScrollBar.h 128776 2019-02-11 16:35:35Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIGraphicsScrollBar class declaration.
  */
@@ -120,6 +120,10 @@ private:
     void prepare();
     /** Prepares widgets. */
     void prepareWidgets();
+    /** Prepares buttons. */
+    void prepareButtons();
+    /** Prepares token. */
+    void prepareToken();
     /** Prepares animation. */
     void prepareAnimation();
 
