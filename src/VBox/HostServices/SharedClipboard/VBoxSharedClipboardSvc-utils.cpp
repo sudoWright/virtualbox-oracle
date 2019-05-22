733d83162184b1c67d4efbcb1254870c9883b6d7
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-05-22 09:58:53 +0000

    Shared Clipboard/URI: SCM fix.
    svn:sync-xref-src-repo-rev: r130720

--- src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc-utils.cpp
+++ src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc-utils.cpp
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $Id: VBoxSharedClipboardSvc-utils.cpp 130720 2019-05-22 09:58:53Z andreas.loeffler@oracle.com $ */
 /** @file
  * Shared Clipboard Service - Host service utility functions.
  */
