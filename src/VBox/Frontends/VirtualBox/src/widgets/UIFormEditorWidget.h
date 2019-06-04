85bb046a27c44d016c3da87fbefc7bb404d8b096
Sergey Dubov <sergey.dubov@oracle.com>
2019-06-04 08:26:16 +0000

    FE/Qt: bugref:9434: Moving UIFormEditorWidget to widgets section of the project, it will be reused from different wizards.
    svn:sync-xref-src-repo-rev: r131077

--- src/VBox/Frontends/VirtualBox/src/widgets/UIFormEditorWidget.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIFormEditorWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIFormEditorWidget.h 130082 2019-04-17 14:57:04Z sergey.dubov@oracle.com $ */
+/* $Id: UIFormEditorWidget.h 131077 2019-06-04 08:26:16Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIFormEditorWidget class declaration.
  */
