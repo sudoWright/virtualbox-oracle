cdc43e2e035bd9ea776f13746b483be556792750
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2019-08-05 12:22:35 +0000

    FE/Qt: bugref: 9510: Windows build fix
    svn:sync-xref-src-repo-rev: r132574

--- src/VBox/Frontends/VirtualBox/src/runtime/information/UIPerformanceMonitor.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/information/UIPerformanceMonitor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIPerformanceMonitor.cpp 132573 2019-08-05 12:15:04Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIPerformanceMonitor.cpp 132574 2019-08-05 12:22:35Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIPerformanceMonitor class implementation.
  */
@@ -360,7 +360,7 @@ const QQueue<ULONG> &UISubMetric::data() const
 
 bool UISubMetric::isPercentage() const
 {
-    return m_strUnit == '%';
+    return m_strUnit == "%";
 }
 
 
