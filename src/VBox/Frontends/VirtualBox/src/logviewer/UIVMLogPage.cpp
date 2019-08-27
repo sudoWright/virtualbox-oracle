f1c5dbf55fd9fb85758330aadc3985009267db0c
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-08-27 17:47:44 +0000

    Shared Clipboard/URI: Added protocol versioning support plus enhanced versions of existing commands (to also provide context IDs, among other stuff). So far only the host service(s) and the Windows guest is using the new(er) protocol.
    svn:sync-xref-src-repo-rev: r132934

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogPage.cpp
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogPage.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMLogPage.cpp 132800 2019-08-18 10:38:58Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMLogPage.cpp 132934 2019-08-27 17:47:44Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewer class implementation.
  */
@@ -192,7 +192,7 @@ void UIVMLogPage::deleteBookmark(int index)
 {
     if (m_bookmarkVector.size() <= index)
          return;
-    m_bookmarkVector.remove(index, 1);
+    //m_bookmarkVector.remove(index, 1);
     updateTextEditBookmarkLineSet();
 }
 
