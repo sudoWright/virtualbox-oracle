2575c64bf7bb6631f44d75d834eb075db8f383af
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-09-15 10:59:26 +0000

    vsheriff: Solaris stuck driver detection.
    svn:sync-xref-src-repo-rev: r133356

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: virtual_test_sheriff.py 132685 2019-08-12 20:04:52Z knut.osmundsen@oracle.com $
+# $Id: virtual_test_sheriff.py 133356 2019-09-15 10:59:26Z knut.osmundsen@oracle.com $
 # pylint: disable=line-too-long
 
 """
@@ -35,7 +35,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 132685 $"
+__version__ = "$Revision: 133356 $"
 
 
 # Standard python imports
@@ -337,7 +337,7 @@ class VirtualTestSheriff(object): # pylint: disable=too-few-public-methods
 
         if self.oConfig.sLogFile:
             self.oLogFile = open(self.oConfig.sLogFile, "a");
-            self.oLogFile.write('VirtualTestSheriff: $Revision: 132685 $ \n');
+            self.oLogFile.write('VirtualTestSheriff: $Revision: 133356 $ \n');
 
 
     def eprint(self, sText):
@@ -711,7 +711,7 @@ class VirtualTestSheriff(object): # pylint: disable=too-few-public-methods
         for idTestResult, tReason in dReasonForResultId.items():
             oFailureReason = self.getFailureReason(tReason);
             if oFailureReason is not None:
-                sComment = 'Set by $Revision: 132685 $' # Handy for reverting later.
+                sComment = 'Set by $Revision: 133356 $' # Handy for reverting later.
                 if idTestResult in dCommentForResultId:
                     sComment += ': ' + dCommentForResultId[idTestResult];
 
@@ -835,12 +835,10 @@ class VirtualTestSheriff(object): # pylint: disable=too-few-public-methods
         ],
         'solaris': [
             # ( Whether to stop on hit, reason tuple, needle text. )
-            ( True, ktReason_Host_InstallationFailed,
-              'svcadm: Couldn\'t bind to svc.configd.' ),
-            ( True, ktReason_Host_InstallationFailed,
-              'pkgadd: ERROR: postinstall script did not complete successfully' ),
-            ( True, ktReason_Host_DriverNotUnloading,
-              'can\'t unload the module: Device busy' ),
+            ( True, ktReason_Host_InstallationFailed, 'svcadm: Couldn\'t bind to svc.configd.' ),
+            ( True, ktReason_Host_InstallationFailed, 'pkgadd: ERROR: postinstall script did not complete successfully' ),
+            ( True, ktReason_Host_DriverNotUnloading, 'can\'t unload the module: Device busy' ),
+            ( True, ktReason_Host_DriverNotUnloading, 'Unloading: Host module ...FAILED!' ),
         ],
     };
 
