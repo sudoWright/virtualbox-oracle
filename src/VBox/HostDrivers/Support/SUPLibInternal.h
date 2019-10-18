5fa1eb9ab39e5ad5c90c9954f6efbd39c3faf8c5
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-10-18 21:13:03 +0000

    *: doxygen fixes
    svn:sync-xref-src-repo-rev: r134082

--- src/VBox/HostDrivers/Support/SUPLibInternal.h
+++ src/VBox/HostDrivers/Support/SUPLibInternal.h
@@ -1,4 +1,4 @@
-/* $Id: SUPLibInternal.h 127870 2019-01-01 04:34:11Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPLibInternal.h 134082 2019-10-18 21:13:03Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Internal header.
  */
@@ -67,7 +67,7 @@
 /** @def SUP_HARDENED_SUID
  * Whether we're employing set-user-ID-on-execute in the hardening.
  */
-#if !defined(RT_OS_OS2) && !defined(RT_OS_WINDOWS) && !defined(RT_OS_L4)
+#if (!defined(RT_OS_OS2) && !defined(RT_OS_WINDOWS) && !defined(RT_OS_L4)) || defined(DOXYGEN_RUNNING)
 # define SUP_HARDENED_SUID
 #else
 # undef  SUP_HARDENED_SUID
