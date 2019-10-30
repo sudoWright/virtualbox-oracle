7b1f7ba03a6f76ff298207b31b6127b0fab83992
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-10-30 14:14:10 +0000

    Devices: Use PDMDEVINS_2_DATA and PDMDEVINS_2_DATA. bugref:9218
    svn:sync-xref-src-repo-rev: r134346

--- src/VBox/Devices/PC/DevPcArch.cpp
+++ src/VBox/Devices/PC/DevPcArch.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevPcArch.cpp 134242 2019-10-24 11:25:11Z knut.osmundsen@oracle.com $ */
+/* $Id: DevPcArch.cpp 134346 2019-10-30 14:14:10Z knut.osmundsen@oracle.com $ */
 /** @file
  * DevPcArch - PC Architecture Device.
  */
@@ -249,7 +249,7 @@ static DECLCALLBACK(int) pcarchInitComplete(PPDMDEVINS pDevIns)
 static DECLCALLBACK(int)  pcarchConstruct(PPDMDEVINS pDevIns, int iInstance, PCFGMNODE pCfg)
 {
     PDMDEV_CHECK_VERSIONS_RETURN(pDevIns);
-    PDEVPCARCH  pThis = PDMINS_2_DATA(pDevIns, PDEVPCARCH);
+    PDEVPCARCH  pThis = PDMDEVINS_2_DATA(pDevIns, PDEVPCARCH);
     int         rc;
     Assert(iInstance == 0); RT_NOREF(iInstance);
 
