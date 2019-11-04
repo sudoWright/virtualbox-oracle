1df0b27851821fdc7f272ca81c407ee3e8a7e2b9
Frank Batschulat <noreply@oracle.com>
2019-11-04 12:10:50 +0000

    Linux 5.4: no more arbitrary executable pages and more changes, ticketref:18945
    svn:sync-xref-src-repo-rev: r134407

--- src/VBox/Runtime/r0drv/linux/thread2-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/thread2-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: thread2-r0drv-linux.c 127855 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: thread2-r0drv-linux.c 134407 2019-11-04 12:10:50Z noreply@oracle.com $ */
 /** @file
  * IPRT - Threads (Part 2), Ring-0 Driver, Linux.
  */
@@ -36,6 +36,9 @@
 #include <iprt/errcore.h>
 #include "internal/thread.h"
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+    #include <uapi/linux/sched/types.h>
+#endif /* >= KERNEL_VERSION(4, 11, 0) */
 
 RTDECL(RTTHREAD) RTThreadSelf(void)
 {
