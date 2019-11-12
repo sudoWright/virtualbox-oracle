ed2d9b184287395854acdfb3c0741cf4c8eeb5f2
Paul Klissner <noreply@oracle.com>
2019-11-12 18:53:07 +0000

    Storage/DevVirtioSCSI.cpp: More fixing  burns on build box for stuff that builds clean on my Debian machine
    svn:sync-xref-src-repo-rev: r134607

--- src/VBox/Devices/Storage/DevVirtioSCSI.cpp
+++ src/VBox/Devices/Storage/DevVirtioSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioSCSI.cpp 134606 2019-11-12 18:46:14Z noreply@oracle.com $ $Revision: 134606 $ $Date: 2019-11-12 19:46:14 +0100 (Tue, 12 Nov 2019) $ $Author: noreply@oracle.com $ */
+/* $Id: DevVirtioSCSI.cpp 134607 2019-11-12 18:53:07Z noreply@oracle.com $ $Revision: 134607 $ $Date: 2019-11-12 19:53:07 +0100 (Tue, 12 Nov 2019) $ $Author: noreply@oracle.com $ */
 /** @file
  * VBox storage devices - Virtio SCSI Driver
  *
@@ -1138,7 +1138,7 @@ static int virtioScsiR3ReqSubmit(PPDMDEVINS pDevIns, PVIRTIOSCSI pThis, PVIRTIOS
     if (uType == 0xc1 && uTarget == 0x01)
     {
         LogRel(("* * * REPORT LUNS LU ACCESSED * * * "));
-        uScsiLun == 0xff; /* Force rejection. todo: figure out right way to handle, r=paul */
+        uScsiLun = 0xff; /* Force rejection. todo: figure out right way to handle, r=paul */
     }
     else
     if (uType != 1)
