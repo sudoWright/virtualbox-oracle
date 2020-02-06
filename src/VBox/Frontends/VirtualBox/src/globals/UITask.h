576dfd270d732ac17606e6cb2c988ac4e0a62d2b
Sergey Dubov <sergey.dubov@oracle.com>
2020-02-06 14:59:10 +0000

    FE/Qt: bugref:9653: !VirtualBox Manager: Chooser pane: Advancing r136014 to asynchronously acquire cloud instance list and append received items to corresponding profile groups; For that separate thread task sub-class was introduced.
    svn:sync-xref-src-repo-rev: r136026

--- src/VBox/Frontends/VirtualBox/src/globals/UITask.h
+++ src/VBox/Frontends/VirtualBox/src/globals/UITask.h
@@ -1,4 +1,4 @@
-/* $Id: UITask.h 136013 2020-02-05 19:14:36Z sergey.dubov@oracle.com $ */
+/* $Id: UITask.h 136026 2020-02-06 14:59:10Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UITask class declaration.
  */
@@ -43,8 +43,9 @@ public:
     /** Task types. */
     enum Type
     {
-        Type_MediumEnumeration = 1,
-        Type_DetailsPopulation = 2,
+        Type_MediumEnumeration     = 1,
+        Type_DetailsPopulation     = 2,
+        Type_CloudAcquireInstances = 3,
     };
 
     /** Constructs the task of passed @a enmType. */
