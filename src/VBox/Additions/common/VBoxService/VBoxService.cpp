6e576450048497a4afa08d4f7285cdd0de1e2fcd
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-03-25 13:15:39 +0000

    Guest Control/VBoxService: Resolved another @todo: Don't ignore non-options in the guest session argument handler. bugref:9320
    svn:sync-xref-src-repo-rev: r136626

--- src/VBox/Additions/common/VBoxService/VBoxService.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxService.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxService.cpp 135976 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxService.cpp 136626 2020-03-25 13:15:39Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxService - Guest Additions Service Skeleton.
  */
@@ -914,7 +914,7 @@ int main(int argc, char **argv)
      * handles a guest control session.
      */
     if (   argc >= 2
-        && !RTStrICmp(argv[1], "guestsession"))
+        && !RTStrICmp(argv[1], VBOXSERVICECTRLSESSION_GETOPT_PREFIX))
         fUserSession = true;
 #endif
 
