970788da2b978f7d8c8767daa08654f945937f21
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-04-07 10:16:49 +0000

    DevVirtioSCSI: file header correction. bugref:9440
    svn:sync-xref-src-repo-rev: r137010

--- src/VBox/Devices/Storage/DevVirtioSCSI.cpp
+++ src/VBox/Devices/Storage/DevVirtioSCSI.cpp
@@ -1,4 +1,4 @@
-    /* $Id: DevVirtioSCSI.cpp 137006 2020-04-07 09:43:23Z knut.osmundsen@oracle.com $ $Revision: 137006 $ $Date: 2020-04-07 11:43:23 +0200 (Tue, 07 Apr 2020) $ $Author: knut.osmundsen@oracle.com $ */
+/* $Id: DevVirtioSCSI.cpp 137010 2020-04-07 10:16:49Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox storage devices - Virtio SCSI Driver
  *
