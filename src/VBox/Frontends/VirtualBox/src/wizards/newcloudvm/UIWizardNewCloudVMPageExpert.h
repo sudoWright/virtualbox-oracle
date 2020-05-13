bd92f2c3afff4b4344e8801360e9a5b4b71a5015
Sergey Dubov <sergey.dubov@oracle.com>
2020-05-13 15:19:41 +0000

    FE/Qt: bugref:9653: New Cloud VM wizard: Possibility to create VM from boot-volume, not just image as before.
    svn:sync-xref-src-repo-rev: r137930

--- src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageExpert.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageExpert.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewCloudVMPageExpert.h 137921 2020-05-13 12:03:55Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardNewCloudVMPageExpert.h 137930 2020-05-13 15:19:41Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewCloudVMPageExpert class declaration.
  */
@@ -71,6 +71,9 @@ private slots:
     void sltHandleAccountComboChange();
     /** Handles account tool-button click. */
     void sltHandleAccountButtonClick();
+
+    /** Handles change in source tab-bar. */
+    void sltHandleSourceChange();
 
     /** Handles change in instance list. */
     void sltHandleInstanceListChange();
