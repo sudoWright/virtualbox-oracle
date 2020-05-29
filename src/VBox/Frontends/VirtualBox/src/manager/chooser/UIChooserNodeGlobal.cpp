19e45098667d40c3f071027148d2f1a8836b4f29
Sergey Dubov <sergey.dubov@oracle.com>
2020-05-29 14:10:06 +0000

    FE/Qt: bugref:9653: !VirtualBox Manager: Chooser pane: General simplification for Chooser-pane nodes constructors.
    svn:sync-xref-src-repo-rev: r138344

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserNodeGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserNodeGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIChooserNodeGlobal.cpp 138109 2020-05-20 14:43:30Z sergey.dubov@oracle.com $ */
+/* $Id: UIChooserNodeGlobal.cpp 138344 2020-05-29 14:10:06Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIChooserNodeGlobal class implementation.
  */
@@ -24,8 +24,8 @@
 
 
 UIChooserNodeGlobal::UIChooserNodeGlobal(UIChooserNode *pParent,
-                                         bool fFavorite,
                                          int iPosition,
+                                         bool fFavorite,
                                          const QString &)
     : UIChooserNode(pParent, fFavorite)
 {
@@ -38,8 +38,8 @@ UIChooserNodeGlobal::UIChooserNodeGlobal(UIChooserNode *pParent,
 }
 
 UIChooserNodeGlobal::UIChooserNodeGlobal(UIChooserNode *pParent,
-                                         UIChooserNodeGlobal *pCopyFrom,
-                                         int iPosition)
+                                         int iPosition,
+                                         UIChooserNodeGlobal *pCopyFrom)
     : UIChooserNode(pParent, pCopyFrom->isFavorite())
 {
     /* Add to parent: */
