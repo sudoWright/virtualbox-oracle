d93c58d95ac2988b1ebb760fba0ccb51b957edc4
Paul Klissner <noreply@oracle.com>
2020-06-11 07:35:40 +0000

    Attempt to fix burn
    svn:sync-xref-src-repo-rev: r138586

--- src/VBox/Devices/Storage/DevVirtioSCSI.cpp
+++ src/VBox/Devices/Storage/DevVirtioSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioSCSI.cpp 138574 2020-06-11 06:01:00Z noreply@oracle.com $ */
+/* $Id: DevVirtioSCSI.cpp 138586 2020-06-11 07:35:40Z noreply@oracle.com $ */
 /** @file
  * VBox storage devices - Virtio SCSI Driver
  *
@@ -124,6 +124,7 @@
 
 #define IS_VIRTQ_EMPTY(pDevIns, pVirtio, idxQueue) \
             (virtioCoreQueueAvailCount(pDevIns, pVirtio, idxQueue) == 0)
+
 
 /*********************************************************************************************************************************
 *   Structures and Typedefs                                                                                                      *
