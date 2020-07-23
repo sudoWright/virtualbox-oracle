0d4825d88c52796d690dacfe4244cb9819fb1d6c
Sergey Dubov <sergey.dubov@oracle.com>
2020-07-23 16:11:33 +0000

    FE/Qt: bugref:9686, bugref:9510: Make sure Performance Monitor tool being properly hidden/shown when current VM item state changes.
    svn:sync-xref-src-repo-rev: r139483

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.h
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManagerWidget.h 138326 2020-05-28 14:47:42Z sergey.dubov@oracle.com $ */
+/* $Id: UIVirtualBoxManagerWidget.h 139483 2020-07-23 16:11:33Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManagerWidget class declaration.
  */
@@ -213,6 +213,12 @@ protected:
     /** @} */
 
 private slots:
+
+    /** @name CVirtualBox event handling stuff.
+      * @{ */
+        /** Handles CVirtualBox event about state change for machine with @a uId. */
+        void sltHandleStateChange(const QUuid &uId);
+    /** @} */
 
     /** @name Tool-bar stuff.
       * @{ */
