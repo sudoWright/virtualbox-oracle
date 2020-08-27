0dcb6f6dec757fe764074224181290b245306e49
Sergey Dubov <sergey.dubov@oracle.com>
2020-08-27 14:38:18 +0000

    FE/Qt: bugref:9609: UIActionPool: Do not initialize standard shortcuts for temporary action-pool, sequence will be revoked from already assigned permanent pool in such case provoking strange Qt crash.
    svn:sync-xref-src-repo-rev: r140092

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPool.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIActionPool.cpp 139788 2020-08-10 12:04:07Z sergey.dubov@oracle.com $ */
+/* $Id: UIActionPool.cpp 140092 2020-08-27 14:38:18Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIActionPool class implementation.
  */
@@ -1367,7 +1367,7 @@ protected:
     /** Returns standard shortcut. */
     virtual QKeySequence standardShortcut(UIActionPoolType) const /* override */
     {
-        return QKeySequence(QKeySequence::Refresh);
+        return actionPool()->isTemporary() ? QKeySequence() : QKeySequence(QKeySequence::Refresh);
     }
 
     /** Handles translation event. */
