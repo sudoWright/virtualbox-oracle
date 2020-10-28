168d50c86575422b4eacdd51c88363969b7c507f
Sergey Dubov <sergey.dubov@oracle.com>
2020-10-28 17:22:07 +0000

    FE/Qt: bugref:9653: !VirtualBox Manager: Chooser pane: Forgot something in r141133.
    svn:sync-xref-src-repo-rev: r141134

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGroup.h 141028 2020-10-20 14:37:58Z sergey.dubov@oracle.com $ */
+/* $Id: UIChooserItemGroup.h 141134 2020-10-28 17:22:07Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGroup class declaration.
  */
@@ -72,7 +72,8 @@ public:
       * @{ */
         /** Returns group node reference. */
         UIChooserNodeGroup *nodeToGroupType() const;
-
+        /** Returns item machine id. */
+        QUuid id() const;
         /** Returns group node type. */
         UIChooserNodeGroupType groupType() const;
 
