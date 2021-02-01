dab3db733b57321733dc9f52b847e444ab8551f4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-02-01 11:39:30 +0000

    VMM/HMR3ResetCpu: Only touch the vmx fields if we're in vmx mode.
    svn:sync-xref-src-repo-rev: r142521

--- src/VBox/VMM/VMMAll/HMAll.cpp
+++ src/VBox/VMM/VMMAll/HMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMAll.cpp 142510 2021-01-29 18:17:10Z knut.osmundsen@oracle.com $ */
+/* $Id: HMAll.cpp 142521 2021-02-01 11:39:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * HM - All contexts.
  */
@@ -789,7 +789,8 @@ VMM_INT_DECL(void) HMHCChangedPagingMode(PVM pVM, PVMCPUCC pVCpu, PGMMODE enmSha
      * If the guest left protected mode VMX execution, we'll have to be
      * extra careful if/when the guest switches back to protected mode.
      */
-    if (enmGuestMode == PGMMODE_REAL)
+    if (   enmGuestMode == PGMMODE_REAL
+        && pVM->hm.s.vmx.fEnabled)
     {
         PVMXVMCSINFOSHARED pVmcsInfoShared = hmGetVmxActiveVmcsInfoShared(pVCpu);
         pVmcsInfoShared->fWasInRealMode = true;
