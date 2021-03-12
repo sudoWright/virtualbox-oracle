1459f43eb6f6cb3ab1b2f8e0485165edfe6f0463
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-03-12 14:16:01 +0000

    FE/Qt: bugref:9950. Some cleaning
    svn:sync-xref-src-repo-rev: r143251

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic1.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic1.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMPageBasic1.h 143250 2021-03-12 13:57:02Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIWizardNewVMPageBasic1.h 143251 2021-03-12 14:16:01Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMPageBasic1 class declaration.
  */
@@ -163,7 +163,6 @@ private:
     /** @name Widgets
      * @{ */
         QIRichTextLabel *m_pNameOSTypeLabel;
-        QIRichTextLabel *m_pUnattendedLabel;
     /** @} */
 };
 
