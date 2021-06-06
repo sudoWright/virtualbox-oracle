4306a0d16be998b8e08cb14684bed48cb635d140
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-06-06 23:15:41 +0000

    DrvHostAudioPulseAudio: LogRel2. bugref:9890
    svn:sync-xref-src-repo-rev: r144886

--- src/VBox/Devices/Audio/DrvHostAudioPulseAudio.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioPulseAudio.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioPulseAudio.cpp 144885 2021-06-06 23:07:39Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvHostAudioPulseAudio.cpp 144886 2021-06-06 23:15:41Z knut.osmundsen@oracle.com $ */
 /** @file
  * Host audio driver - Pulse Audio.
  */
@@ -1205,7 +1205,7 @@ static DECLCALLBACK(int) drvHstAudPaHA_StreamCreate(PPDMIHOSTAUDIO pInterface, P
             pStreamPA->BufAttr.fragsize  = cbSchedHint;
 
             /* (tlength, minreq and prebuf are playback only) */
-            LogFunc(("Requesting: BufAttr: fragsize=%#RX32 maxLength=%#RX32\n",
+            LogRel2(("PulseAudio: Requesting: BufAttr: fragsize=%#RX32 maxLength=%#RX32\n",
                      pStreamPA->BufAttr.fragsize, pStreamPA->BufAttr.maxlength));
         }
         else
