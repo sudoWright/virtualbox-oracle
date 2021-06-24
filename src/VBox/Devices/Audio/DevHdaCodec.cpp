4bc6700d03655e518a6333d80a2989b9ebd24127
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-06-24 12:30:53 +0000

    DevHda: Merged DevHdaCommon.h into DevHda.h and mostly likewise with the corresponding code.  bugref:9890
    svn:sync-xref-src-repo-rev: r145345

--- src/VBox/Devices/Audio/DevHdaCodec.cpp
+++ src/VBox/Devices/Audio/DevHdaCodec.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevHdaCodec.cpp 145327 2021-06-23 19:06:11Z knut.osmundsen@oracle.com $ */
+/* $Id: DevHdaCodec.cpp 145345 2021-06-24 12:30:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * Intel HD Audio Controller Emulation - Codec, Sigmatel/IDT STAC9220.
  *
@@ -40,8 +40,6 @@
 #include "VBoxDD.h"
 #include "AudioMixer.h"
 #include "DevHda.h"
-#include "DevHdaCodec.h"
-#include "DevHdaCommon.h"
 
 
 /*********************************************************************************************************************************
