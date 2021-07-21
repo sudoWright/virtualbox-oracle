b479c38df2459cff5e438b423b6c751c653838a6
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-07-21 11:29:35 +0000

    FE/Qt: bugref:9996. More fixes related to medium path updates. take 2
    svn:sync-xref-src-repo-rev: r145812

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMPageExpert.h 145802 2021-07-20 14:30:44Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIWizardNewVMPageExpert.h 145812 2021-07-21 11:29:35Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMPageExpert class declaration.
  */
@@ -134,7 +134,7 @@ private:
     QWidget *createNameOSTypeWidgets();
 
     void updateVirtualMediumPathFromMachinePathName();
-    void updateWidgetAfterMediumFormatChange();
+    void updateDiskWidgetsAfterMediumFormatChange();
     void updateHostnameDomainNameFromMachineName();
     void setEnableNewDiskWidgets(bool fEnable);
     void setVirtualDiskFromDiskCombo();
