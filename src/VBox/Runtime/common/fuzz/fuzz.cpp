8e8b4b78dcf6d2bdb758a4101568a611bfb44064
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-08-23 19:08:38 +0000

    Runtime: More VALID_PTR -> RT_VALID_PTR/AssertPtr.
    svn:sync-xref-src-repo-rev: r146438

--- src/VBox/Runtime/common/fuzz/fuzz.cpp
+++ src/VBox/Runtime/common/fuzz/fuzz.cpp
@@ -1,4 +1,4 @@
-/* $Id: fuzz.cpp 139061 2020-07-08 19:33:26Z knut.osmundsen@oracle.com $ */
+/* $Id: fuzz.cpp 146438 2021-08-23 19:08:38Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Fuzzing framework API, core.
  */
@@ -2136,7 +2136,7 @@ RTDECL(int) RTFuzzCtxInputGenerate(RTFUZZCTX hFuzzCtx, PRTFUZZINPUT phFuzzInput)
 
         rc = pMutator->pfnPrep(pThis, offStart, pMutationParent, &pMutation);
         if (   RT_SUCCESS(rc)
-            && VALID_PTR(pMutation))
+            && RT_VALID_PTR(pMutation))
         {
             pMutation->pMutator = pMutator;
 
