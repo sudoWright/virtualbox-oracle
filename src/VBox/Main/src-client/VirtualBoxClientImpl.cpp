960e9a50392558fe912d9f8008dae34c058d6ad0
Alexey Diachenko <noreply@oracle.com>
2021-08-25 13:28:50 +0000

    Main: bugref:1909: Some improvements in translation event handling
    svn:sync-xref-src-repo-rev: r146517

--- src/VBox/Main/src-client/VirtualBoxClientImpl.cpp
+++ src/VBox/Main/src-client/VirtualBoxClientImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxClientImpl.cpp 146515 2021-08-25 12:20:08Z noreply@oracle.com $ */
+/* $Id: VirtualBoxClientImpl.cpp 146517 2021-08-25 13:28:50Z noreply@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -83,7 +83,7 @@ public:
         {
             case VBoxEventType_OnLanguageChanged:
             {
-                VirtualBoxTranslator *pTranslator = VirtualBoxTranslator::instance();
+                VirtualBoxTranslator *pTranslator = VirtualBoxTranslator::tryInstance();
                 if (pTranslator)
                 {
                     ComPtr<ILanguageChangedEvent> pEvent = aEvent;
