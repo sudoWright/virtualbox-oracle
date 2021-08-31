a9d91ed0a4b18f1e2462316473d9dc2ccfc4df2b
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-08-31 15:57:07 +0000

    FE/Qt: bugref:9996. Some more code cleaning
    svn:sync-xref-src-repo-rev: r146677

--- src/VBox/Frontends/VirtualBox/src/wizards/newvd/UIWizardNewVDPageFileType.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvd/UIWizardNewVDPageFileType.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVDPageFileType.h 146630 2021-08-30 09:38:18Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIWizardNewVDPageFileType.h 146677 2021-08-31 15:57:07Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVDPageFileType class declaration.
  */
@@ -27,11 +27,6 @@
 /* Forward declarations: */
 class QIRichTextLabel;
 class UIDiskFormatsGroupBox;
-class UIWizardNewVD;
-
-namespace UIWizardNewVDFileType
-{
-}
 
 /* 1st page of the New Virtual Hard Drive wizard (basic extension): */
 class SHARED_LIBRARY_STUFF UIWizardNewVDPageFileType : public UINativeWizardPage
