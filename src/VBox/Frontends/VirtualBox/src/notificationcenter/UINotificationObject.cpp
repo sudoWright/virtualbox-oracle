4e0a73a31ffdd40788e0f87e38e69cca34c9b11a
Sergey Dubov <sergey.dubov@oracle.com>
2021-09-14 10:22:33 +0000

    FE/Qt: bugref:10067: Close all finished notification-progresses unconditionally when they finished; To be done conditionally and protected with e-data flag later.
    svn:sync-xref-src-repo-rev: r146879

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObject.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObject.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObject.cpp 146725 2021-09-01 18:43:24Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObject.cpp 146879 2021-09-14 10:22:33Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINotificationObject class implementation.
  */
@@ -187,6 +187,7 @@ void UINotificationProgress::sltHandleProgressFinished()
 {
     m_uPercent = 100;
     emit sigProgressFinished();
+    close();
 }
 
 
