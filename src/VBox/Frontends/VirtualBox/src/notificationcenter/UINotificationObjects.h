c19de8864579da54b11a1488d78557061c7a8a5d
Sergey Dubov <sergey.dubov@oracle.com>
2021-10-22 18:03:41 +0000

    FE/Qt: bugref:10067: Extend UINotificationProgressSnapshotTake with signal notifying listeners about snapshot with particular id is taken.
    svn:sync-xref-src-repo-rev: r147821

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.h
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.h
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObjects.h 147775 2021-10-21 15:53:59Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObjects.h 147821 2021-10-22 18:03:41Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - Various UINotificationObjects declarations.
  */
@@ -1416,6 +1416,11 @@ class SHARED_LIBRARY_STUFF UINotificationProgressSnapshotTake : public UINotific
 {
     Q_OBJECT;
 
+signals:
+
+    /** Notifies listeners about snapshot with @a id taken. */
+    void sigSnapshotTaken(const QVariant &id);
+
 public:
 
     /** Constructs snapshot take notification-progress.
@@ -1452,6 +1457,8 @@ private:
     QString   m_strMachineName;
     /** Holds the session being opened. */
     CSession  m_comSession;
+    /** Holds the taken snapshot id. */
+    QUuid     m_uSnapshotId;
 };
 
 /** UINotificationProgress extension for snapshot restore functionality. */
