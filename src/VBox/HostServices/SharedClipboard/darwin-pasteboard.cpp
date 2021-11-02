351ec16798a003a92c1dbb20b873987957973326
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-11-02 21:47:25 +0000

    SharedClipboard/darwin: Force UTF-8 file encoding.
    svn:sync-xref-src-repo-rev: r148011

--- src/VBox/HostServices/SharedClipboard/darwin-pasteboard.cpp
+++ src/VBox/HostServices/SharedClipboard/darwin-pasteboard.cpp
@@ -1,10 +1,10 @@
-/* $Id: darwin-pasteboard.cpp 140013 2020-08-20 14:28:33Z andreas.loeffler@oracle.com $ */
+/* $Id: darwin-pasteboard.cpp 148011 2021-11-02 21:47:25Z knut.osmundsen@oracle.com $ */
 /** @file
  * Shared Clipboard Service - Mac OS X host implementation.
  */
 
 /*
- * Includes contributions from François Revol
+ * Includes contributions from FranÃ§ois Revol
  *
  * Copyright (C) 2008-2020 Oracle Corporation
  *
