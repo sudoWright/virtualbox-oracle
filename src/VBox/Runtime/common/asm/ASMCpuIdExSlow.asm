8705fae1fdd512d53065a76e92c009e3f21b74e2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-12-08 00:47:27 +0000

    iprt/asmdefs.h,IPRT/**/*asm: Added a RT_BEGINPROC macro that takes IN_RT_STATIC into account and selects either BEGINPROC_EXPORTED or BEGINPROC as appropriate.  Updated all relevant IRPT assembly code to use RT_BEGINPROC instead of BEGINPROC_EXPORTED.  (Hopefully this makes the GA build stop complaining about R_X86_64_PC32 relocations to RTStrEnd.)
    svn:sync-xref-src-repo-rev: r148719

--- src/VBox/Runtime/common/asm/ASMCpuIdExSlow.asm
+++ src/VBox/Runtime/common/asm/ASMCpuIdExSlow.asm
@@ -1,4 +1,4 @@
-; $Id: ASMCpuIdExSlow.asm 135976 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $
+; $Id: ASMCpuIdExSlow.asm 148719 2021-12-08 00:47:27Z knut.osmundsen@oracle.com $
 ;; @file
 ; IPRT - ASMCpuIdExSlow().
 ;
@@ -45,7 +45,7 @@ BEGINCODE
 ;
 ; @returns  EAX
 ;
-BEGINPROC_EXPORTED ASMCpuIdExSlow
+RT_BEGINPROC ASMCpuIdExSlow
         push    xBP
         mov     xBP, xSP
         push    xBX
