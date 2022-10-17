10ca1a9c061d6ecfcc0043099d36362329ff86c6
Alexander Eichner <alexander.eichner@oracle.com>
2022-10-17 17:47:19 +0000

    VMM/NEMR3Native-darwin: Addendum for r154132, unmapping doesn't unwedge the map. Check whether it would be possible to make it work when we set g_fAppleHvNoWX upfront by determining the macOS version, bugref:9044
    svn:sync-xref-src-repo-rev: r154150

--- src/VBox/VMM/VMMR3/NEMR3.cpp
+++ src/VBox/VMM/VMMR3/NEMR3.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR3.cpp 153224 2022-08-22 17:43:14Z klaus.espenlaub@oracle.com $ */
+/* $Id: NEMR3.cpp 154150 2022-10-17 17:47:19Z alexander.eichner@oracle.com $ */
 /** @file
  * NEM - Native execution manager.
  */
@@ -100,6 +100,7 @@ VMMR3_INT_DECL(int) NEMR3InitConfig(PVM pVM)
                                   "|VmxPleGap"
                                   "|VmxPleWindow"
                                   "|VmxLbr"
+                                  "|CatalinaWxWorkaround"
 #endif
                                   ,
                                   "" /* pszValidNodes */, "NEM" /* pszWho */, 0 /* uInstance */);
