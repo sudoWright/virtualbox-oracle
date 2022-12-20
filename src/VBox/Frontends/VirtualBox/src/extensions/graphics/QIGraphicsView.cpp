4d7c45616dbc7a29d4b3c222ca4462e6c2c07db9
Sergey Dubov <sergey.dubov@oracle.com>
2022-12-20 16:04:33 +0000

    FE/Qt: bugref:9898: Just todos and comments.
    svn:sync-xref-src-repo-rev: r154935

--- src/VBox/Frontends/VirtualBox/src/extensions/graphics/QIGraphicsView.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/graphics/QIGraphicsView.cpp
@@ -1,4 +1,4 @@
-/* $Id: QIGraphicsView.cpp 153224 2022-08-22 17:43:14Z klaus.espenlaub@oracle.com $ */
+/* $Id: QIGraphicsView.cpp 154935 2022-12-20 16:04:33Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - Qt extensions: QIGraphicsView class implementation.
  */
@@ -56,7 +56,7 @@ bool QIGraphicsView::event(QEvent *pEvent)
             QTouchEvent *pTouchEvent = static_cast<QTouchEvent*>(pEvent);
             AssertPtrReturn(pTouchEvent, QGraphicsView::event(pEvent));
             /* For touch-screen event we have something special: */
-#ifdef VBOX_IS_QT6_OR_LATER
+#ifdef VBOX_IS_QT6_OR_LATER /* QTouchDevice was consumed by QInputDevice in 6.0 */
             if (pTouchEvent->device()->type() == QInputDevice::DeviceType::TouchScreen)
 #else
             if (pTouchEvent->device()->type() == QTouchDevice::TouchScreen)
@@ -77,7 +77,7 @@ bool QIGraphicsView::event(QEvent *pEvent)
             QTouchEvent *pTouchEvent = static_cast<QTouchEvent*>(pEvent);
             AssertPtrReturn(pTouchEvent, QGraphicsView::event(pEvent));
             /* For touch-screen event we have something special: */
-#ifdef VBOX_IS_QT6_OR_LATER
+#ifdef VBOX_IS_QT6_OR_LATER /* QTouchDevice was consumed by QInputDevice in 6.0 */
             if (pTouchEvent->device()->type() == QInputDevice::DeviceType::TouchScreen)
 #else
             if (pTouchEvent->device()->type() == QTouchDevice::TouchScreen)
@@ -104,7 +104,7 @@ bool QIGraphicsView::event(QEvent *pEvent)
             QTouchEvent *pTouchEvent = static_cast<QTouchEvent*>(pEvent);
             AssertPtrReturn(pTouchEvent, QGraphicsView::event(pEvent));
             /* For touch-screen event we have something special: */
-#ifdef VBOX_IS_QT6_OR_LATER
+#ifdef VBOX_IS_QT6_OR_LATER /* QTouchDevice was consumed by QInputDevice in 6.0 */
             if (pTouchEvent->device()->type() == QInputDevice::DeviceType::TouchScreen)
 #else
             if (pTouchEvent->device()->type() == QTouchDevice::TouchScreen)
