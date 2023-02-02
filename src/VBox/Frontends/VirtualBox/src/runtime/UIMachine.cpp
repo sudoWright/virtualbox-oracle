d03c05c0873a9e5e28600a54e7beaf374f4c268c
Sergey Dubov <sergey.dubov@oracle.com>
2023-02-02 15:47:01 +0000

    FE/Qt: bugref:10322: Runtime UI: Cleanup for UISession stuff across whole the Runtime UI; Removing where it's possible.
    svn:sync-xref-src-repo-rev: r155699

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachine.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachine.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachine.cpp 155698 2023-02-02 14:46:36Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachine.cpp 155699 2023-02-02 15:47:01Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachine class implementation.
  */
@@ -526,7 +526,7 @@ void UIMachine::closeRuntimeUI()
 void UIMachine::sltChangeVisualState(UIVisualStateType visualState)
 {
     /* Create new machine-logic: */
-    UIMachineLogic *pMachineLogic = UIMachineLogic::create(this, uisession(), visualState);
+    UIMachineLogic *pMachineLogic = UIMachineLogic::create(this, visualState);
 
     /* First we have to check if the selected machine-logic is available at all.
      * Only then we delete the old machine-logic and switch to the new one. */
