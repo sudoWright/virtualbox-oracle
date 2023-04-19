560c3120c128c27f2ee9f3d744bcb89ffe5c5177
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-04-19 15:54:44 +0000

    Validation Kit/Shared Folders: Fixes for really old Linux guests (coreutils location) [pylint].
    svn:sync-xref-src-repo-rev: r156937

--- src/VBox/ValidationKit/tests/additions/tdAddSharedFolders1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddSharedFolders1.py
@@ -36,7 +36,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 156936 $"
+__version__ = "$Revision: 156937 $"
 
 # Standard Python imports.
 import os
@@ -191,7 +191,7 @@ class SubTstDrvAddSharedFolders1(base.SubTestDriverBase):
         """
         return self.unmountShareEx(oSession, oTxsSession, sShareName, sGuestMountPoint, fMustSucceed = True);
 
-    def testIt(self, oTestVm, oSession, oTxsSession):
+    def testIt(self, oTestVm, oSession, oTxsSession): #pylint: disable=too-many-statements
         """
         Executes the test.
 
