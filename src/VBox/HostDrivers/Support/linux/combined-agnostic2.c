b730c0471818bab1ac3dc86902003abb700e9618
Alexander Eichner <alexander.eichner@oracle.com>
2023-06-28 10:17:34 +0000

    Runtime,Additions,HostDrivers: Provide API to query the host page size and associated parameters for architectures where the used page size can vary (like on linux.arm64 depending on the kernel config), bugref:10476
    svn:sync-xref-src-repo-rev: r158022

--- src/VBox/HostDrivers/Support/linux/combined-agnostic2.c
+++ src/VBox/HostDrivers/Support/linux/combined-agnostic2.c
@@ -1,4 +1,4 @@
-/* $Id: combined-agnostic2.c 155244 2023-01-17 14:15:46Z knut.osmundsen@oracle.com $ */
+/* $Id: combined-agnostic2.c 158022 2023-06-28 10:17:34Z alexander.eichner@oracle.com $ */
 /** @file
  * SUPDrv - Combine a bunch of OS agnostic sources into one compile unit.
  */
@@ -102,6 +102,8 @@
 #include "generic/errvars-generic.c"
 #undef LOG_GROUP
 #include "generic/mppresent-generic.c"
+#undef LOG_GROUP
+#include "generic/system-page-size-generic.c"
 #undef LOG_GROUP
 #include "generic/uuid-generic.c"
 #undef LOG_GROUP
