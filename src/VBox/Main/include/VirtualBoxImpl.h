74b1061e492501e521b6a6ebe6ff97cf4d767595
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-09-27 08:21:09 +0000

    Main: Added IPlatformProperties::supportedGuestOSTypes() getter to return supported guest OS types for a specific platform. The IVirtualBox::guestOSTypes() getter in turn will return all guest OS types for all platforms. bugref:10384
    svn:sync-xref-src-repo-rev: r159265

--- src/VBox/Main/include/VirtualBoxImpl.h
+++ src/VBox/Main/include/VirtualBoxImpl.h
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.h 159153 2023-09-19 17:18:37Z brent.paulson@oracle.com $ */
+/* $Id: VirtualBoxImpl.h 159265 2023-09-27 08:21:09Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -321,8 +321,7 @@ public:
     HRESULT i_retainCryptoIf(PCVBOXCRYPTOIF *ppCryptoIf);
     HRESULT i_releaseCryptoIf(PCVBOXCRYPTOIF pCryptoIf);
     HRESULT i_unloadCryptoIfModule(void);
-
-
+    HRESULT i_getSupportedGuestOSTypes(std::vector<PlatformArchitecture_T> aArchitectures, std::vector<ComPtr<IGuestOSType>> &aGuestOSTypes);
 
 private:
     class ClientWatcher;
