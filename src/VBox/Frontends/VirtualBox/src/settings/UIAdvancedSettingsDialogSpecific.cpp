9276a2b04b39cf311dcd6dc0edb4a8b8afd35639
Sergey Dubov <sergey.dubov@oracle.com>
2023-10-25 19:32:41 +0000

    FE/Qt: bugref:10513: Machine settings / Network page: Renaming UIMachineSettingsNetworkPage to UIMachineSettingsNetwork, similarly to other pages.
    svn:sync-xref-src-repo-rev: r159680

--- src/VBox/Frontends/VirtualBox/src/settings/UIAdvancedSettingsDialogSpecific.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/UIAdvancedSettingsDialogSpecific.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIAdvancedSettingsDialogSpecific.cpp 159476 2023-10-12 11:28:52Z sergey.dubov@oracle.com $ */
+/* $Id: UIAdvancedSettingsDialogSpecific.cpp 159680 2023-10-25 19:32:41Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISettingsDialogSpecific class implementation.
  */
@@ -704,7 +704,7 @@ void UIAdvancedSettingsDialogMachine::prepare()
                 /* Network page: */
                 case MachineSettingsPageType_Network:
                 {
-                    pSettingsPage = new UIMachineSettingsNetworkPage;
+                    pSettingsPage = new UIMachineSettingsNetwork;
                     addItem(":/nw_32px.png", ":/nw_24px.png", ":/nw_16px.png",
                             iPageIndex, "#network", pSettingsPage);
                     addPageHelpKeyword(iPageIndex, "settings-network");
