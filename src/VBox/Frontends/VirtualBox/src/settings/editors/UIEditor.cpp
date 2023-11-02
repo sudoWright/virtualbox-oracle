527a7ccafa6708858c7aeab490c40ccbcefd47dd
Sergey Dubov <sergey.dubov@oracle.com>
2023-11-02 12:22:25 +0000

    FE/Qt: bugref:10513: UIEditor and sub-classes: Simplify filterOut stuff; No need to pass everything through whole the hierarchy, enough to handle optional method only.
    svn:sync-xref-src-repo-rev: r159811

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIEditor.cpp 159785 2023-10-31 16:46:47Z sergey.dubov@oracle.com $ */
+/* $Id: UIEditor.cpp 159811 2023-11-02 12:22:25Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIEditor class implementation.
  */
@@ -115,6 +115,9 @@ void UIEditor::filterOut(bool fExpertMode, const QString &strFilter)
      * activated after visibility changes: */
     foreach (QLayout *pLayout, findChildren<QLayout*>())
         pLayout->activate();
+
+    /* Call for filter change handler finally: */
+    handleFilterChange();
 }
 
 QStringList UIEditor::description() const
