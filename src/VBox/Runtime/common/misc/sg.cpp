807cbcff47f15112d4bfeb7fd7ecce82bfaf8255
Ksenia Stepanova <ksenia.s.stepanova@oracle.com>
2023-11-03 15:57:39 +0000

    added more detailed logging, tstRTSg.cpp testcase
    svn:sync-xref-src-repo-rev: r159849

--- src/VBox/Runtime/common/misc/sg.cpp
+++ src/VBox/Runtime/common/misc/sg.cpp
@@ -1,6 +1,6 @@
-/* $Id: sg.cpp 158845 2023-08-19 02:57:05Z knut.osmundsen@oracle.com $ */
+/* $Id: sg.cpp 159849 2023-11-03 15:57:39Z ksenia.s.stepanova@oracle.com $ */
 /** @file
- * IPRT - S/G buffer handling.
+ * IPRT - Scatter / Gather buffer handling.
  */
 
 /*
