d8f73b2db60ad6490d3ae608162d9ebe0dcd8bf8
Sergey Dubov <sergey.dubov@oracle.com>
2023-11-16 11:31:05 +0000

    FE/Qt: bugref:10513: VBox Manager: Hide Media, Network and Activity manager tools for Basic experience mode; Still need to do the same for File menu / Tools submenu.
    svn:sync-xref-src-repo-rev: r160241

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.h
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManagerWidget.h 160240 2023-11-16 11:23:25Z sergey.dubov@oracle.com $ */
+/* $Id: UIVirtualBoxManagerWidget.h 160241 2023-11-16 11:31:05Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManagerWidget class declaration.
  */
@@ -346,6 +346,8 @@ private:
 
     /** @name Tools / Common stuff.
       * @{ */
+        /** Updates Global tools menu. */
+        void updateToolsMenuGlobal();
         /** Updates Machine tools menu for @a pItem specified. */
         void updateToolsMenuMachine(UIVirtualMachineItem *pItem);
 
