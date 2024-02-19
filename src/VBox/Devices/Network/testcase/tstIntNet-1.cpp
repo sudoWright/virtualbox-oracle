29e7676b69432619652c6aa6760dfa32305056c9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-02-19 12:06:37 +0000

    tstIntNet-1: RTStrCat won't fail because we already subtracted the size of the string-to-append from the buffer size in the RTPathExecDir call. Just use memcpy to avoid upsetting anyone. bugref:3409
    svn:sync-xref-src-repo-rev: r161758

--- src/VBox/Devices/Network/testcase/tstIntNet-1.cpp
+++ src/VBox/Devices/Network/testcase/tstIntNet-1.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstIntNet-1.cpp 161579 2024-02-08 15:27:12Z andreas.loeffler@oracle.com $ */
+/* $Id: tstIntNet-1.cpp 161758 2024-02-19 12:06:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox - Testcase for internal networking, simple NetFlt trunk creation.
  */
@@ -818,7 +818,7 @@ extern "C" DECLEXPORT(int) TrustedMain(int argc, char **argv, char **envp)
                 return 1;
 
             case 'V':
-                RTPrintf("$Revision: 161579 $\n");
+                RTPrintf("$Revision: 161758 $\n");
                 return 0;
 
             default:
@@ -838,20 +838,15 @@ extern "C" DECLEXPORT(int) TrustedMain(int argc, char **argv, char **envp)
         return 1;
     }
 
-    char szPath[RTPATH_MAX];
-    rc = RTPathExecDir(szPath, sizeof(szPath) - sizeof("/../VMMR0.r0"));
+    static const char s_szVmmR0[] = "/../VMMR0.r0";
+    char              szPath[RTPATH_MAX];
+    rc = RTPathExecDir(szPath, sizeof(szPath) - sizeof(s_szVmmR0));
     if (RT_FAILURE(rc))
     {
         RTPrintf("tstIntNet-1: RTPathExecDir -> %Rrc\n", rc);
         return 1;
     }
-
-    rc = RTStrCat(szPath, sizeof(szPath), "/../VMMR0.r0");
-    if (RT_FAILURE(rc))
-    {
-        RTPrintf("tstIntNet-1: RTStrCat -> %Rrc\n", rc);
-        return 1;
-    }
+    memcpy(strchr(szPath, '\0'), s_szVmmR0, sizeof(s_szVmmR0));
 
     char szAbsPath[RTPATH_MAX];
     rc = RTPathAbs(szPath, szAbsPath, sizeof(szAbsPath));
