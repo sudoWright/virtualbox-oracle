158713b28423947d035a146285dbb99ff124dfa9
Alexander Eichner <alexander.eichner@oracle.com>
2024-03-13 08:13:38 +0000

    VMM/IEM: Implement native emitter for IEM_MC_BROADCAST_XREG_U64_ZX_VLMAX(), bugref:10614
    svn:sync-xref-src-repo-rev: r162184

--- src/VBox/VMM/VMMAll/IEMAllInstPython.py
+++ src/VBox/VMM/VMMAll/IEMAllInstPython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: IEMAllInstPython.py 162183 2024-03-13 07:20:44Z alexander.eichner@oracle.com $
+# $Id: IEMAllInstPython.py 162184 2024-03-13 08:13:38Z alexander.eichner@oracle.com $
 
 """
 IEM instruction extractor.
@@ -43,7 +43,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 162183 $"
+__version__ = "$Revision: 162184 $"
 
 # pylint: disable=anomalous-backslash-in-string,too-many-lines
 
@@ -2960,7 +2960,7 @@ g_dMcStmtParsers = {
     'IEM_MC_BEGIN':                                              (McBlock.parseMcBegin,             False, False, True,  ),
     'IEM_MC_BROADCAST_XREG_U16_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BROADCAST_XREG_U32_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
-    'IEM_MC_BROADCAST_XREG_U64_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
+    'IEM_MC_BROADCAST_XREG_U64_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  g_fNativeSimd),
     'IEM_MC_BROADCAST_XREG_U8_ZX_VLMAX':                         (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BROADCAST_YREG_U128_ZX_VLMAX':                       (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BROADCAST_YREG_U16_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
