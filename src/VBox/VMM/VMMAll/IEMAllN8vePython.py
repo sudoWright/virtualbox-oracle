fa7d60055734de792fc6f3d7fac1081e3b800899
Alexander Eichner <alexander.eichner@oracle.com>
2024-03-24 16:27:34 +0000

    VMM/IEM: Implement native emitters for IEM_MC_CALL_AVX_AIMPL_2() and IEM_MC_CALL_AVX_AIMPL_3(), bugref:10614
    svn:sync-xref-src-repo-rev: r162409

--- src/VBox/VMM/VMMAll/IEMAllN8vePython.py
+++ src/VBox/VMM/VMMAll/IEMAllN8vePython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: IEMAllN8vePython.py 162407 2024-03-24 01:07:36Z knut.osmundsen@oracle.com $
+# $Id: IEMAllN8vePython.py 162409 2024-03-24 16:27:34Z alexander.eichner@oracle.com $
 # pylint: disable=invalid-name
 
 """
@@ -34,7 +34,7 @@ along with this program; if not, see <https://www.gnu.org/licenses>.
 
 SPDX-License-Identifier: GPL-3.0-only
 """
-__version__ = "$Revision: 162407 $"
+__version__ = "$Revision: 162409 $"
 
 # Standard python imports:
 import copy;
@@ -387,8 +387,6 @@ class NativeRecompFunctionVariation(object):
                     # references).
                     #
                     asCallParams = oStmt.asParams[oStmt.idxParams:];
-                    if oStmt.sName.startswith('IEM_MC_CALL_AVX_AIMPL_'):
-                        asCallParams.insert(0, 'pXState');
                     for sParam in asCallParams:
                         oVarInfo = dVars.get(sParam);
                         if oVarInfo:
