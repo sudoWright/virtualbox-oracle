8a94db248057508071afd0dbb53a52dd3d5ad928
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2024-11-14 13:56:24 +0000

    FE/Qt: ​bugref:9831. Now each wizard page can have its own help keyword enabling help viewer to navigate to page specific sections.
    svn:sync-xref-src-repo-rev: r165939

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMNameOSTypePage.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMNameOSTypePage.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMNameOSTypePage.h 164827 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: UIWizardNewVMNameOSTypePage.h 165939 2024-11-14 13:56:24Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMNameOSTypePage class declaration.
  */
@@ -67,7 +67,7 @@ class UIWizardNewVMNameOSTypePage : public UINativeWizardPage
 public:
 
     /** Constructor. */
-    UIWizardNewVMNameOSTypePage();
+    UIWizardNewVMNameOSTypePage(const QString strHelpKeyword = QString());
     void setISOFilePath(const QString &strISOFilePath);
 
 protected:
