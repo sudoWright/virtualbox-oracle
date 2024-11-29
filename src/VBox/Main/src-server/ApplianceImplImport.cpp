8c6f7d8d7c615260d2b2b0f0c06a93fd5134f423
Brent Paulson <brent.paulson@oracle.com>
2024-11-29 19:10:14 +0000

    7.1: Backported r166200: FE/VBoxManage,Main/Appliance: Add the ability to export and import VMs which contain an NVMe storage controller. Follow-up changes to include support for moving a disk to an NVMe controller during import and also add some missing NVMe support in Appliance::i_importMachineGeneric().  bugref:10159
    svn:sync-xref-src-repo-rev: r166201

--- src/VBox/Main/src-server/ApplianceImplImport.cpp
+++ src/VBox/Main/src-server/ApplianceImplImport.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImplImport.cpp 164827 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: ApplianceImplImport.cpp 166201 2024-11-29 19:10:14Z brent.paulson@oracle.com $ */
 /** @file
  * IAppliance and IVirtualSystem COM class implementations.
  */
@@ -4080,6 +4080,12 @@ void Appliance::i_convertDiskAttachmentValues(const ovf::HardDiskController &hdc
             lDevice         = 0;
             break;
 
+        case ovf::HardDiskController::NVMe:
+            controllerName  = "NVMe";
+            lControllerPort = (int32_t)ulAddressOnParent;
+            lDevice         = 0;
+            break;
+
         default: break;
     }
 
@@ -5258,6 +5264,9 @@ l_skipped:
                         }
                         case ovf::HardDiskController::VIRTIOSCSI:
                             hdStorageControllerType = StorageControllerType_VirtioSCSI;
+                            break;
+                        case ovf::HardDiskController::NVMe:
+                            hdStorageControllerType = StorageControllerType_NVMe;
                             break;
                         default:
                             throw setError(E_FAIL,
