16aff88d8a40c2113e121a0cb996eddca2ef1941
Dmitrii Grigorev <dmitrii.grigorev@oracle.com>
2025-01-08 12:33:38 +0000

    7.1: Backported r166454. 3D: Check SVGA_CAP_GBOBJECTS capability before loading 3D surfaces from a saved state. More reliable check of 3D state presence should be implemented. bugref:10580
    svn:sync-xref-src-repo-rev: r166557

--- src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA.cpp 165995 2024-11-19 17:21:14Z dmitrii.grigorev@oracle.com $ */
+/* $Id: DevVGA-SVGA.cpp 166557 2025-01-08 12:33:38Z dmitrii.grigorev@oracle.com $ */
 /** @file
  * VMware SVGA device.
  *
@@ -6105,6 +6105,19 @@ int vmsvgaR3LoadExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM, uint32_t uVersion, uin
     /* Load our part of the VGAState */
     rc = pHlp->pfnSSMGetStructEx(pSSM, &pThis->svga, sizeof(pThis->svga), 0, g_aVGAStateSVGAFields, NULL);
     AssertRCReturn(rc, rc);
+
+    if (pThis->svga.fVMSVGA2dGBO)
+    {
+        if (pThis->svga.u32DeviceCaps & SVGA_CAP_GBOBJECTS)
+        {
+            LogRel(("VGA: VMSVGA2dGBO enabled in VM config and SVGA_CAP_GBOBJECTS is present in Caps. 3D state should be loaded.\n"));
+        }
+        else
+        {
+            LogRel(("VGA: VMSVGA2dGBO enabled in VM config but SVGA_CAP_GBOBJECTS is NOT present in Caps, so fVMSVGA2dGBO should be forced to 0\n"));
+            pThis->svga.fVMSVGA2dGBO = false;
+        }
+    }
 
     /* Load the VGA framebuffer. */
     AssertCompile(VMSVGA_VGA_FB_BACKUP_SIZE >= _32K);
