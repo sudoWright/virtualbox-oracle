f8308a27e9d8c391113fba67317626447fcd84c3
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-01-24 15:30:59 +0000

    7.1: Backported r167176.     * r167176: GuestHost/installation/VBoxWinDrvInst.cpp: Try harder if DiInstallDriverW() returns ERROR_WRONG_INF_TYPE by installing the detect INF section. This happens on Windows Vista where DiInstallDriverW() can't handle primitive drivers and should fix the mouse driver installation. For the mouse driver we can't use the [Manufacturer] section for primitive drivers, as InfVerif.exe will complain.
    svn:sync-xref-src-repo-rev: r167177

--- src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
+++ src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxWinDrvInst.cpp 166993 2025-01-16 15:24:16Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxWinDrvInst.cpp 167177 2025-01-24 15:30:59Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxWinDrvInst - Windows driver installation handling.
  */
@@ -1263,6 +1263,15 @@ static int vboxWinDrvInstallPerform(PVBOXWINDRVINSTINTERNAL pCtx, PVBOXWINDRVINS
                     if (dwErr == ERROR_LINE_NOT_FOUND)
                         fRc = true;
 
+                    /*
+                     * Work around an error which occurs on Windows Vista, where DiInstallDriverW() can't handle
+                     * primitive drivers (i.e. [Manufacturer] section is missing). So try installing the detected
+                     * INF section in the next block below.
+                     */
+                    if (dwErr == ERROR_WRONG_INF_TYPE)
+                        fRc = true;
+
+                    /* For anything else we want to get notified that something isn't working. */
                     if (!fRc)
                         rc = vboxWinDrvInstLogLastError(pCtx, "DiInstallDriverW() failed");
                 }
