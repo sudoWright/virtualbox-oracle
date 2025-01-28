da78abbc66084d58c50b24b92e11ed062439c408
Sergey Dubov <sergey.dubov@oracle.com>
2025-01-28 12:04:06 +0000

    FE/Qt: bugref:10814: VBox Manager / Tools pane: Graphical widget scene now automatically grabs mouse if you hide the widget on mouse press event before mouse release event arrived; This is a new behavior aspect starting since Qt6.8 and it was difficult to find.
    svn:sync-xref-src-repo-rev: r167212

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsModel.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsModel.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsModel.cpp 167190 2025-01-27 15:59:36Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsModel.cpp 167212 2025-01-28 12:04:06Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsModel class implementation.
  */
@@ -396,7 +396,7 @@ bool UIToolsModel::eventFilter(QObject *pWatched, QEvent *pEvent)
     switch (pEvent->type())
     {
         /* Mouse handler: */
-        case QEvent::GraphicsSceneMousePress:
+        case QEvent::GraphicsSceneMouseRelease:
         {
             /* Acquire event: */
             QGraphicsSceneMouseEvent *pMouseEvent = static_cast<QGraphicsSceneMouseEvent*>(pEvent);
