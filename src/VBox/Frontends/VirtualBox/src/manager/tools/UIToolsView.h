8759e64a8759fee31b4cbd3e87f3c2ee525a12fb
Sergey Dubov <sergey.dubov@oracle.com>
2025-02-13 15:25:28 +0000

    FE/Qt: bugref:10814: VBox Manager / Tools pane: Remove focus-item change handler as all items always visible in the tools menu.
    svn:sync-xref-src-repo-rev: r167515

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsView.h
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsView.h
@@ -1,4 +1,4 @@
-/* $Id: UIToolsView.h 167397 2025-02-06 15:06:48Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsView.h 167515 2025-02-13 15:25:28Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsView class declaration.
  */
@@ -60,12 +60,6 @@ public:
     /** @} */
 
 public slots:
-
-    /** @name General stuff.
-      * @{ */
-        /** Handles focus change to @a pFocusItem. */
-        void sltFocusChanged();
-    /** @} */
 
     /** @name Layout stuff.
       * @{ */
