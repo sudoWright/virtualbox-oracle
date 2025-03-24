f61ab4510457677bfaba5f4a4f2b8add696ef0b6
Alexander Eichner <alexander.eichner@oracle.com>
2025-03-24 18:54:47 +0000

    HostDrivers/Support: Make SUPR3 work on hosts where the page size is not known during compile time, bugref:10391 [build fix]
    svn:sync-xref-src-repo-rev: r168148

--- src/VBox/HostDrivers/Support/linux/SUPLib-linux.cpp
+++ src/VBox/HostDrivers/Support/linux/SUPLib-linux.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPLib-linux.cpp 168147 2025-03-24 18:53:21Z alexander.eichner@oracle.com $ */
+/* $Id: SUPLib-linux.cpp 168148 2025-03-24 18:54:47Z alexander.eichner@oracle.com $ */
 /** @file
  * VirtualBox Support Library - GNU/Linux specific parts.
  */
@@ -63,6 +63,7 @@
 #include <iprt/assert.h>
 #include <VBox/types.h>
 #include <iprt/string.h>
+#include <iprt/system.h>
 #include <VBox/err.h>
 #include <VBox/param.h>
 #include "../SUPLibInternal.h"
