237b493286da08f155f434879cb7cfd5d48ba476
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2025-04-09 11:16:52 +0000

    7.1: Backported r168162: Additions: Linux: vboxwl: Improve check for Plasma (on Wayland) DE, bugref:10194.
    svn:sync-xref-src-repo-rev: r168392

--- src/VBox/Additions/x11/VBoxClient/vboxwl.cpp
+++ src/VBox/Additions/x11/VBoxClient/vboxwl.cpp
@@ -1,4 +1,4 @@
-/* $Id: vboxwl.cpp 164827 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: vboxwl.cpp 168392 2025-04-09 11:16:52Z vadim.galitsyn@oracle.com $ */
 /** @file
  * Guest Additions - Helper tool for grabbing input focus and perform
  * drag-n-drop and clipboard sharing in Wayland.
@@ -668,7 +668,9 @@ static void vboxwl_check(void)
 
         /* Try to detect Plasma. */
         const char *pcszDesktopSession = RTEnvGet(VBGH_ENV_DESKTOP_SESSION);
-        if (RT_VALID_PTR(pcszDesktopSession) && RTStrIStr(pcszDesktopSession, "plasmawayland"))
+        if (   RT_VALID_PTR(pcszDesktopSession)
+            && (   RTStrIStr(pcszDesktopSession, "plasmawayland")
+                || RTStrIStr(pcszDesktopSession, "plasma")))
             fWayland = true;
     }
 
