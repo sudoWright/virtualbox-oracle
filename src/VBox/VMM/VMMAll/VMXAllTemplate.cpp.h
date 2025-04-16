32dded38ec6ab9844ec9f1ca59a1c56e3fbd3e43
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2025-04-16 06:47:47 +0000

    VMM: bugref:3409 Build fix.
    svn:sync-xref-src-repo-rev: r168519

--- src/VBox/VMM/VMMAll/VMXAllTemplate.cpp.h
+++ src/VBox/VMM/VMMAll/VMXAllTemplate.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: VMXAllTemplate.cpp.h 168518 2025-04-16 06:35:42Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: VMXAllTemplate.cpp.h 168519 2025-04-16 06:47:47Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Code template for our own hypervisor and the NEM darwin backend using Apple's Hypervisor.framework.
  */
@@ -4489,7 +4489,7 @@ static void vmxHCTrpmTrapToPendingEvent(PVMCPUCC pVCpu)
     u32IntInfo  = uVector | VMX_IDT_VECTORING_INFO_VALID;
     u32IntInfo |= HMTrpmEventTypeToVmxEventType(uVector, enmTrpmEvent, fIcebp);
 
-    rc = TRPMResetTrap(pVCpu);
+    int const rc = TRPMResetTrap(pVCpu);
     AssertRC(rc);
     Log4(("TRPM->HM event: u32IntInfo=%#RX32 enmTrpmEvent=%d cbInstr=%u uErrCode=%#RX32 GCPtrFaultAddress=%#RGv\n",
           u32IntInfo, enmTrpmEvent, cbInstr, uErrCode, GCPtrFaultAddress));
