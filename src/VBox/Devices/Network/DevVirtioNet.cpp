a9e0744351aa53072e45124f12137be5a2e3e368
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2025-05-26 19:56:17 +0000

    7.1: Backported r169002: Devices/DevVirtioNet: Use the same file header style as everywhere.
    svn:sync-xref-src-repo-rev: r169005

--- src/VBox/Devices/Network/DevVirtioNet.cpp
+++ src/VBox/Devices/Network/DevVirtioNet.cpp
@@ -1,5 +1,4 @@
-/* $Id: DevVirtioNet.cpp 168753 2025-05-07 08:16:10Z aleksey.ilyushin@oracle.com $ $Revision: 168753 $ $Date: 2025-05-07 10:16:10 +0200 (Wed, 07 May 2025) $ $Author: aleksey.ilyushin@oracle.com $ */
-
+/* $Id: DevVirtioNet.cpp 169005 2025-05-26 19:56:17Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VBox storage devices - Virtio NET Driver
  *
