0c27b4d44ddd481bd7783d4d51bbcb3367a5e57c
Alexander Eichner <alexander.eichner@oracle.com>
2025-06-10 11:22:29 +0000

    apps/svn2git-vbox: Remove .gitignore on non empty directories if svn:ignore properties got deleted, bugref:10900
    svn:sync-xref-src-repo-rev: r169164

--- src/apps/svn2git-vbox/svn2git.cpp
+++ src/apps/svn2git-vbox/svn2git.cpp
@@ -1,4 +1,4 @@
-/* $Id: svn2git.cpp 169163 2025-06-10 11:14:47Z alexander.eichner@oracle.com $ */
+/* $Id: svn2git.cpp 169164 2025-06-10 11:22:29Z alexander.eichner@oracle.com $ */
 /** @file
  * svn2git - Convert a svn repository to git.
  */
@@ -388,7 +388,7 @@ static RTEXITCODE s2gParseArguments(PS2GCTX pThis, int argc, char **argv)
             case 'V':
             {
                 /* The following is assuming that svn does it's job here. */
-                static const char s_szRev[] = "$Revision: 169163 $";
+                static const char s_szRev[] = "$Revision: 169164 $";
                 const char *psz = RTStrStripL(strchr(s_szRev, ' '));
                 RTMsgInfo("r%.*s\n", strchr(psz, ' ') - psz, psz);
                 return RTEXITCODE_SUCCESS;
@@ -1378,9 +1378,7 @@ static RTEXITCODE s2gSvnProcessIgnores(PS2GCTX pThis, PS2GSVNREV pRev, const cha
             rcExit = s2gSvnPathIsEmptyDir(pRev, pszSvnPath, &fIsEmpty);
             if (   rcExit == RTEXITCODE_SUCCESS
                 && fIsEmpty)
-            {
-                
-            }
+                rcExit = s2gSvnDeleteGitIgnore(pThis, pszGitPath);
         }
 
         /* Process global ignores only in the root path. */
