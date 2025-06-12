735b562666e83a1a921fff131dd0309edfbb1746
Alexander Eichner <alexander.eichner@oracle.com>
2025-06-12 10:56:35 +0000

    VMM/NEM/win: Fix minor typo, github:gh-40
    svn:sync-xref-src-repo-rev: r169191

--- src/VBox/VMM/VMMR3/NEMR3Native-win-armv8.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-win-armv8.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR3Native-win-armv8.cpp 169094 2025-06-02 22:08:28Z knut.osmundsen@oracle.com $ */
+/* $Id: NEMR3Native-win-armv8.cpp 169191 2025-06-12 10:56:35Z alexander.eichner@oracle.com $ */
 /** @file
  * NEM - Native execution manager, native ring-3 Windows backend.
  *
@@ -1503,7 +1503,7 @@ static DECLCALLBACK(int) nemR3WinSave(PVM pVM, PSSMHANDLE pSSM)
 
         HRESULT hrc = WHvGetVirtualProcessorRegisters(pVM->nem.s.hPartition, pVCpu->idCpu, &s_Name, 1, &Reg);
         AssertLogRelMsgReturn(SUCCEEDED(hrc),
-                              ("WHvSetVirtualProcessorRegisters(%p, 0,{WHvRegisterInternalActivityState}, 1,) -> %Rhrc (Last=%#x/%u)\n",
+                              ("WHvGetVirtualProcessorRegisters(%p, 0,{WHvRegisterInternalActivityState}, 1,) -> %Rhrc (Last=%#x/%u)\n",
                                pVM->nem.s.hPartition, pVCpu->idCpu, hrc, RTNtLastStatusValue(), RTNtLastErrorValue())
                               , VERR_NEM_IPE_9);
 
